4cc0e03e2590a5ff5411d9d8db70b7248c90f242
Nliker <tjwjdgus83@naver.com>
2022-11-05 16:47:18 +0900

    app.py 완성

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_cors import CORS
+from sqlalchemy import create_engine
+from model import UserDao,ImageDao
+from service import UserService,ImageService
+from view import user_router,image_router
+
+import config
+
+class Services:
+    pass
+
+def create_app(test_config=None):
+    app=Flask(__name__)
+
+    CORS(app)
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+        
+    database=create_engine(app.config['DB_URL'],encoding='utf-8',max_overflow=0)
+
+    print("데이터베이스 연결 성공")
+
+    @app.route("/ping",methods=["GET"])
+    def ping():
+        return "pong",200
+    
+    user_dao=UserDao(database)
+    image_dao=ImageDao(database)
+    
+    services=Services
+
+    services.user_service=UserService(user_dao,app.config)
+    services.image_service=ImageService(image_dao,app.config)
+
+    user_router(app,services)
+    image_router(app,services)
+    
+    return app
