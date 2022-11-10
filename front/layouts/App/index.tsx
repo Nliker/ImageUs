4ec44a60fbb089d64b7de265025e489bbdfbd925
kanghunK <ltbehappy16@gmail.com>
2022-11-11 00:02:25 +0900

    feature: 로그인 페이지 초안 작성

---
+++ front/layouts/App/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import loadable from '@loadable/component';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<LogIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+};
+
+export default App;
