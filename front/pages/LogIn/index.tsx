ba71ff106d20ce89c215d7db54d1e48d72f3b5b9
kanghunK <ltbehappy16@gmail.com>
2022-11-11 17:04:25 +0900

    feature: 회원정보 페이지 초안 작성

--- front/pages/LogIn/index.tsx
+++ front/pages/LogIn/index.tsx
@@ -1,38 +1,47 @@
 import React, { useCallback, useState } from 'react';
-import { EmailInputBox, InnerContainer, OuterContainer, CheckBox, Wrapper, MoveBox, EmailInputDiv } from './styled';
+import UserInfoInputBox from '@components/UserInfoInputBox';
+import { EmailInputContainer, InputDiv, PasswordInputContainer } from './styled';
+import { CheckBox, InputBox } from '@components/UserInfoInputBox/styles';
+
+/*
+    이메일 체크를 하고 확인이 되었으면 비밀번호 확인을 한다.
+*/
 
 const LogIn = () => {
+  const [isValidEmail, setIsValidEmail] = useState(false);
   const [checked, setChecked] = useState(false);
 
   const checkHandler = useCallback(() => {
-    console.log(checked);
     return setChecked((prev) => !prev);
   }, []);
 
   return (
-    <Wrapper>
-      <OuterContainer>
-        <h1>로그인</h1>
-        <InnerContainer>
-          <EmailInputBox>
+    <div>
+      <UserInfoInputBox>
+        <EmailInputContainer>
+          <InputBox>
             <label htmlFor="email-input">이메일 주소를 입력하세요.</label>
-            <EmailInputDiv>
+            <InputDiv>
               <input type="text" id="email-input" placeholder="이메일을 입력하세요." />
-            </EmailInputDiv>
-          </EmailInputBox>
-          <CheckBox>
-            <label htmlFor="remember-id" onClick={checkHandler}>
-              <input type="checkbox" checked={checked} readOnly/>
-              로그인 상태 유지하기
-            </label>
-          </CheckBox>
-          <MoveBox>
-            <button type="button">계정 만들기</button>
-            <button type="button">다음</button>
-          </MoveBox>
-        </InnerContainer>
-      </OuterContainer>
-    </Wrapper>
+            </InputDiv>
+          </InputBox>
+        </EmailInputContainer>
+        <PasswordInputContainer>
+          <InputBox>
+            <label htmlFor="password-input">비밀번호를 입력하세요.</label>
+            <InputDiv>
+              <input type="text" id="password-input" placeholder="비밀번호를 입력하세요." />
+            </InputDiv>
+          </InputBox>
+        </PasswordInputContainer>
+        <CheckBox>
+          <label htmlFor="remember-id" onClick={checkHandler}>
+            <input type="checkbox" checked={checked} readOnly />
+            로그인 상태 유지하기
+          </label>
+        </CheckBox>
+      </UserInfoInputBox>
+    </div>
   );
 };
 
