25aef15861185a01d7bfcf36143d2ce82fe62a85
kanghunK <ltbehappy16@gmail.com>
2022-11-13 00:03:34 +0900

    feature: 로그인 입력값의 형식 검사 추가

--- front/components/UserInfoInputBox/index.tsx
+++ front/components/UserInfoInputBox/index.tsx
@@ -1,35 +1,36 @@
-import React, { PropsWithChildren } from 'react';
-import { InnerContainer, OuterContainer, Wrapper, MoveBox } from './styles';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { InnerContainer, OuterContainer, Wrapper } from './styles';
 
-const UserInfoInputBox = ({ children }: PropsWithChildren) => {
-//   const [checked, setChecked] = useState(false);
+interface InputBoxProps {
+  children?: React.ReactNode;
+  pageName?: string;
+}
 
-//   const checkHandler = useCallback(() => {
-//     return setChecked((prev) => !prev);
-//   }, []);
+const UserInfoInputBox = ({ children, pageName }: InputBoxProps) => {
+  // const { currentPage } = useParams<{ currentPage: string }>();
+  //   const [checked, setChecked] = useState(false);
 
+  //   const checkHandler = useCallback(() => {
+  //     return setChecked((prev) => !prev);
+  //   }, []);
+  // console.log(currentPage);
   return (
     <Wrapper>
       <OuterContainer>
         <h1>로그인</h1>
         <InnerContainer>
-          {/* <EmailInputBox>
-            <label htmlFor="email-input">이메일 주소를 입력하세요.</label>
-            <EmailInputDiv>
-              <input type="text" id="email-input" placeholder="이메일을 입력하세요." />
-            </EmailInputDiv>
-          </EmailInputBox>
-          <CheckBox>
-            <label htmlFor="remember-id" onClick={checkHandler}>
-              <input type="checkbox" checked={checked} readOnly/>
-              로그인 상태 유지하기
-            </label>
-          </CheckBox> */}
           {children}
-          <MoveBox>
-            <button type="button">계정 만들기</button>
-            <button type="button">다음</button>
-          </MoveBox>
+          {/* {pageName === 'login' ? (
+            <MoveBox>
+              <NavLink to={'/signup'}>계정 만들기</NavLink>
+              <button type="submit">확인</button>
+            </MoveBox>
+          ) : (
+            <MoveBox>
+              <button type="submit">제출</button>
+            </MoveBox>
+          )} */}
         </InnerContainer>
       </OuterContainer>
     </Wrapper>
