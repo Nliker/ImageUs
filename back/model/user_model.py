ec3532a2a1df9dfe7b6dc8f460994e7431798e66
Nliker <tjwjdgus83@naver.com>
2022-11-14 02:35:33 +0900

    user_model 함수 정의 수정

--- back/model/user_model.py
+++ back/model/user_model.py
@@ -6,12 +6,12 @@ class ImageDao:
         
     def insert_user(new_user):
         
-    def get_user_id_and_password(email):
+    def get_user_id_and_password(self,email):
 
-    def get_user_info(user_id):
+    def get_user_info(self,user_id):
         
-    def insert_friend(user_id,friend_user_id):
+    def insert_friend(self,user_id,friend_user_id):
     
-    def get_user_friend(user_id,friend_user_id):
+    def get_user_friend(self,user_id,friend_user_id):
     
-    def get_user_frinedlist(user_id):
+    def get_user_frinedlist(self,user_id):
