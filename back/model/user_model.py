0c673b14cc2f701366f5b03bbd8bc57d4f3e3fc8
Nliker <tjwjdgus83@naver.com>
2022-11-14 02:31:34 +0900

    user_model 함수 정의

---
+++ back/model/user_model.py
@@ -0,0 +1,17 @@
+from sqlalchemy import text
+
+class ImageDao:
+    def __init__(self,database):
+        self.db=database
+        
+    def insert_user(new_user):
+        
+    def get_user_id_and_password(email):
+
+    def get_user_info(user_id):
+        
+    def insert_friend(user_id,friend_user_id):
+    
+    def get_user_friend(user_id,friend_user_id):
+    
+    def get_user_frinedlist(user_id):
