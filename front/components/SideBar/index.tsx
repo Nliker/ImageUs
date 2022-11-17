09f2c8cf5c16ec4abe704bb076e23f38042c5731
kanghunK <ltbehappy16@gmail.com>
2022-11-17 23:31:23 +0900

    feature: 사이드바 효과 추가

--- front/components/SideBar/index.tsx
+++ front/components/SideBar/index.tsx
@@ -1,26 +1,26 @@
 import ChannelList from '@components/ChannelList';
 import MemberList from '@components/MemberList';
 import React from 'react';
-import { ContentWrapper, RoomItem, Wrapper } from './styles';
+import { ChannelListBox, ContentWrapper, Wrapper } from './styles';
 
 // 현재 구상 중인 것은 모바일 버전일 경우 토글 버튼으로 왼쪽에서 나오고
 // PC버전일 경우 왼쪽부분에 그대로 붙어있는 모양이다.
 
-const SideBar = () => {
+const SideBar = ({show}: {show: boolean}) => {
   
   return (
-    <Wrapper>
+    <Wrapper show={show}>
       {/* <MemberList /> */}
-      <div>
+      {/* <div>
         <h1>방 이름</h1>
-      </div>
+      </div> */}
       <ContentWrapper>
-        <div>
-          <h2>방 목록</h2>
+        <ChannelListBox>
+          {/* <h2>방 목록</h2> */}
           <ChannelList />
-        </div>
+        </ChannelListBox>
         <div>
-          <h2>현재 방에 있는 사람 목록</h2>
+          {/* <h2>현재 방에 있는 사람 목록</h2> */}
           <MemberList />
         </div>
         {/* <div>
