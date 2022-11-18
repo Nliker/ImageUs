c0c76fd4b7fafa9e06476a5e448ebdc95e86ac85
kanghunK <ltbehappy16@gmail.com>
2022-11-18 11:18:58 +0900

    docs, design: NavBar 컴포넌트 -> TopNavBar, BottomNavBar 추가 및 css 수정

---
+++ front/components/BottomNavBar/styles.tsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.nav`
+  display: block;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  z-index: 100;
+  font-size: 0.75rem;
+`;
+
+export const BottomContainer = styled.div`
+  height: 66px;
+  width: 100%;
+  box-shadow: 0 -2px 4px 0 rgb(33 37 41 / 8%);
+  background-color: #fff;
+  a {
+    text-decoration: none;
+    color: #47443e;
+  }
+`;
+
+export const MobileNavList = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+`;
+
+export const MobileNavItem = styled.li`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  list-style: none;
+  box-sizing: border-box;
+  padding: 0.5rem 1rem;
+  a {
+    border-radius: 5px;
+    svg {
+      display: block;
+      margin: 0 auto 3px;
+      width: 25px;
+      height: auto;
+    }
+    &:hover {
+      background-color: #d6d6d6;
+    }
+  }
+`;
---
+++ front/components/TopNavBar/index.tsx
@@ -0,0 +1,116 @@
+import React, { useCallback, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { DesktopContainer, NavItem, NavList, Wrapper } from './pc_styles';
+import { HiOutlineHome } from 'react-icons/hi';
+import { BsPeople, BsListUl } from 'react-icons/bs';
+import { RiFilter2Line } from 'react-icons/ri';
+import { IoPeopleCircleSharp } from 'react-icons/io5';
+import { MdOutlineManageAccounts } from 'react-icons/md';
+import { MobileBtnDiv, MobileButton, MobileContainer, TopContainer } from './mobile_styles';
+import { useMediaQuery } from 'react-responsive';
+
+const TopNavBar = () => {
+  const DesktopNav = ({ children }: any) => {
+    const isDesktop = useMediaQuery({ minWidth: 1024 });
+    return isDesktop ? children : null;
+  };
+  const MobileNav = ({ children }: any) => {
+    const isMobile = useMediaQuery({ maxWidth: 1023 });
+    return isMobile ? children : null;
+  };
+  const isMobile = useMediaQuery({ maxWidth: 1023 });
+  const [showUserMenu, setShowUserMenu] = useState(false);
+  const onClickUserProfile = useCallback((e: { stopPropagation: () => void }) => {
+    e.stopPropagation();
+    setShowUserMenu((prev) => !prev);
+  }, []);
+
+  return (
+    <Wrapper>
+      <DesktopNav>
+        <DesktopContainer>
+          <h1>
+            <NavLink to={'/main_page'}>
+              <HiOutlineHome />
+              Cloudy
+            </NavLink>
+          </h1>
+          <NavList>
+            {/* <NavItem>
+              <NavLink to={'/main_page'}>
+                <HiOutlineHome />
+                메인 페이지
+              </NavLink>
+            </NavItem> */}
+            <NavItem>
+              <NavLink to={'/my_page'}>
+                <MdOutlineManageAccounts />
+                마이 페이지
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to={'/magnagement_page'}>
+                <BsPeople />
+                친구목록 관리
+              </NavLink>
+            </NavItem>
+          </NavList>
+          {/* <ProfileIconWrapper>
+            <span>
+              <button onClick={onClickUserProfile}>프로필 이미지</button>
+            </span>
+            {showUserMenu && <Menu show={showUserMenu} onCloseModal={onClickUserProfile}></Menu>}
+          </ProfileIconWrapper> */}
+        </DesktopContainer>
+      </DesktopNav>
+      <MobileNav>
+        <MobileContainer>
+          <TopContainer>
+            <MobileBtnDiv>
+              <MobileButton>
+                <IoPeopleCircleSharp />
+                {/* 현재 방의 제목이 들어감 */}
+              </MobileButton>
+            </MobileBtnDiv>
+            <div>
+              <h1>
+                <NavLink to={'/main_page'}>
+                  <HiOutlineHome />
+                  Cloudy
+                </NavLink>
+              </h1>
+            </div>
+            <MobileBtnDiv>
+              <MobileButton>
+                <RiFilter2Line />
+              </MobileButton>
+            </MobileBtnDiv>
+          </TopContainer>
+          {/* <BottomContainer>
+            <MobileNavList>
+              <MobileNavItem>
+                <NavLink to={'/my_page'}>
+                  <MdOutlineManageAccounts />
+                  마이 페이지
+                </NavLink>
+              </MobileNavItem>
+              <MobileNavItem>
+                <NavLink to={'/'}>
+                  홈 버튼
+                </NavLink>
+              </MobileNavItem>
+              <MobileNavItem>
+                <NavLink to={'/magnagement_page'}>
+                  <BsPeople />
+                  친구목록 관리
+                </NavLink>
+              </MobileNavItem>
+            </MobileNavList>
+          </BottomContainer> */}
+        </MobileContainer>
+      </MobileNav>
+    </Wrapper>
+  );
+};
+
+export default TopNavBar;
