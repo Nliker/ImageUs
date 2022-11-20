521fc7eca2dcd0d590e0f137d22014dac5376c42
Nliker <tjwjdgus83@naver.com>
2022-11-20 18:10:33 +0900

    image_model 수정

--- image_back/model/image_model.py
+++ image_back/model/image_model.py
@@ -10,6 +10,7 @@ class ImageDao:
             from images
             where user_id=:user_id
             and link=:image_link
+            and deleted=0
             """),{'user_id':user_id,'image_link':image_link}).fetchone()
         
         image_info={
@@ -19,10 +20,28 @@ class ImageDao:
         } if row else None
 
         return image_info
+    
+    def get_image_info_by_id(self,image_id):
+        row=self.db.execute(text("""
+            select id,user_id,link,public
+            from images
+            where id=:image_id
+            and deleted=0
+            """),{'image_id':image_id}).fetchone()
+
+        image_info={
+            'id':row['id'],
+            'user_id':row['user_id'],
+            'link':row['link'],
+            'public':row['public']
+        } if row else None
+        
+        return image_info
+    
     def image_room_userlist(self,image_id):
         rows=self.db.execute(text("""
             select
-                i_r.room_id as room_id
+                i_r.room_id as room_id,
                 r_u.user_id as user_id
             from images_room_list as i_r
             join rooms_user_list as r_u
