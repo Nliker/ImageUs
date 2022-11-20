bf265e8e1deb3fa11c81e39d042e0a2010cf44e1
kanghunK <ltbehappy16@gmail.com>
2022-11-20 17:39:45 +0900

    design: 하단 네비게이션바 업로드 버튼 추가 및 메인페이지 주석 제거

--- front/pages/MainPage/styles.tsx
+++ front/pages/MainPage/styles.tsx
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled';
 
-export const ProfileModal = styled.div``;
+// export const ProfileModal = styled.div``;
 
-export const LogOutButton = styled.button``;
+// export const LogOutButton = styled.button``;
 
 export const Wrappper = styled.div``;
 
