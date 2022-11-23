44c2fd59ae6e8eb02786cad0756d75bf33bd8487
Nliker <tjwjdgus83@naver.com>
2022-11-23 16:04:16 +0900

    test_service 수정

--- back/test/test_service.py
+++ back/test/test_service.py
@@ -876,9 +876,14 @@ def test_delete_image(image_service):
             'user_id':2
         }
     ]
+    image_roomlist=[image_room_info['id'] for image_room_info in get_image_roomlist(2)]
+    assert image_roomlist==[1,2]
     #2번 이미지 삭제 후 2번 유저의 이미지 목록 확인 및 1번 방의 이미지 목록을 확인
     result=image_service.delete_image(2)
-    assert result==1
+    assert result=={
+        'delete_image':1,
+        'delete_image_room':len(image_roomlist)
+    }
     user_image_info_list=get_user_imagelist(2)
     assert user_image_info_list==[]
     room_image_info_list=get_room_imagelist(1)
@@ -894,12 +899,16 @@ def test_delete_image(image_service):
             'user_id':None
         }
     ]
-
+    image_roomlist=[image_room_info['id'] for image_room_info in get_image_roomlist(2)]
+    assert image_roomlist==[]
     image_info=get_image_info(2)
     assert image_info==None
     #이미 삭제된 이미지의 중복 삭제 확인
     result=image_service.delete_image(2)
-    assert result==0
+    assert result=={
+        'delete_image':0,
+        'delete_image_room':len(image_roomlist)
+    }
     
 #방에 속한 유저의 모든 이미지 제거
 def test_delete_room_user_image(image_service):
@@ -1139,6 +1148,27 @@ def test_upload_room_image(image_service):
         }
     ]
 
+def test_is_user_image_room_member(image_service):
+    is_user_image_room_member=image_service.is_user_image_room_member(3,4)
+    assert is_user_image_room_member==True
+    is_user_image_room_member=image_service.is_user_image_room_member(3,2)
+    assert is_user_image_room_member==True
+    
+    is_user_image_room_member=image_service.is_user_image_room_member(1,2)
+    assert is_user_image_room_member==True
+    is_user_image_room_member=image_service.is_user_image_room_member(1,3)
+    assert is_user_image_room_member==False
+
+    row=database.execute(text("""
+        update rooms_user_list
+        set deleted=1
+        where room_id=1
+        and user_id=1
+        and deleted=0
+        """)).rowcount
+    assert row==1
+    is_user_image_room_member=image_service.is_user_image_room_member(1,2)
+    assert is_user_image_room_member==False
 
 '''
     유저 1,2,3 (친구 1-2,친구 2-1,3,친구 3-2)
