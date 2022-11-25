948d9ede9a1c647dbd3ca3c6150bd8b1a8a4d19c
Nliker <tjwjdgus83@naver.com>
2022-11-25 18:56:32 +0900

    restx 의 room_view 관련 parser 클래스 작성

--- back/tool/api.py
+++ back/tool/api.py
@@ -1,4 +1,5 @@
 from flask_restx import reqparse
+from werkzeug.datastructures import FileStorage
 
 class ParserModule:
     def __init__(self,api,args):
@@ -8,41 +9,87 @@ class ParserModule:
         if 'Authorization in args':
             self.parser.add_argument('Authorization',type=str,
                         location='headers',
-                        helsp='access_token')
+                        helsp='access_token',
+                        required=False)
         
         if 'name' in args:
             self.parser.add_argument('name',type=str,
                         location='json',
-                        help='str_name')
+                        help='str_name',
+                        required=False)
         
         if 'email' in args:
             self.parser.add_argument('email',type=str,
                         location='json',
-                        help='str_email')
+                        help='str_email',
+                        required=False)
         
         if 'profile' in args:
             self.parser.add_argument('profile',type=str,
                         location='json',
-                        help='str_profile')
+                        help='str_profile',
+                        required=False)
         
         if 'password' in args:
             self.parser.add_argument('password',type=str,
                         location='json',
-                        help='str_password')
+                        help='str_password',
+                        required=False)
         
         if 'friend_user_id' in args:
             self.parser.add_argument('friend_user_id',type=int,
                         location='json',
-                        help='int_friend_user_id')
+                        help='int_friend_user_id',
+                        required=False)
         
         if 'delete_friend_user_id' in args:
             self.parser.add_argument('delete_friend_user_id',type=int,
                         location='json',
-                        help='int_delete_friend_user_id')
+                        help='int_delete_friend_user_id',
+                        required=False)
         
         if 'delete_user_room_id' in args:
             self.parser.add_argument('delete_user_room_id',tpye=int,
                         location='json',
-                        help='int_delete_user_room_id')
+                        help='int_delete_user_room_id',
+                        required=False)
         
+        if 'userlist' in args:
+            self.parser.add_argument('userlist[]',type=int,action='append',
+                        location='json',
+                        help='int_array_userlist',
+                        required=False)
+    
+        if 'title' in args:
+            self.parser.add_argument('title',type=str,
+                        location='json',
+                        help='str_title',
+                        required=False)
+
+        if 'image' in args:
+            self.parser.add_argument('image',type=FileStorage,
+                            location='files',
+                            help='imagefile',
+                            required=False)
+        
+        if 'delete_room_image_id' in args:
+            self.parser.add_argument('delete_room_image_id',type=int,
+                        location='json',
+                        help='int_delete_room_image_id',
+                        required=False)
+            
+        if 'delete_room_user_id' in args:
+            self.parser.add_argument('delete_room_user_id',type=int,
+                        location='json',
+                        help='int_delete_room_user_id',
+                        required=False)
+                
+        if 'invite_userlist' in args:
+            self.parser.add_argument('invite_userlist[]',
+                        type=int,
+                        action='append',
+                        location='json',
+                        help='int_array_invite_userlist',
+                        required=False)
+            
         
