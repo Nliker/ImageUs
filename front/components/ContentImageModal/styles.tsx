88abc58113f768c76a7a1b774cc416b08462dd46
kanghunK <ltbehappy16@gmail.com>
2022-11-27 18:18:29 +0900

    style: 이미지 모달 position 및 z-index 수정

--- front/components/ContentImageModal/styles.tsx
+++ front/components/ContentImageModal/styles.tsx
@@ -1,7 +1,15 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const ModalBoxContainer = styled.div`
-    position: relative;
+  position: relative;
+  height: 100%;
+  z-index: 1002;
 `;
 
-export const ModalBox = styled.div``;
+export const ModalBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
