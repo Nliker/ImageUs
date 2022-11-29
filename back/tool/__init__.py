b27ae5cf16d556cd031cbedb86649b67968cd721
Nliker <tjwjdgus83@naver.com>
2022-11-29 15:54:03 +0900

    class 변수 수정 및 메서드 수정

--- back/tool/__init__.py
+++ back/tool/__init__.py
@@ -1,8 +1,10 @@
 from .tools import generate_random_sting
 from .api import ParserModule,ApiModel
+from .api import ApiError
 
 __all__=[
     'generate_random_sting',
     'ParserModule',
-    'ApiModel'
+    'ApiModel',
+    'ApiError'
 ]
