d6a2c1e97c269e4dba0ce946d65b869b8e6f6465
kanghunK <ltbehappy16@gmail.com>
2022-11-30 22:18:02 +0900

    feat: 친구목록 관리 css 추가

--- front/pages/FriendsList/index.tsx
+++ front/pages/FriendsList/index.tsx
@@ -1,12 +1,87 @@
-import AppLayout from "@layouts/AppLayout";
-import React from "react";
+import AppLayout from '@layouts/AppLayout';
+import React from 'react';
+import { ContentBox, ContentList, Header, Wrapper } from './styles';
+
+const dummyFreindList = [
+  {
+    id: 1,
+    name: '친구1',
+    email: 'email1',
+    type: 'freind',
+  },
+  {
+    id: 2,
+    name: '친구2',
+    email: 'email2',
+    type: 'freind',
+  },
+  {
+    id: 3,
+    name: '친구3',
+    email: 'email3',
+    type: 'freind',
+  },
+  {
+    id: 4,
+    name: '가족1',
+    email: 'email4',
+    type: 'family',
+  },
+  {
+    id: 5,
+    name: '가족2',
+    email: 'email5',
+    type: 'family',
+  },
+];
+
+// 전체 친구들 목록을 보여주고 각 친구들 추가와 삭제
 
 const FriendList = () => {
-    return (
-        <AppLayout>
-            
-        </AppLayout>
-    );
+  return (
+    <AppLayout>
+      <Wrapper>
+        <Header>
+          <div>
+            <h1>Follower List</h1>
+          </div>
+        </Header>
+        <ContentBox>
+          <ContentList>
+            <div>
+              <table>
+                <colgroup></colgroup>
+                <thead>
+                  <tr>
+                    <th scope="col">이름</th>
+                    <th scope="col">이메일</th>
+                    <th scope="col">관계</th>
+                    <th scope="col">조회</th>
+                    <th scope="col">목록에서 삭제</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {dummyFreindList.map((data) => (
+                    <tr key={data.id}>
+                      <td>{data.name}</td>
+                      <td>{data.email}</td>
+                      <td>{data.type}</td>
+                      <td>
+                        <div><button type='button'>함께 있는 방 조회</button></div>
+                      </td>
+                      <td>
+                        <div><button type='button'>삭제</button></div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </ContentList>
+        </ContentBox>
+      </Wrapper>
+    </AppLayout>
+  );
 };
 
 export default FriendList;
