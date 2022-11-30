092c04896575a40833386b5eaf88e2ed52a52ff8
kanghunK <ltbehappy16@gmail.com>
2022-11-30 15:46:14 +0900

    feature: FriendsList 페이지 생성

---
+++ front/pages/FriendsList/index.tsx
@@ -0,0 +1,12 @@
+import AppLayout from "@layouts/AppLayout";
+import React from "react";
+
+const FriendList = () => {
+    return (
+        <AppLayout>
+            
+        </AppLayout>
+    );
+};
+
+export default FriendList;
