459e20276fd146a21c73e968e50820d10d269e65
Nliker <tjwjdgus83@naver.com>
2022-11-30 09:47:49 +0900

    asyncio wait gather 테스트

--- back/sample.py
+++ back/sample.py
@@ -51,17 +51,20 @@ import time
 import asyncio
 
 async def find_users_async(n):
-    print(""""=======""""")
+    
     for i in range(1, n + 1):
         print(f'{n}명 중 {i}번 째 사용자 조회 중 ...')
+        await asyncio.sleep(1)
     print(f'> 총 {n} 명 사용자 비동기 조회 완료!')
 
     
 async def process_async():
     start = time.time()
-    await find_users_async(3),
-    await find_users_async(2),
-    await find_users_async(1),
+    await asyncio.wait([
+        find_users_async(3),
+        find_users_async(2),
+        find_users_async(1),
+    ])
     end = time.time()
     print(f'>>> 비동기 처리 총 소요 시간: {end - start}')
 
