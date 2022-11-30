4e6a4b732c386f23ef720659d7b4d447cbe68f6d
Nliker <tjwjdgus83@naver.com>
2022-11-30 09:40:35 +0900

    asyncio 테스트

--- back/sample.py
+++ back/sample.py
@@ -47,6 +47,23 @@
 # update_list=sorted(image_roomlist.union(update_roomlist))
 # print(update_list)
 
-a={}
-a['name']=1
-print(a)
+import time
+import asyncio
+
+async def find_users_async(n):
+    print(""""=======""""")
+    for i in range(1, n + 1):
+        print(f'{n}명 중 {i}번 째 사용자 조회 중 ...')
+    print(f'> 총 {n} 명 사용자 비동기 조회 완료!')
+
+    
+async def process_async():
+    start = time.time()
+    await find_users_async(3),
+    await find_users_async(2),
+    await find_users_async(1),
+    end = time.time()
+    print(f'>>> 비동기 처리 총 소요 시간: {end - start}')
+
+if __name__ == '__main__':
+    asyncio.run(process_async())
