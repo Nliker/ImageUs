9a378fe3c79e4571e121f1472a6da5d4e20b3191
kanghunK <ltbehappy16@gmail.com>
2022-12-01 12:38:55 +0900

    design: 사용자 정보 및 로그아웃 박스 css 추가

--- front/components/ToolBar/styles.tsx
+++ front/components/ToolBar/styles.tsx
@@ -7,7 +7,7 @@ export const Wrapper = styled.div`
   align-items: center;
   padding: 0 20px;
   box-shadow: rgb(0 0 0 / 10%) 2px 0px 2px;
-  div {
+  & > div:not(:last-of-type) {
     display: flex;
     align-items: center;
     height: 100%;
@@ -27,6 +27,7 @@ export const Wrapper = styled.div`
 export const RightIcons = styled.div`
   flex-basis: 50%;
   justify-content: flex-end;
+  margin-right: 26px;
   & > div:first-of-type {
     margin-right: 20px;
   }
@@ -34,6 +35,57 @@ export const RightIcons = styled.div`
 
 export const LeftIcon = styled.div`
   flex-basis: 50%;
+`;
+
+export const UserBox = styled.div`
+  position: absolute;
+  flex-direction: column;
+  height: auto;
+  right: 20px;
+  top: 45px;
+  padding: 20px;
+  background-color: white;
+  z-index: 1;
+  border-radius: 12px;
+  box-shadow: 0 0.125rem 0.5rem rgb(0 0 0 / 30%), 0 0.0625rem 0.125rem rgb(0 0 0 / 20%);
+  &::before {
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 0;
+    bottom: 100%;
+    right: 1.5em;
+    border: 0.75rem solid transparent;
+    border-top: none;
+    border-bottom-color: #fff;
+    filter: drop-shadow(0 -0.0625rem 0.0625rem rgba(0, 0, 0, 0.1));
+  }
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  p {
+    margin: 0;
+    span {
+      font-weight: bold;
+    }
+  }
+  img {
+    width: 50px;
+  }
+  .info_words {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin-left: 10px;
+  }
+`;
+
+export const LogoutBtn = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: end;
+  height: 40px;
 `;
 
 // export const SideBarButton = styled.span`
