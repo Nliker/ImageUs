84893583c621dd7f1453fcc2387e791f5e3f952f
kanghunK <ltbehappy16@gmail.com>
2022-12-02 22:49:10 +0900

    feat: 회원, 비회원시 보여줄 컴포넌트 라우팅 설정

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import loadable from '@loadable/component';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import FriendList from '@pages/FriendsList';
+import useSWR from 'swr';
 
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 const MainPage = loadable(() => import('@pages/MainPage'));
 const MyPage = loadable(() => import('@pages/MyPage'));
+const FriendList = loadable(() => import('@pages/FriendsList'));
 
 const App = () => {
+  const { data: isLogIn } = useSWR('login');
+  console.log('app', isLogIn);
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace={true} />} />
+      <Route
+        path="/"
+        element={isLogIn?.login === true ? <Navigate to="/main_page" replace /> : <Navigate to="/login" replace />}
+      />
       <Route path="/login" element={<LogIn />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/main_page" element={<MainPage />} />
-      <Route path="/my_page/*" element={<MyPage />} />
-      <Route path="/friend_list" element={<FriendList />} />
+      {/* 이하 비회원 접근 불가 페이지 */}
+      {/* <Route path="/*" element={<LogInCheck />} /> */}
+      {isLogIn && (
+        <>
+          <Route path="/main_page" element={<MainPage />} />
+          <Route path="/my_page/*" element={<MyPage />} />
+          <Route path="/friend_list" element={<FriendList />} />
+        </>
+      )}
+      {/* 추후에 404페이지 작성용 Route */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
