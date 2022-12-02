821eeaf3311cf395126f79d3c7334b597b50ed60
kanghunK <ltbehappy16@gmail.com>
2022-12-02 22:51:12 +0900

    feat: 비회원 접근 제한하도록 코드 추가

--- front/pages/MainPage/index.tsx
+++ front/pages/MainPage/index.tsx
@@ -1,37 +1,27 @@
 import ContentImageModal from '@components/ContentImageModal';
 import ContentSection from '@components/ContentSection';
 import AppLayout from '@layouts/AppLayout';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import useSWR from 'swr';
 import { ContentWrappper, Wrappper } from './styles';
 
 const MainPage = () => {
-  const [showUserMenu, setShowUserMenu] = useState(false);
-  // const onClickUserProfile = useCallback((e: { stopPropagation: () => void }) => {
-  //   e.stopPropagation();
-  //   setShowUserMenu((prev) => !prev);
-  // }, []);
-
-  const onLogout = useCallback(() => {
-    //  axios 요청
-    // axios
-    //   .post('/api/users/logout', null, {
-    //     withCredentials: true,
-    //   })
-    //   .then(() => {
-    //     // mutate(false);
-    //     revalidateUser();
-    //   })
-    //   .catch((error) => {
-    //     console.dir(error);
-    //     toast.error(error.response?.data, { position: 'bottom-center' });
-    //   });
+  const { data: isLogIn } = useSWR('login');
+  const navigate = useNavigate();
+  console.log('main', isLogIn);
+  useEffect(() => {
+    if (!isLogIn) {
+      alert('로그인이 필요합니다.');
+      navigate('/login');
+    }
   }, []);
-  
+
   return (
     <AppLayout>
       <Wrappper>
         <ContentWrappper>
-          <ContentSection/>
+          <ContentSection />
         </ContentWrappper>
       </Wrappper>
     </AppLayout>
