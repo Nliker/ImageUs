4b4661b7a7cc063f2051587412f69c5539fe4e0b
kanghunK <ltbehappy16@gmail.com>
2022-12-02 18:50:46 +0900

    feat: 리다이렉트 Route 추가

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -11,12 +11,13 @@ const MyPage = loadable(() => import('@pages/MyPage'));
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to="/login" replace={true} />} />
       <Route path="/login" element={<LogIn />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/main_page" element={<MainPage />} />
       <Route path="/my_page/*" element={<MyPage />} />
       <Route path="/friend_list" element={<FriendList />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
