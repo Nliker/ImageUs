f83815eb9cfdc59b41faa449f2dd4ff764357ec0
kanghunK <ltbehappy16@gmail.com>
2022-12-03 17:57:17 +0900

    feat: 방 생성하는 버튼 html, css 추가

--- front/components/SideBar/index.tsx
+++ front/components/SideBar/index.tsx
@@ -1,7 +1,7 @@
 import ChannelList from '@components/ChannelList';
 import MemberList from '@components/MemberList';
 import React from 'react';
-import { ChannelListBox, ContentWrapper, Wrapper } from './styles';
+import { ChannelListBox, ContentWrapper, CreateRoomBtn, Wrapper } from './styles';
 
 interface SidebarProps {
   show: boolean;
@@ -11,10 +11,6 @@ const SideBar = ({ show }: SidebarProps) => {
   // console.log(isMobile);
   return (
     <Wrapper show={show}>
-      {/* <MemberList /> */}
-      {/* <div>
-        <h1>방 이름</h1>
-      </div> */}
       <ContentWrapper>
         <ChannelListBox>
           {/* <h2>방 목록</h2> */}
@@ -23,6 +19,15 @@ const SideBar = ({ show }: SidebarProps) => {
         <div>
           {/* <h2>현재 방에 있는 사람 목록</h2> */}
           <MemberList />
+        </div>
+        <div>
+          <CreateRoomBtn>
+            <div className='btn_content'>
+              <div className='btn_icon'>
+                <p className='btn_text'>방 생성하기</p>
+              </div>
+            </div>
+          </CreateRoomBtn>
         </div>
         {/* <div>
           <span>+</span>
