07db8ba433b05cc4f1eab92a69e07d9420150560
kanghunK <ltbehappy16@gmail.com>
2022-12-03 17:57:17 +0900

    feat: 방 생성하는 버튼 html, css 추가

--- front/components/SideBar/styles.tsx
+++ front/components/SideBar/styles.tsx
@@ -14,7 +14,7 @@ export const Wrapper = styled.div<{ show: boolean }>`
   transform: translateX(-215px);
 
   ${({ show }) =>
-    (show) &&
+    show &&
     `
     visibility: visible;
     transition: transform 0.5s;
@@ -31,4 +31,80 @@ export const ContentWrapper = styled.div`
 
 export const ChannelListBox = styled.div`
   // margin-bottom: 10px;
+`;
+
+export const CreateRoomBtn = styled.button`
+  position: relative;
+
+  padding: 0;
+  width: 150px;
+  height: 50px;
+
+  border: 4px solid #888888;
+  outline: none;
+  background-color: #f4f5f6;
+  border-radius: 40px;
+  box-shadow: -6px -20px 35px #ffffff, -6px -10px 15px #ffffff, -20px 0px 30px #ffffff, 6px 20px 25px rgba(0, 0, 0, 0.2);
+  transition: 0.13s ease-in-out;
+  cursor: pointer;
+
+  &:active {
+    box-shadow: none;
+    .btn_content {
+      box-shadow: none;
+      .btn_icon,
+      .btn_text {
+        transform: translate3d(0px, 0px, 0px);
+      }
+    }
+  }
+  .btn_content {
+    position: relative;
+    // display: grid;
+
+    // padding: 20px;
+    width: 100%;
+    height: 100%;
+
+    // grid-template-columns: 1fr 1fr 1fr 1fr;
+    // grid-template-rows: 1fr 1fr;
+
+    box-shadow: inset 0px -8px 0px #dddddd, 0px -8px 0px #f4f5f6;
+    border-radius: 40px;
+    transition: 0.13s ease-in-out;
+
+    z-index: 1;
+  }
+  .btn_icon {
+    position: relative;
+    display: flex;
+    justify-content: center;
+
+    transform: translate3d(0px, -4px, 0px);
+    grid-column: 4;
+    align-self: start;
+    justify-self: end;
+    // width: 32px;
+    height: 32px;
+    transition: 0.13s ease-in-out;
+  }
+  .btn_text {
+    position: relative;
+
+    transform: translate3d(0px, -4px, 0px);
+    margin: 0;
+    align-self: end;
+    grid-column: 1/5;
+    grid-row: 2;
+
+    text-align: center;
+    // font-size: 32px;
+    background-color: #888888;
+    color: transparent;
+    text-shadow: 2px 2px 3px rgba(255, 255, 255, 0.5);
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    background-clip: text;
+    transition: 0.13s ease-in-out;
+  }
 `;
