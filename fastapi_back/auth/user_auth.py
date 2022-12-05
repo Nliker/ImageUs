c11674c3d979246761a250a2ca1db17aac8a144f
Nliker <tjwjdgus83@naver.com>
2022-12-05 18:04:28 +0900

    로그인 인증 ㄱ구현

--- fastapi_back/auth/user_auth.py
+++ fastapi_back/auth/user_auth.py
@@ -1,17 +1,18 @@
 import sys,os
 sys.path.append((os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 from fastapi import Header
-from config import Settings
+from config import dev_settings
 import jwt
+from typing import Union
 from custom_exception import error as er
 
-def verify_token(Authorization: str | None=Header(default=None),settings=Settings(),error=er()):
-    access_token=Authorization
+error=er()
+
+async def verify_token(access_token: Union  [str, None]=Header(default=None)):
     if access_token is not None:
         try:
-            payload=jwt.decode(access_token,settings.JWT_SECRET_KEY,"HS256")
+            payload=jwt.decode(access_token,dev_settings.JWT_SECRET_KEY,"HS256")
         except:
-            print("asdasd")
             error.verify_token_error().get_raise()
         if 'user_id' not in payload and type(payload['user_id']) !=type(1):
             print("good")
