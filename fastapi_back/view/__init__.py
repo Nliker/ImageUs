c05c87e10ce28d01fea199bc63723d9490289229
Nliker <tjwjdgus83@naver.com>
2022-12-05 18:02:49 +0900

    init 추가

--- fastapi_back/view/__init__.py
+++ fastapi_back/view/__init__.py
@@ -1,10 +1,10 @@
-# from .image_router import image_router
-# from .room_router import room_router
+from .image_router import image_router
+from .room_router import room_router
 from .user_router import user_router
 
 __all__=[
-    # 'image_router',
-    # 'room_router',
-    'user_router'
+    'room_router',
+    'user_router',
+    'image_router'
 ]
 
