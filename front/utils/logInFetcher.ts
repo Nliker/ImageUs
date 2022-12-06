1b3f5f280ff8d6d4bf0c48b70b07a568549d63bb
kanghunK <ltbehappy16@gmail.com>
2022-12-06 11:07:40 +0900

    style: api 테스트 및 주석 추가

--- front/utils/logInFetcher.ts
+++ front/utils/logInFetcher.ts
@@ -1,15 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
+
+// 유저 아이디를 저장하고 토큰 확인을 하는 기능을 한다.
+// 백엔드 수정 시 이름은 Auth로 변경 예정
 
 // const logInFetcher = (url: string) => {
-//     axios.get('/auth', {
-//         headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${sessionStorage.getItem('token')}`
-//         }
-//     }).then(() => {
-//         return true;
-//     }).catch(() => {
-//         return false;
+//   if (!sessionStorage.getItem('token')) return false;
+//   axios
+//     .get(url, {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+//       },
+//     })
+//     .then((res) => {
+//       sessionStorage.setItem('user_id', res.data.id);
+//       return true;
+//     })
+//     .catch(() => {
+//       console.log('유효하지 않은 접근입니다.');
+//       return false;
 //     });
 // };
 
