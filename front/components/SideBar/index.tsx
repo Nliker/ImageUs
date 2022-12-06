3437f6053ea52d4b3ebe6dd8037023206a839efb
kanghunK <ltbehappy16@gmail.com>
2022-12-06 12:50:31 +0900

    feat: 사이드바 목록 별 탭 추가

--- front/components/SideBar/index.tsx
+++ front/components/SideBar/index.tsx
@@ -1,8 +1,8 @@
 import ChannelList from '@components/ChannelList';
 import MemberList from '@components/MemberList';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import useSWR from 'swr';
-import { ChannelListBox, ContentWrapper, CreateRoomBtn, Wrapper } from './styles';
+import { ContentTabs, ContentWrapper, CreateRoomBtn, Tab, Wrapper } from './styles';
 
 interface SidebarProps {
   show: boolean;
@@ -11,6 +11,12 @@ interface SidebarProps {
 const SideBar = ({ show }: SidebarProps) => {
   // console.log(isMobile);
   const { data: showModalData, mutate: showModalMutate } = useSWR('showModalState');
+
+  // const [switchTab, setSwitchTab] = useState<boolean>(true);
+
+  // const onChangeTab = useCallback(() => {
+  //   setSwitchTab(prev => !prev);
+  // }, [])
 
   const onClickCreateRoomBtn = useCallback(() => {
     showModalMutate({
@@ -23,26 +29,48 @@ const SideBar = ({ show }: SidebarProps) => {
   return (
     <Wrapper show={show}>
       <ContentWrapper>
-        <ChannelListBox>
-          {/* <h2>방 목록</h2> */}
-          <ChannelList />
-        </ChannelListBox>
-        <div>
-          {/* <h2>현재 방에 있는 사람 목록</h2> */}
-          <MemberList />
-        </div>
-        <div>
-          <CreateRoomBtn onClick={onClickCreateRoomBtn}>
-            <div className="btn_content">
-              <div className="btn_icon">
-                <p className="btn_text">방 생성하기</p>
-              </div>
+        <ContentTabs>
+          {/* 라디오 그룹을 name 이름이 동일하게 그룹으로 묶고 
+          id로 구분해준다. 그러면 하나의 그룹 안에서 한가지만 선택할 수 있게 된다. */}
+          <Tab>
+            <input type="radio" id="tab-1" name="tab-group-1" defaultChecked/>
+            <label htmlFor="tab-1">방 목록</label>
+            <div className='tab_content'>
+              {/* <h2>방 목록</h2> */}
+              <article>
+                <ChannelList />
+              </article>
+              {/* <div>
+                <CreateRoomBtn onClick={onClickCreateRoomBtn}>
+                  <div className="btn_content">
+                    <div className="btn_icon">
+                      <p className="btn_text">방 생성하기</p>
+                    </div>
+                  </div>
+                </CreateRoomBtn>
+              </div> */}
             </div>
-          </CreateRoomBtn>
-        </div>
-        {/* <div>
-          <span>+</span>
-        </div> */}
+          </Tab>
+          <Tab>
+            <input type="radio" id="tab-2" name="tab-group-1" />
+            <label htmlFor="tab-2">친구목록</label>
+            <div className='tab_content'>
+              {/* <h2>현재 방에 있는 사람 목록</h2> */}
+              <article>
+                <MemberList />
+              </article>
+              {/* <div className='tab_content'>
+                <CreateRoomBtn onClick={onClickCreateRoomBtn}>
+                  <div className="btn_content">
+                    <div className="btn_icon">
+                      <p className="btn_text">방 생성하기</p>
+                    </div>
+                  </div>
+                </CreateRoomBtn>
+              </div> */}
+            </div>
+          </Tab>
+        </ContentTabs>
       </ContentWrapper>
     </Wrapper>
   );
