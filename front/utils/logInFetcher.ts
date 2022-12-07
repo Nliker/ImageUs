555a2a4f0f621a41318176864206e2c89c532f06
kanghunK <ltbehappy16@gmail.com>
2022-12-07 12:48:13 +0900

    fix: useSWR 데이터에 값이 return 되지 않던 버그 수정

--- front/utils/logInFetcher.ts
+++ front/utils/logInFetcher.ts
@@ -1,34 +1,35 @@
 import axios from 'axios';
 
-// 유저 아이디를 저장하고 토큰 확인을 하는 기능을 한다.
-// 백엔드 수정 시 이름은 Auth로 변경 예정
+// key 값: '/user/my' => 반환값으로 유저의 정보를 반환한다.
+// logInFetcher => 토큰 확인을 하는 기능을 하고 로그인 여부를 반환한다.
 
-// const logInFetcher = (url: string) => {
-//   if (!sessionStorage.getItem('token')) return false;
-//   axios
-//     .get(url, {
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-//       },
-//     })
-//     .then((res) => {
-//       sessionStorage.setItem('user_id', res.data.id);
-//       return true;
-//     })
-//     .catch(() => {
-//       console.log('유효하지 않은 접근입니다.');
-//       return false;
-//     });
-// };
-
-// 임시 로그인 Fetcher
-const logInFetcher = () => {
-    const hasToken = sessionStorage.getItem('token');
-    if (hasToken) {
-        return true;
-    }
+const logInFetcher = (url: string) => {
+  if (!sessionStorage.getItem('TOKEN')) {
+    console.log('토큰 없음');
     return false;
+  }
+  return axios
+    .get(url, {
+      headers: {
+        Authorization: `${sessionStorage.getItem('TOKEN')}`,
+      },
+    })
+    .then(() => {
+      console.log('토큰 정상');
+      return true;
+    })
+    .catch(() => {
+      console.log('유효하지 않은 접근입니다.');
+      return false;
+    });
 };
+// 임시 로그인 Fetcher
+// const logInFetcher = () => {
+//     const hasToken = sessionStorage.getItem('token');
+//     if (hasToken) {
+//         return true;
+//     }
+//     return false;
+// };
 
 export default logInFetcher;
