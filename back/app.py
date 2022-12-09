237d73e7bab0c3c0f8d511e0ac4272ccc562e0e3
Nliker <tjwjdgus83@naver.com>
2022-12-09 14:59:59 +0900

    검색을 위한 템플릿 작성

--- back/app.py
+++ back/app.py
@@ -1,11 +1,11 @@
-from flask import Flask,make_response
+from flask import Flask,make_response,render_template
 from flask_cors import CORS
 from sqlalchemy import create_engine
 from model import UserDao,ImageDao,RoomDao
 from service import UserService,ImageService,RoomService
 from view import user_router,image_router,room_router
-
 from flask_restx import Api,Resource
+from elasticsearch import Elasticsearch
 
 class Services:
     pass
@@ -17,15 +17,23 @@ def create_app(test_config=None):
     CORS(app)
     
     api=Api(app,title='cloudy back-server api docs',doc='/api-docs')
-    
+
     if test_config is None:
         app.config.from_pyfile("config.py")
     else:
         app.config.update(test_config)
         
     database=create_engine(app.config['DB_URL'],encoding='utf-8',max_overflow=0)
+    print("mysql 데이터베이스 연결 성공")
+    es=Elasticsearch(hosts=app.config['ELASTIC_URL'])
+    print("elastic 데이터베이스 연결 성공")
 
-    print("데이터베이스 연결 성공")
+    @api.route("/search")
+    class search_user(Resource):
+        def get(self):
+            headers = {'Content-Type': 'text/html'}
+            return make_response(render_template('index.html'),200,
+                                              headers)
     
     @api.route("/ping")
     class ping(Resource):
@@ -43,7 +51,7 @@ def create_app(test_config=None):
     services.image_service=ImageService(image_dao,app.config)
     services.room_service=RoomService(room_dao,app.config)
 
-    user_router(api,services)
+    user_router(api,services,app.config,es)
     room_router(api,services)
     image_router(api,services)
     
