2e84d882bd20976091869f22bf8b744484fcfe79
Nliker <tjwjdgus83@naver.com>
2022-12-09 15:00:50 +0900

    sample user를 삽입하는 코드 작성

--- back/sample.py
+++ back/sample.py
@@ -56,15 +56,24 @@ async def find_users_async(n):
         print(f'{n}명 중 {i}번 째 사용자 조회 중 ...')
         await asyncio.sleep(1)
     print(f'> 총 {n} 명 사용자 비동기 조회 완료!')
-
     
+async def say_hello_delay(n):
+    print("hello",n)
+    await asyncio.sleep(n)
+async def task1(n):
+    task=asyncio.create_task(say_hello_delay(n))
+    await task
+    return n
 async def process_async():
     start = time.time()
-    await asyncio.wait([
-        find_users_async(3),
-        find_users_async(2),
-        find_users_async(1),
-    ])
+    # await asyncio.wait([
+    #     find_users_async(3),
+    #     find_users_async(2),
+    #     find_users_async(1),
+    # ])
+    await task1(5)
+    print("giid")
+    task1(3)
     end = time.time()
     print(f'>>> 비동기 처리 총 소요 시간: {end - start}')
 
