7ac831a44151e8c690e48e7555c92706682dc8c0
kanghunK <ltbehappy16@gmail.com>
2022-12-11 15:55:01 +0900

    style: 주석 제거

--- front/hooks/useInput.ts
+++ front/hooks/useInput.ts
@@ -1,11 +1,5 @@
 import { Dispatch, SetStateAction, useCallback, useState } from "react"
 
-// interface UserInputProps{
-//     value: string;
-//     onChangeForm: (e: ChangeEvent) => void;
-//     setValue: React.Dispatch<SetStateAction<string>>;
-// }
-
 const useInput = (initialValue: string): [string, Dispatch<SetStateAction<string>>, (e: React.ChangeEvent<HTMLInputElement>) => void] => {
     const [value, setValue] = useState(initialValue);
     const onChangeForm = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
