249e570383e00244afba9baf243c591969a319fb
kanghunK <ltbehappy16@gmail.com>
2022-12-11 22:56:55 +0900

    feat: 친구 목록 페이지에 검색 UI 및 데이터 요청 추가

--- front/pages/FriendsList/index.tsx
+++ front/pages/FriendsList/index.tsx
@@ -1,6 +1,10 @@
+import useInput from '@hooks/useInput';
 import AppLayout from '@layouts/AppLayout';
-import React from 'react';
-import { ContentBox, ContentList, Header, SearchBox, Wrapper } from './styles';
+import { ISearchData } from '@typing/db';
+import searchFetcher from '@utils/searchFetcher';
+import React, { FormEvent, useCallback, useEffect, useState } from 'react';
+import useSWR from 'swr';
+import { ContentBox, ContentList, Header, SearchBox, SearchResult, Wrapper } from './styles';
 
 const dummyFreindList = [
   {
@@ -38,6 +42,39 @@ const dummyFreindList = [
 // 전체 친구들 목록을 보여주고 각 친구들 추가와 삭제
 
 const FriendList = () => {
+  const [queryParams, setQueryParams] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [tmpInputData, , handleTmpInputData] = useInput('');
+  const { data: searchData, mutate: searchMutate } = useSWR(['user/search', queryParams], searchFetcher, {
+    revalidateOnFocus: false,
+    revalidateOnMount: false,
+    revalidateOnReconnect: false,
+  });
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      setQueryParams(tmpInputData);
+      searchMutate();
+    }, 1000);
+    return () => clearTimeout(debounce);
+  }, [tmpInputData]);
+
+  const onSubmitSearchData = useCallback((e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // setSearchResult(() => {
+    //   const newData = searchData.map((data: ISearchData) => {
+    //     return {
+    //       email: data.email,
+    //       name: data.name
+    //     }
+    //   });
+    //   return newData;
+    // })
+  }, [tmpInputData]);
+
+  console.log(searchData);
+
   return (
     <AppLayout>
       <Wrapper>
@@ -47,15 +84,49 @@ const FriendList = () => {
           </div>
         </Header>
         <ContentBox>
+          <h2>유저 검색</h2>
           <SearchBox>
-            <form method='get'>
-              <div className='search_input'>
+            <form onSubmit={onSubmitSearchData}>
+              <div className="search_input">
                 <label htmlFor="searchFriend">Search</label>
-                <input type="text" id='searchFriend' name='search' autoComplete='off' placeholder='검색할 유저의 이메일을 입려하세요..'/>
+                <input
+                  type="text"
+                  id="searchFriend"
+                  name="search"
+                  autoComplete="off"
+                  placeholder="검색할 유저의 이메일을 입력하세요.."
+                  onChange={handleTmpInputData}
+                  value={tmpInputData}
+                />
               </div>
-              <div className='search_btn'><button type='submit' >검색</button></div>
+              <div className="search_btn">
+                <button type="submit">검색</button>
+              </div>
             </form>
+            <SearchResult>
+              <div>
+                {searchData && searchData?.length !== 0 ? (
+                  <ul>
+                    {searchData.map((data: ISearchData) => (
+                      <li>
+                        이름: {data.name}, email: {data.email}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <span>검색 결과가 없습니다.</span>
+                )}
+              </div>
+              <div>
+                {searchData && searchData.length !== 0 ? <ul>
+                  {searchData.map((data: ISearchData) => {
+                    return <li>이름은 {data.name}, 이메일은 {data.email}입니다.</li>
+                  })}
+                </ul> : <span>검색결과가 없습니다.</span>}
+              </div>
+            </SearchResult>
           </SearchBox>
+          <h2>친구 목록</h2>
           <ContentList>
             <div>
               <table>
@@ -76,10 +147,14 @@ const FriendList = () => {
                       <td>{data.email}</td>
                       <td>{data.type}</td>
                       <td>
-                        <div><button type='button'>함께 있는 방 조회</button></div>
+                        <div>
+                          <button type="button">함께 있는 방 조회</button>
+                        </div>
                       </td>
                       <td>
-                        <div><button type='button'>삭제</button></div>
+                        <div>
+                          <button type="button">삭제</button>
+                        </div>
                       </td>
                     </tr>
                   ))}
