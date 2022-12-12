acfa989897c3f53bf10b8a4db5b96d0715802225
kanghunK <ltbehappy16@gmail.com>
2022-12-12 21:54:52 +0900

    design: 탭 목록으로 친구목록, 유저검색을 전환해서 볼 수 있도록 css 추가

--- front/components/FriendList/styles.tsx
+++ front/components/FriendList/styles.tsx
@@ -1,9 +1,9 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  margin-top: 30px;
-  border-radius: 12px;
-  box-shadow: 0 0 5px rgb(0 0 0 / 20%);
+  // margin-top: 30px;
+  // border-radius: 12px;
+  // box-shadow: 0 0 5px rgb(0 0 0 / 20%);
   background-color: white;
 
   overflow: hidden;
