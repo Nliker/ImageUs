0c36da4e7f035d2d58cf11901b58fdf3ed07d5d0
kanghunK <ltbehappy16@gmail.com>
2022-12-13 23:31:00 +0900

    modify: searchFetcher 매개변수 변경

--- front/utils/searchFetcher.ts
+++ front/utils/searchFetcher.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const searchFetcher = async (url: string, email: string) => {
+const searchFetcher = async (arg: Array<string>) => {
+    const [url, email] = [arg[0], arg[1]];
     return axios.get(`${url}?email=${email}`).then((res) => {
         const { result } = res.data;
         return result;
