a432341273fa725aa8818c435bbe88b9c9d3ae77
kanghunK <ltbehappy16@gmail.com>
2022-12-14 21:47:44 +0900

    modify: AppLayout에  Toolbar, SideBar 주석처리 및 MainPage로 이동

--- front/pages/MainPage/index.tsx
+++ front/pages/MainPage/index.tsx
@@ -1,19 +1,32 @@
 import ContentImageModal from '@components/ContentImageModal';
 import ContentSection from '@components/ContentSection';
 import CreateRoomModal from '@components/CreateRoomModal';
+import SideBar from '@components/SideBar';
+import ToolBar from '@components/ToolBar';
 import AppLayout from '@layouts/AppLayout';
 import logInFetcher from '@utils/logInFetcher';
 import React, { useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
+import { Route, Routes, useNavigate } from 'react-router';
 import useSWR from 'swr';
 import { ContentWrappper, Wrappper } from './styles';
 
 const MainPage = () => {
+  const [showSideBar, setshowSideBar] = useState<boolean>(false);
+
+  const handleRoomListBtn = useCallback(() => {
+    setshowSideBar((prev) => !prev);
+  }, [showSideBar]);
+
+  
   return (
     <AppLayout>
+      <ToolBar handleRoomListBtn={handleRoomListBtn} />
+      <SideBar show={showSideBar} />
       <Wrappper>
         <ContentWrappper>
-          <ContentSection />
+          <Routes>
+            <Route path={'/main_page/:roomId'} element={<ContentSection />} />
+          </Routes>
         </ContentWrappper>
       </Wrappper>
     </AppLayout>
