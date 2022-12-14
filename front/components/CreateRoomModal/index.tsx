c4db2fcf988d487f675a74abc9adb8dc3704cf54
kanghunK <ltbehappy16@gmail.com>
2022-12-14 20:13:49 +0900

    rename: userFriendFetcher -> userDataFetcher로 파일명 수정, getUserRoomListFetcher 폴더 삭제 후 userDataFetcher로 이동

--- front/components/CreateRoomModal/index.tsx
+++ front/components/CreateRoomModal/index.tsx
@@ -1,4 +1,14 @@
-import React, { useCallback, useState } from 'react';
+import React, {
+  ChangeEvent,
+  FormEvent,
+  FormEventHandler,
+  memo,
+  useCallback,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import useSWR from 'swr';
 import useInput from '@hooks/useInput';
@@ -17,19 +27,50 @@ import {
   Title,
 } from './styles';
 import axios from 'axios';
+import { getUserFriendList } from '@utils/userDataFetcher';
+import { IFriendData } from '@typing/db';
+import { useLinkClickHandler } from 'react-router-dom';
 
 const CreateRoomModal = () => {
   const { data: modalState, mutate: modalMutate } = useSWR('showModalState');
-  const [roomName, setRoomName, handleRoomName] = useInput('');
+  const { data: friendList, mutate: mutateFriendList } = useSWR('friendlist', getUserFriendList, {
+    dedupingInterval: 2000,
+  });
+
   const [currentStage, setCurrentStage] = useState(0);
+  const [roomName, setRoomName, handleRoomName] = useInput('');
+
+  const [selectedMemberId, setSelectedMemberId] = useState<Array<string>>([]);
+  const [checkedMember, setCheckedMember] = useState<Array<IFriendData>>([]);
+
+  useEffect(() => {
+    if (!friendList) return;
+  }, [friendList]);
 
   const onClickPrevStage = useCallback(() => {
     setCurrentStage(0);
   }, []);
 
-  const onClickNextStage = useCallback(() => {
-    setCurrentStage(1);
-  }, []);
+  const onClickNext = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+      // const formData = new FormData(e.target as HTMLFormElement);
+      // const entries = formData.entries();
+      // const selectData = [];
+      // for (const data of entries) {
+      //   const friendId = data[0];
+      //   selectData.push(friendId);
+      // }
+      // setSelectedMemberId([...selectData]);
+      // console.log(selectedMemberId);
+      const selectFriends = friendList.filter((data: IFriendData) => {
+        return selectedMemberId.includes(data.id);
+      });
+      setCheckedMember([...selectFriends]);
+      setCurrentStage(1);
+    },
+    [friendList, checkedMember, selectedMemberId],
+  );
 
   const onClickCloseBtn = useCallback(() => {
     modalMutate(
@@ -41,28 +82,66 @@ const CreateRoomModal = () => {
     );
   }, []);
 
-  const onClickRequest = useCallback(() => {
-    axios
+  const onClickRequest = useCallback(async () => {
+    await axios
       .post(
         '/room',
         {
-          'userlist': [],
-          'title': roomName,
+          userlist: selectedMemberId,
+          title: roomName,
         },
         {
           headers: {
             Authorization: `${sessionStorage.getItem('TOKEN')}`,
+            'Content-Type': 'application/json',
           },
         },
       )
       .then((res) => {
         console.log(res);
+        onClickCloseBtn();
+        alert('방을 생성했습니다!');
       })
       .catch((err) => {
         console.log(err);
       });
-    return;
-  }, [roomName]);
+  }, [roomName, selectedMemberId]);
+
+  const onChangeCheckBox = useCallback(
+    (id: string) => (e: ChangeEvent<HTMLInputElement>) => {
+      if (!e.currentTarget.checked) {
+        setSelectedMemberId((prevArr) => {
+          const newArr = prevArr.filter((dataId) => dataId !== id);
+          console.log(newArr);
+          return [...newArr];
+        });
+      } else {
+        setSelectedMemberId((prevArr) => {
+          return [...prevArr, id];
+        });
+      }
+    },
+    [selectedMemberId],
+  );
+
+  const EachCheckBox = memo(({ friendData }: { friendData: IFriendData }) => {
+    return (
+      <>
+        <div>
+          <label htmlFor={friendData.id}>
+            {friendData.name}
+            <input
+              type="checkbox"
+              id={friendData.id}
+              name={friendData.name}
+              onChange={onChangeCheckBox(friendData.id)}
+              checked={selectedMemberId.includes(friendData.id) ? true : false}
+            />
+          </label>
+        </div>
+      </>
+    );
+  });
 
   return (
     <ModalBoxContainer>
@@ -80,25 +159,18 @@ const CreateRoomModal = () => {
                 <label htmlFor="room_name">방 이름</label>
                 <input type="text" id="room_name" onChange={handleRoomName} />
               </RoomName>
-              <MemeberList></MemeberList>
+              <MemeberList>
+                {friendList &&
+                  friendList.map((friend: IFriendData) => {
+                    return <EachCheckBox key={friend.id} friendData={friend} />;
+                  })}
+                <ActionBtn>
+                  <button type="button" onClick={onClickNext}>
+                    다음
+                  </button>
+                </ActionBtn>
+              </MemeberList>
             </Content>
-            {/* <label htmlFor="member1">
-                멤버1
-                <input type="checkbox" id="member1" />
-              </label>
-              <label htmlFor="member2">
-                멤버2
-                <input type="checkbox" id="member2" />
-              </label>
-              <label htmlFor="member3">
-                멤버3
-                <input type="checkbox" id="member3" />
-              </label> */}
-            <ActionBtn>
-              <button type="button" onClick={onClickNextStage}>
-                다음
-              </button>
-            </ActionBtn>
           </ContentBox>
         ) : (
           currentStage === 1 && (
@@ -110,7 +182,18 @@ const CreateRoomModal = () => {
                 </ResultRoomName>
                 <ResultMembers>
                   <label htmlFor="member_list">초대할 멤버</label>
-                  <div id="member_list"></div>
+                  <div id="member_list">
+                    {checkedMember &&
+                      checkedMember.map((friend: IFriendData) => {
+                        return (
+                          <div key={friend.id}>
+                            <label htmlFor={friend.name}>
+                              <p>{friend.name}</p>
+                            </label>
+                          </div>
+                        );
+                      })}
+                  </div>
                 </ResultMembers>
               </Content>
               <ResultActionBtn>
