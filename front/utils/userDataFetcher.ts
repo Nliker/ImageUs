f404b5e6d157890e0fcd24a1fd4b67029f5fb1a9
kanghunK <ltbehappy16@gmail.com>
2022-12-16 11:23:41 +0900

    feat: userDataFetcher 작성

---
+++ front/utils/userDataFetcher.ts
@@ -0,0 +1,59 @@
+import { IRoomData } from '@typing/db';
+import axios from 'axios';
+
+const userId = sessionStorage.getItem('USER_ID');
+
+const getUserFriendList = async (url: string) => {
+  console.log('freindlist');
+  try {
+    const response = await axios.get(`/user/${userId}/${url}`, {
+      headers: {
+        Authorization: `${sessionStorage.getItem('TOKEN')}`,
+      },
+    });
+    const { friendlist } = await response.data;
+    return friendlist;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteUserFriend = async (url: string, { arg }: {arg: string}) => {
+  try {
+    const response = await axios.delete(`/user/${userId}/friend`, {
+      data: {
+        "delete_friend_user_id": arg,
+      },
+      headers: {
+        Authorization: `${sessionStorage.getItem('TOKEN')}`
+      }
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getUserRoomListFetcher = async (url: string) => {
+  return axios
+    .get(`/user/${userId}/${url}`, {
+      headers: {
+        Authorization: `${sessionStorage.getItem('TOKEN')}`,
+      },
+    })
+    .then((res) => {
+      const { data } = res;
+      const roomList = data.roomlist.map((roomData: IRoomData) => {
+        return {
+          id: roomData.id,
+          title: roomData.title
+        };
+      });
+      return roomList;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export { getUserFriendList, deleteUserFriend, getUserRoomListFetcher };
