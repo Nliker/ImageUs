8652eeed77e85749f059f38e6530884f4155685f
kanghunK <ltbehappy16@gmail.com>
2022-12-16 17:50:42 +0900

    refactor: memo() 추가 및 swr 요청 한 번만 요청되도록 수정

--- front/components/MemberList/index.tsx
+++ front/components/MemberList/index.tsx
@@ -1,29 +1,41 @@
 import EachMember from '@components/EachMember';
-import React, { useCallback, useState } from 'react';
+import React, { memo, useCallback, useState } from 'react';
+import useSWR from 'swr';
 import { IoMdArrowDropright } from 'react-icons/io';
+import { useParams } from 'react-router-dom';
 import { Collapse, Subtitle } from './styles';
+import { getUserListFetcher } from '@utils/roomDataFetcher';
+import { IFriendData } from '@typing/db';
 
-const MemberList = () => {
+const MemberList = memo(() => {
+  // const { roomId } = useParams<{roomId?: string}>();
+  // console.log(roomId);
+  const { data: roomId } = useSWR('roomId');
+  const { data: userlist } = useSWR(['userlist', roomId], getUserListFetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  } );
   const [memberCollapse, setMemberCollapse] = useState<boolean>(false);
   // 임시 데이터
-  const memberData = [
-    {
-      id: 1,
-      name: '멤버1',
-    },
-    {
-      id: 2,
-      name: '멤버2',
-    },
-    {
-      id: 3,
-      name: '멤버3',
-    },
-    {
-      id: 4,
-      name: '멤버4',
-    },
-  ];
+  // const memberData = [
+  //   {
+  //     id: 1,
+  //     name: '멤버1',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: '멤버2',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: '멤버3',
+  //   },
+  //   {
+  //     id: 4,
+  //     name: '멤버4',
+  //   },
+  // ];
 
   const toggleMemberCollapse = useCallback(() => setMemberCollapse((prev) => !prev), []);
   return (
@@ -36,12 +48,12 @@ const MemberList = () => {
       </Subtitle>
       <div>
         {memberCollapse &&
-          memberData?.map((member) => {
+          userlist?.map((member: IFriendData) => {
             return <EachMember key={member.id} member={member} />;
           })}
       </div>
     </>
   );
-};
+});
 
 export default MemberList;
