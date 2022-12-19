a1d86920fd5a468f4b9b21a65f51f741d9600060
kanghunK <ltbehappy16@gmail.com>
2022-12-19 22:11:46 +0900

    refactor: 더미 데이터 주석 처림 및 이미지 리스트 props로 받도록 수정

--- front/components/MyPictures/index.tsx
+++ front/components/MyPictures/index.tsx
@@ -1,24 +1,25 @@
+import { IImageData } from '@typing/db';
 import React from 'react';
 import { Wrapper } from './styles';
 
-const dummyImages = [
-  { id: 1, name: 'first_image', url: 'image_test.png' },
-  { id: 2, name: 'second_image', url: 'image_test2.jpg' },
-  { id: 3, name: 'third_image', url: 'image_test3.jpg' },
-  { id: 4, name: 'first_image', url: 'image_test.png' },
-  { id: 5, name: 'second_image', url: 'image_test2.jpg' },
-  { id: 6, name: 'third_image', url: 'image_test3.jpg' },
-];
+// const dummyImages = [
+//   { id: 1, name: 'first_image', url: 'image_test.png' },
+//   { id: 2, name: 'second_image', url: 'image_test2.jpg' },
+//   { id: 3, name: 'third_image', url: 'image_test3.jpg' },
+//   { id: 4, name: 'first_image', url: 'image_test.png' },
+//   { id: 5, name: 'second_image', url: 'image_test2.jpg' },
+//   { id: 6, name: 'third_image', url: 'image_test3.jpg' },
+// ];
 
 // Route를 활용해서 만든 컴포넌트
-const MyPictures = () => {
+const MyPictures = ({ imageList }: {imageList?: Array<IImageData>}) => {
   return (
     <Wrapper>
       <ul>
-        {dummyImages.map((image) => (
+        {imageList && imageList.map((image) => (
           <li key={image.id}>
             <div>
-              <img src={image.url} alt={image.name} />
+              <img src={image.link} alt={`테스트 요청 alt`} />
             </div>
           </li>
         ))}
