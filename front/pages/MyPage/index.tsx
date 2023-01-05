9bc838ec7bcdb454ecdb417ca70226c4bc7cb4bd
kanghunK <ltbehappy16@gmail.com>
2023-01-05 20:40:12 +0900

    Modify: MyPictures의 imagList Props의 변수 이름 변경

--- front/pages/MyPage/index.tsx
+++ front/pages/MyPage/index.tsx
@@ -1,12 +1,14 @@
 import MypageInfo from '@components/MypageInfo';
 import MyPictures from '@components/MyPictures';
 import AppLayout from '@layouts/AppLayout';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useSWR from 'swr';
+import useSWRMutation from 'swr/mutation';
+
 import { NavLink } from 'react-router-dom';
 import { Routes, Route } from 'react-router';
 import { ContentBox, EachRoomPictureList, ProfileBox, ProfileImage, ProfileInfo, SubMenu, WrapperBox } from './styles';
-import { getUserFriendList, getUserImageList, getUserRoomListFetcher } from '@utils/userDataFetcher';
+import { getImageData, getUserFriendList, getUserImageList, getUserRoomListFetcher } from '@utils/userDataFetcher';
 import { IImageData } from '@typing/db';
 import { userImageLoadNumber } from '@hooks/swrStore';
 
@@ -28,11 +30,17 @@ const MyPage = () => {
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   });
+  const { data: imageDataList, trigger } = useSWRMutation('/image-download', getImageData);
 
   // 스크롤이 일어날 때 imageList 요청을 보내서 다음 사진을 받고
   // mutate(key, start + 개수 + 1, true)로 이미지 로드 시작번호를 업데이트 한다.
 
   console.log(imageList);
+
+  useEffect(() => {
+    if (!imageList) return;
+    trigger(imageList);
+  }, [imageList]);
 
   return (
     <AppLayout>
@@ -77,7 +85,7 @@ const MyPage = () => {
             <div>
               {/* 여기를 Route를 활용해서 만든다. */}
               <Routes>
-                <Route path="/" element={<MyPictures imageList={imageList} />} />
+                <Route path="/" element={<MyPictures imageList={imageDataList} />} />
                 <Route path="my_profile" element={<MypageInfo />} />
               </Routes>
             </div>
