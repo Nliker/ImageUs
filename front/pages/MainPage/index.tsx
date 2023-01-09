805364ee3d8bcd33d144dacbb05f3db3df19365c
kanghunK <ltbehappy16@gmail.com>
2023-01-09 16:32:46 +0900

    Add: 메인페이지 내용 문구 추가

--- front/pages/MainPage/index.tsx
+++ front/pages/MainPage/index.tsx
@@ -9,40 +9,44 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { useLocation } from 'react-router';
 import { Route, Routes, useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
 import useSWR, { mutate } from 'swr';
-import { ContentWrappper, Wrappper } from './styles';
+import { ContentWrappper, MainIntroduction, Wrappper } from './styles';
 
-const MainPage = () => {
+interface MainPageProps {
+  isLogIn?: boolean;
+}
+
+const MainPage = ({ isLogIn }: MainPageProps) => {
   const { roomId } = useParams<{ roomId: string | undefined }>();
   const [showSideBar, setshowSideBar] = useState<boolean>(false);
-
-  // useEffect(() => {
-  //   // if (!roomId) return;
-  //   console.log('메인페이지', roomId);
-  //   mutate('roomId', roomId, true);
-  // }, [roomId]);
 
   const handleRoomListBtn = useCallback(() => {
     setshowSideBar((prev) => !prev);
   }, [showSideBar]);
 
   console.log(roomId, '메인페이지');
-  // const location = useLocation();
-  // useEffect(() => {
-  //   console.log(location);
-  // }, [ location ])
 
   return (
     <AppLayout roomId={roomId}>
-      <ToolBar handleRoomListBtn={handleRoomListBtn} />
-      <SideBar show={showSideBar} roomId={roomId} />
+      <ToolBar handleRoomListBtn={handleRoomListBtn} isLogIn={isLogIn} />
+      {/* {roomId && <SideBar show={showSideBar} roomId={roomId} />} */}
       <Wrappper>
         <ContentWrappper>
-          <ContentSection roomId={roomId} />
-          {/* <Routes>
-            <Route index element={<ContentSection />} />
-            <Route path=":roomId" element={<ContentSection />} />
-          </Routes> */}
+          <main>
+            <MainIntroduction>
+              <header>
+                <h1>
+                  지인들과의 사진을 <span className="brand_logo">ImageUS</span>와
+                  <br /> 공유하고 간직하세요!
+                </h1>
+              </header>
+            </MainIntroduction>
+          </main>
+          {/* {roomId ? (
+            <ContentSection roomId={roomId} />
+          ) : (
+          )} */}
         </ContentWrappper>
       </Wrappper>
     </AppLayout>
