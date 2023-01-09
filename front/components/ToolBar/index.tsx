fb4a66d3bc08d276c70be6e7736a2d46d3abd194
kanghunK <ltbehappy16@gmail.com>
2023-01-09 16:31:01 +0900

    Feat: 로그아웃 모달 박스 기능 추가

--- front/components/ToolBar/index.tsx
+++ front/components/ToolBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { LeftIcon, LogoutBtn, RightIcons, UserBox, UserInfo, Wrapper } from './styles';
 import { MdOutlineSpaceDashboard } from 'react-icons/md';
 import { useMediaQuery } from 'react-responsive';
@@ -7,59 +7,91 @@ import { BiUserCircle } from 'react-icons/bi';
 import { NavLink, useNavigate } from 'react-router-dom';
 import useSWR, { mutate } from 'swr';
 import logInFetcher from '@utils/logInFetcher';
+import { Link } from 'react-router-dom';
+import { Node } from 'typescript';
 
 interface Props {
   handleRoomListBtn: (e: any) => void;
+  isLogIn?: boolean;
+  roomId?: string;
 }
 
-const ToolBar = ({ handleRoomListBtn }: Props) => {
+const ToolBar = ({ handleRoomListBtn, isLogIn, roomId }: Props) => {
   const isMobile = useMediaQuery({ maxWidth: 1023 });
-  const [showUserBox, setShowUserBox] = useState(false);
+  const [showUserBox, setShowUserBox] = useState<boolean | null>(null);
+  const el = useRef<HTMLDivElement>(null);
   const navigate = useNavigate();
 
-  const handleUserBox = useCallback(() => setShowUserBox((prev) => !prev), []);
+  useEffect(() => {
+    window.addEventListener('click', handleCloseUserBox);
+    return () => {
+      window.removeEventListener('click', handleCloseUserBox);
+    };
+  }, [showUserBox]);
+
+  const handleClickUserBox = useCallback(() => setShowUserBox(true), [showUserBox]);
+
+  const handleCloseUserBox = useCallback(
+    (e: MouseEvent) => {
+      if (e.target instanceof HTMLElement && showUserBox && !el.current?.contains(e.target)) setShowUserBox(false);
+    },
+    [showUserBox],
+  );
 
   const onClickLogOut = useCallback(() => {
     sessionStorage.clear();
     mutate('/user/my');
     navigate('/');
   }, []);
-  
+
   return (
     <Wrapper>
-      <LeftIcon className='toolbar_icon'>
-        {/* <SideBarButton>{isMobile && <MdOutlineSpaceDashboard onClick={handleRoomListBtn} />}</SideBarButton> */}
-        <span>
-          <MdOutlineSpaceDashboard onClick={handleRoomListBtn} />
-        </span>
-      </LeftIcon>
-      <RightIcons className='toolbar_icon'>
-        <div>
-          <span>
-            <RiListSettingsLine />
-          </span>
+      {isLogIn ? (
+        <>
+          <RightIcons className="toolbar_icon">
+            {/* <div>
+              <span>
+                <RiListSettingsLine />
+              </span>
+            </div> */}
+            <div>
+              <span onClick={handleClickUserBox}>
+                <BiUserCircle />
+              </span>
+            </div>
+          </RightIcons>
+          {roomId && (
+            <LeftIcon className="toolbar_icon">
+              <span>
+                <MdOutlineSpaceDashboard onClick={handleRoomListBtn} />
+              </span>
+            </LeftIcon>
+          )}
+        </>
+      ) : (
+        <div className="action_box">
+          <Link to={'/login'}>로그인</Link>
+          <Link to={'/signup'}>회원가입</Link>
         </div>
-        <div>
-          <span onClick={handleUserBox}>
-            <BiUserCircle />
-          </span>
-        </div>
-      </RightIcons>
-      {showUserBox && <UserBox>
-        <UserInfo>
-          <img src="image_test.png" alt="test_img" />
-          <div className={'info_words'}>
-            <p>
-              <span>이름</span>님 어서오세요!
-            </p>
-            {/* <p>email: </p> */}
-            <p>asdfaskla@naver.com</p>
-          </div>
-        </UserInfo>
-        <LogoutBtn>
-          <span onClick={onClickLogOut}>로그아웃</span>
-        </LogoutBtn>
-      </UserBox>}
+      )}
+
+      {/* 로그아웃 상자 토글 버튼 */}
+      {showUserBox && (
+        <UserBox ref={el}>
+          <UserInfo>
+            {/* <img src="image_test.png" alt="test_img" /> */}
+            <div className={'info_words'}>
+              <p>
+                <span>이름</span>님 어서오세요!
+              </p>
+              <p>asdfaskla@naver.com</p>
+            </div>
+          </UserInfo>
+          <LogoutBtn>
+            <span onClick={onClickLogOut}>로그아웃</span>
+          </LogoutBtn>
+        </UserBox>
+      )}
     </Wrapper>
   );
 };
