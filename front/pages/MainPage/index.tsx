842e7c5f98f74731056c4412e318c44752dcc3b0
kanghunK <ltbehappy16@gmail.com>
2023-01-11 16:57:51 +0900

    Modify: 메인 페이지 로그인 유무에 따른 화면 전환되도록 수정, css 수정

--- front/pages/MainPage/index.tsx
+++ front/pages/MainPage/index.tsx
@@ -4,44 +4,74 @@ import CreateRoomModal from '@components/CreateRoomModal';
 import SideBar from '@components/SideBar';
 import ToolBar from '@components/ToolBar';
 import AppLayout from '@layouts/AppLayout';
-import logInFetcher from '@utils/logInFetcher';
+import { IRoomData } from '@typing/db';
+import { logInCheckFetcher } from '@utils/logInFetcher';
+import { getUserRoomListFetcher } from '@utils/userDataFetcher';
 import React, { useCallback, useEffect, useState } from 'react';
-import { useParams } from 'react-router';
 import { useLocation } from 'react-router';
-import { Route, Routes, useNavigate } from 'react-router';
+import { Route, Routes, useNavigate, useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import useSWR, { mutate } from 'swr';
-import { ContentWrappper, MainIntroduction, Wrappper } from './styles';
+import useSWRMutation from 'swr/mutation';
+import { ContentWrappper, MainIntroduction, MainRoomList, Wrappper } from './styles';
 
-interface MainPageProps {
-  isLogIn?: boolean;
-}
+const MainPage = () => {
+  const { data: isLogIn } = useSWR('/user/my');
+  const { data: roomlist, trigger, isMutating } = useSWRMutation('roomlist', getUserRoomListFetcher);
+  const navigate = useNavigate();
+  // const { data: roomlist, isLoading } = useSWR('roomlist', getUserRoomListFetcher, {
+  //   revalidateIfStale: false,
+  //   revalidateOnFocus: false,
+  //   revalidateOnReconnect: false,
+  // });
+  // const { data: room}
 
-const MainPage = ({ isLogIn }: MainPageProps) => {
-  const { roomId } = useParams<{ roomId: string | undefined }>();
-  const [showSideBar, setshowSideBar] = useState<boolean>(false);
+  // const { roomId } = useParams<{ roomId: string | undefined }>();
+  // const [showSideBar, setshowSideBar] = useState<boolean>(false);
+  // const handleRoomListBtn = useCallback(() => {
+  //   setshowSideBar((prev) => !prev);
+  // }, [showSideBar]);
 
-  const handleRoomListBtn = useCallback(() => {
-    setshowSideBar((prev) => !prev);
-  }, [showSideBar]);
+  // const onClickRoomList = useCallback(() => {}, [roomlist]);
 
-  console.log(roomId, '메인페이지');
+  useEffect(() => {
+    if (!isLogIn) return;
+    trigger();
+  }, [isLogIn]);
+
+  console.log('메인페이지', '로그인 유무:', isLogIn);
 
   return (
-    <AppLayout roomId={roomId}>
-      <ToolBar handleRoomListBtn={handleRoomListBtn} isLogIn={isLogIn} />
+    <AppLayout>
+      <ToolBar />
       {/* {roomId && <SideBar show={showSideBar} roomId={roomId} />} */}
       <Wrappper>
         <ContentWrappper>
           <main>
-            <MainIntroduction>
-              <header>
-                <h1>
-                  지인들과의 사진을 <span className="brand_logo">ImageUS</span>와
-                  <br /> 공유하고 간직하세요!
-                </h1>
-              </header>
-            </MainIntroduction>
+            {!isLogIn ? (
+              <MainIntroduction>
+                <header>
+                  <h1>
+                    지인들과의 사진을 <span className="brand_logo">ImageUS</span>와
+                    <br /> 공유하고 간직하세요!
+                  </h1>
+                </header>
+              </MainIntroduction>
+            ) : (
+              <section>
+                <MainRoomList>
+                  <strong>방에 입장하기</strong>
+                  <ul>
+                    {!isMutating &&
+                      roomlist?.map((roomData: IRoomData) => (
+                        <li key={roomData.id}>
+                          <Link to={`/booth/${roomData.id}`}>{roomData.title}</Link>
+                        </li>
+                      ))}
+                  </ul>
+                </MainRoomList>
+              </section>
+            )}
           </main>
           {/* {roomId ? (
             <ContentSection roomId={roomId} />
