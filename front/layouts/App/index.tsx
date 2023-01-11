a27afa455f63359aa1e976f7d23a894e8532c0bf
kanghunK <ltbehappy16@gmail.com>
2023-01-11 16:54:31 +0900

    Add, Modify: ImageRoom 페이지 라우팅 추가 및 Fetcher 네이밍수정

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -2,17 +2,18 @@ import React, { useEffect } from 'react';
 import loadable from '@loadable/component';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import useSWR from 'swr';
-import logInFetcher from '@utils/logInFetcher';
+import { logInCheckFetcher } from '@utils/logInFetcher';
 
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 const MainPage = loadable(() => import('@pages/MainPage'));
 const MyPage = loadable(() => import('@pages/MyPage'));
 const PeopleManagement = loadable(() => import('@pages/PeopleManagement'));
+const ImageRoom = loadable(() => import('@pages/ImageRoom'));
 
 const App = () => {
   // 한 번만 요청하도록 옵션 추가
-  const { data: isLogIn, isValidating } = useSWR('/user/my', logInFetcher, {
+  const { data: isLogIn, isValidating } = useSWR('/user/my', logInCheckFetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
@@ -24,14 +25,17 @@ const App = () => {
     <div>로딩중...</div>
   ) : (
     <Routes>
-      <Route path="/" element={<Navigate to="/main_page" />} />
-      <Route path="/main_page" element={<MainPage isLogIn={isLogIn} />} />
-      <Route path="/main_page/:roomId" element={<MainPage isLogIn={isLogIn} />} />
+      <Route path="/main_page" element={<MainPage />} />
+      {/* <Route path="/" element={<Navigate to="/main_page" />} /> */}
+      {/* <Route path="/main_page/:roomId" element={<MainPage isLogIn={isLogIn} />} /> */}
       {isLogIn ? (
         <>
           {/* 이하 비회원 접근 불가 페이지 */}
           {/* <Route path="/main_page/:roomId" element={<MainPage />} />
           <Route path="/main_page/*" element={<MainPage />} /> */}
+
+          {/* 웹팩 프록시를 사용함으로 image가 들어간 네이밍을 path로 넣으면 안된다! */}
+          <Route path="/booth/:roomId" element={<ImageRoom />} />
           <Route path="/my_page/*" element={<MyPage />} />
           <Route path="/people_management/*" element={<PeopleManagement />} />
         </>
@@ -43,7 +47,7 @@ const App = () => {
         </>
       )}
       {/* 추후에 404페이지 작성용 Route */}
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="*" element={<Navigate to="/main_page" />} />
     </Routes>
   );
 };
