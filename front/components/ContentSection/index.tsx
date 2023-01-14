e4ed74d3287c5f2041158c16308b9eece1e2d086
kanghunK <ltbehappy16@gmail.com>
2023-01-14 15:06:43 +0900

    Feat: 스크롤 시 이미지 데이터 요청하도록 intersectionObserver 추가

--- front/components/ContentSection/index.tsx
+++ front/components/ContentSection/index.tsx
@@ -1,130 +1,175 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { forwardRef, memo, MutableRefObject, useCallback, useEffect, useState } from 'react';
 import { IImageData } from '@typing/db';
-import { getImageData, getRoomImageListFetcher } from '@utils/roomDataFetcher';
+import { getImageData, getMarkerFetcher, getRoomImageListFetcher } from '@utils/roomDataFetcher';
 import { Link, useParams } from 'react-router-dom';
-import useSWR from 'swr';
+import useSWR, { mutate } from 'swr';
 import useSWRMutation from 'swr/mutation';
-import { ContentBox, ImageCard, ImageInfo, InfoItem, MainContainer, Wrapper } from './styles';
+import { ContentBox, ImageCard, ImageInfo, InfoItem, MainContainer, PostImage, Wrapper } from './styles';
 import { imageLoadNumber } from '@hooks/swrStore';
 import { AxiosResponse } from 'axios';
+import useIntersect from '@hooks/useIntersect';
+import Scrollbars from 'react-custom-scrollbars';
+import ImageContentList from '@components/ImageContentList';
 
-/*
-    grid로 격자 형식으로 각 div에 너비와 높이를 똑같이 지정해서 보여준다.
-    각 row마다 div로 감싸져서 전체 이미지 데이터를 받아온 후 가공해서 렌더링한다.
-    전체 이미지는 60개, 80개 등으로 받아온 후에 스크롤이 내려가면 다시 백엔드에 요청을 보내는
-    인피니티 스크롤링을 고려한다.
-*/
+interface ImageData {
+  id: number;
+  imageUrl: string;
+  create_date: string;
+}
 
 interface Props {
-  onShowModal: (e: any) => void;
+  roomId?: string;
 }
 
-interface ImageType {
-  id: number;
-  imageUrl: string | null;
-}
+const ContentSection = forwardRef<Scrollbars, Props>(({ roomId }, scrollRef) => {
+  const [readStartNumber, setReadStartNumber] = useState(0);
+  const [allImageData, setAllImageData] = useState<Array<ImageData>>([]);
+  const [scrollHeight, setScrollHeight] = useState();
 
-interface b64Image {
-  id: number;
-  imageData: string | null;
-}
-
-const ContentSection = memo(({ roomId }: { roomId: string | undefined }) => {
-  // const { roomId } = useParams<{ roomId?: string }>();
-  // const { data: roomId } = useSWR('roomId');
-  const { start } = imageLoadNumber();
   const {
     data: imageList,
-    mutate: imageListTrigger,
-    isLoading,
-  } = useSWR(['imagelist', roomId, start], getRoomImageListFetcher, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
+    trigger: imageListTrigger,
+    isMutating: imageListLoading,
+  } = useSWRMutation(`/room/${roomId}/imagelist`, getRoomImageListFetcher);
+  const {
+    data: imageDataList,
+    trigger: imageDataTrigger,
+    isMutating: imageDataLoading,
+  } = useSWRMutation('/image-download', getImageData);
+  const { data: markerNumber, trigger: markerTrigger } = useSWRMutation(`/room/${roomId}/marker`, getMarkerFetcher);
+
+  const observerRef = useIntersect(async (entry, observer) => {
+    observer.unobserve(entry.target);
+
+    // 데이터 fetching 중이 아니고 다음 로드할 데이터가 남아있다면 데이터를 부른다.(imageList 요청)
+    if (!imageListLoading && !imageDataLoading && imageList?.imagelist.length === 12) {
+      console.log('인터섹션 데이터 패칭 요청');
+      imageListTrigger({ roomId, start: readStartNumber });
+    }
   });
-  const { data: imageDataList, trigger } = useSWRMutation('/image-download', getImageData);
+
+  // 처음 마운트 됐을 때
+  useEffect(() => {
+    console.log('컨텐트 섹션 처음 마운트 룸아이디', roomId);
+    imageListTrigger({ roomId, start: readStartNumber });
+    markerTrigger();
+  }, [roomId]);
 
   // 스크롤이 일어날 때 imageList 요청을 보내서 다음 사진을 받고
-  // mutate(key, start + 개수 + 1, true)로 이미지 로드 시작번호를 업데이트 한다.
-
+  // 이미지 로드 시작번호를 업데이트 한다.
   useEffect(() => {
     if (!imageList) return;
-    trigger(imageList);
+    setReadStartNumber((prev) => prev + 12);
+    imageDataTrigger(imageList);
   }, [imageList]);
 
+  // 전달받은 이미지 정보를 allImageData에 업데이트
+  useEffect(() => {
+    if (!imageDataList) return;
+    setAllImageData((prev) => [...prev, ...imageDataList.imgData]);
+  }, [imageDataList]);
+
+  // useEffect(() => {
+  //   //  패칭 후에 스크롤을 현재 위치로 유지시킨다.
+  //   const current = (scrollRef as MutableRefObject<Scrollbars>)?.current;
+  //   console.log(current.getScrollHeight());
+  //   if (current) {
+  //     current.scrollTop(current.getScrollHeight());
+  //   }
+  // }, [allImageData]);
+
+  // const handleIntersectTargetRef = useCallback(() => {
+
+  // }, []);
   return (
     <Wrapper>
-      <MainContainer>
-        {isLoading ? (
-          <div>로딩중입니다..</div>
-        ) : (
+      <Scrollbars ref={scrollRef}>
+        <MainContainer>
           <div>
-            <div>
-              <span>최신 업로드 이미지</span>
-            </div>
-            {imageDataList?.nextImgData.map((data) => {
-              // console.log(data);
-              return (
-                <ContentBox key={data.id}>
+            {/* {imageDataList?.nextImgData.length !== 0 && (
+                <div>
                   <div>
-                    <Link to="#">
-                      <ImageCard>
-                        <img src={data.imageUrl} />
-                      </ImageCard>
-                      <ImageInfo>
-                        <InfoItem>
-                          <span>이미지 1</span>
-                        </InfoItem>
-                        <InfoItem>
-                          <span>{data.create_date}</span>
-                        </InfoItem>
-                      </ImageInfo>
-                    </Link>
+                    <span>최신 업로드 이미지</span>
                   </div>
-                </ContentBox>
-              );
-            })}
-            <div>
-              <span>게시된 이미지</span>
-            </div>
-            {imageDataList?.prevImgData.map((data) => {
-              // console.log(data);
-              return (
-                <ContentBox key={data.id}>
                   <div>
-                    <Link to="#">
-                      <ImageCard>
-                        <img src={data.imageUrl} />
-                      </ImageCard>
-                      <ImageInfo>
-                        <InfoItem>
-                          <span>이미지 1</span>
-                        </InfoItem>
-                        <InfoItem>
-                          <span>{data.create_date}</span>
-                        </InfoItem>
-                      </ImageInfo>
-                    </Link>
+                    {imageDataList?.nextImgData.map((data) => {
+                      return (
+                        <ContentBox key={data.id}>
+                          <div>
+                            <Link to="#">
+                              <ImageCard>
+                                <img src={data.imageUrl} />
+                              </ImageCard>
+                              <ImageInfo>
+                                <InfoItem>
+                                  <span>이미지 1</span>
+                                </InfoItem>
+                                <InfoItem>
+                                  <span>{data.create_date}</span>
+                                </InfoItem>
+                              </ImageInfo>
+                            </Link>
+                          </div>
+                        </ContentBox>
+                      );
+                    })}
                   </div>
-                </ContentBox>
-              );
-            })}
+                </div>
+              )} */}
             <div>
-              <span>삭제된 이미지 목록</span>
+              <div>
+                <span>게시된 이미지</span>
+              </div>
+              <PostImage>
+                {imageDataList?.imgData.length === 0 ? (
+                  <div>
+                    <span>게시된 이미지가 없습니다.</span>
+                  </div>
+                ) : (
+                  <ImageContentList allImageData={allImageData} observerRef={observerRef} />
+                )}
+              </PostImage>
             </div>
-            {imageDataList?.deleteImgData.map((data, i) => {
-              // console.log(data);
-              return (
-                <ContentBox key={data.id}>
-                  <div>{i}번째 이미지는 삭제되었습니다..</div>
-                </ContentBox>
-              );
-            })}
+            {/* allImageData.map((data, index, thisArr) => {
+                return (
+                  <ContentBox key={data.id} ref={thisArr.length - 1 === index ? observerRef : undefined}>
+                    <div>
+                      <Link to="#">
+                        <ImageCard>
+                          <img src={data.imageUrl} />
+                        </ImageCard>
+                        <ImageInfo>
+                          <InfoItem>
+                            <span>이미지 1</span>
+                          </InfoItem>
+                          <InfoItem>
+                            <span>{data.create_date}</span>
+                          </InfoItem>
+                        </ImageInfo>
+                      </Link>
+                    </div>
+                  </ContentBox>
+                );
+              }) */}
+            {/* <div>
+                <div>
+                  <span>삭제된 이미지 목록</span>
+                </div>
+                <div>
+                  {imageDataList?.deleteImgData.map((data, i) => {
+                    // console.log(data);
+                    return (
+                      <ContentBox key={data.id}>
+                        <div>{i}번째 이미지는 삭제되었습니다..</div>
+                      </ContentBox>
+                    );
+                  })}
+                </div>
+              </div> */}
           </div>
-        )}
-      </MainContainer>
+        </MainContainer>
+      </Scrollbars>
     </Wrapper>
   );
 });
-
 export default ContentSection;
