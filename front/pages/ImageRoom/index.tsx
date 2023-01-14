3648a66de4320628be8ec809f48f3739453c5309
kanghunK <ltbehappy16@gmail.com>
2023-01-14 15:14:15 +0900

    Add: 스크롤바 Ref 추가 및 key 추가로 방 이동 시 리마운트 발생

--- front/pages/ImageRoom/index.tsx
+++ front/pages/ImageRoom/index.tsx
@@ -2,12 +2,15 @@ import ContentSection from '@components/ContentSection';
 import SideBar from '@components/SideBar';
 import ToolBar from '@components/ToolBar';
 import AppLayout from '@layouts/AppLayout';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import Scrollbars from 'react-custom-scrollbars';
 import { useParams } from 'react-router';
 
 const ImageRoom = () => {
   const { roomId } = useParams<{ roomId: string }>();
   const [showSideBar, setshowSideBar] = useState<boolean>(false);
+  const scrollbarRef = useRef<Scrollbars>(null);
+
   const toggleSidebar = useCallback(() => {
     setshowSideBar((prev) => !prev);
   }, [showSideBar]);
@@ -16,11 +19,14 @@ const ImageRoom = () => {
     setshowSideBar(false);
   }, [showSideBar]);
 
+  console.log('룸 룸아이디:', roomId);
+
   return (
     <AppLayout>
       <ToolBar handleSidebar={toggleSidebar} />
       <SideBar show={showSideBar} roomId={roomId} close={closeSidebar} />
-      <ContentSection roomId={roomId} />
+      {/* 키를 줌으로서 룸아이디에 따라서 컴포넌트를 unmount 시키고 remount 시킬 수 있다. */}
+      <ContentSection key={roomId} roomId={roomId} ref={scrollbarRef} />
     </AppLayout>
   );
 };
