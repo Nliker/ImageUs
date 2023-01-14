1b66cf69c258bd5b502dacfb7c38250416728e34
kanghunK <ltbehappy16@gmail.com>
2023-01-14 15:13:12 +0900

    Feat: getMarkerFetcher 추가 및 주석 제거

--- front/utils/roomDataFetcher.ts
+++ front/utils/roomDataFetcher.ts
@@ -1,58 +1,22 @@
 import { IImageData } from '@typing/db';
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
-const getRoomImageListFetcher = async (arg: Array<string | undefined>) => {
-  const token = sessionStorage.getItem('TOKEN');
-  const [url, roomId, start] = arg;
-  if (!roomId) return null;
-  const loadNumber = 10;
-
+const getRoomImageListFetcher = async (url: string, { arg }: { arg: { roomId?: string; start: number } }) => {
   // 이미지를 10개씩 끊어서 불러오고 불러올 이미지의 시작 위치는 마지막 번호 + 1를 저장해서 사용한다.
   try {
-    const imageInfoResponse = await axios.get(`/room/${roomId}/${url}?start=${start}&limit=${loadNumber}`, {
+    const token = sessionStorage.getItem('TOKEN');
+    const { roomId, start } = arg;
+
+    if (!roomId) return null;
+    const loadNumber = 12;
+    const imageInfoResponse = await axios.get(`${url}?start=${start}&limit=${loadNumber}`, {
       headers: {
         Authorization: token,
       },
     });
 
-    const { imagelist, read_history_row } = imageInfoResponse.data;
-    return { imagelist, read_history_row };
-    // 다음 서버에서 불러올 이미지 시작번호 업데이트
-    // mutate(start + loadNumber + 1, false);
-
-    // console.log(imageInfoResponse);
-    // const imagelist = [...imageInfoResponse.data.imagelist];
-    // console.log(imagelist);
-
-    // const imageDataList: Array<{ id: number; imageData: AxiosResponse<object> | null }> = [];
-    // const imageDataList = await Promise.all(
-    //   imageInfoList.map(async (data: IImageData) => {
-    //     const imageData = data.link
-    //       ? await axios.get(`/image-download/${data.link}`, {
-    //           headers: {
-    //             Authorization: `${sessionStorage.getItem('TOKEN')}`,
-    //           },
-    //         })
-    //       : null;
-    //     const newObj = {
-    //       id: data.id,
-    //       imageData,
-    //     };
-    //     return newObj;
-    //     // imageDataList.push(newObj);
-    //   }),
-    // );
-    // console.log(imageDataList);
-    // return imageDataList;
-
-    // const testData = await axios.get(`/image-download/${imageInfoResponse.data.imagelist[1].link}`, {
-    //   headers: {
-    //     Authorization: `${sessionStorage.getItem('TOKEN')}`,
-    //   },
-    // });
-    // const testObj = [{ id: 5, imageData: 'test' }, testData];
-    // console.log(testObj);
-    // return testObj;
+    const { imagelist } = imageInfoResponse.data;
+    return { imagelist };
   } catch (error) {
     console.log(error);
   }
@@ -65,13 +29,11 @@ const getImageData = async (
   }: {
     arg: {
       imagelist: Array<{ created_at: string; id: number; link: string; user_id: number }>;
-      read_history_row: number;
     };
   },
 ) => {
   try {
-    const prevImgData: Array<{ id: number; imageUrl: string; create_date: string }> = [];
-    const nextImgData: Array<{ id: number; imageUrl: string; create_date: string }> = [];
+    const imgData: Array<{ id: number; imageUrl: string; create_date: string }> = [];
     const deleteImgData: Array<{ id: number }> = [];
 
     for (const imageData of arg.imagelist) {
@@ -87,30 +49,13 @@ const getImageData = async (
 
         const create_date = imageData.created_at.split(' ')[0];
         const url = window.URL.createObjectURL(new Blob([res.data], { type: res.headers['content-type'] }));
-        prevImgData.push({ id: imageData.id, imageUrl: url, create_date: create_date });
+        imgData.push({ id: imageData.id, imageUrl: url, create_date: create_date });
       }
     }
-    // const imageDataList = await Promise.all(
-    //   arg.map(async (imageData) => {
-    //     if (!imageData.link) return { id: imageData.id, imageData: null };
-    //     const res = await axios.get(`/image-download/${imageData.link}`, {
-    //       headers: {
-    //         Authorization: `${sessionStorage.getItem('TOKEN')}`,
-    //       },
-    //       responseType: 'blob',
-    //     });
-
-    //     const url = window.URL.createObjectURL(new Blob([res.data], { type: res.headers['content-type'] }));
-    //     console.log(url);
-    //     return { id: imageData.id, imageUrl: url };
-    //   }),
-    // );
     const imageDataList = {
-      prevImgData: [...prevImgData],
-      nextImgData: [...nextImgData],
+      imgData: [...imgData],
       deleteImgData: [...deleteImgData],
     };
-    // console.log(imageDataList);
     return imageDataList;
   } catch (error) {
     console.log(error);
@@ -159,4 +104,21 @@ const inviteFriendFetcher = async (
   }
 };
 
-export { getRoomImageListFetcher, getUserListFetcher, inviteFriendFetcher, getImageData };
+const getMarkerFetcher = async (url: string) => {
+  try {
+    const token = sessionStorage.getItem('TOKEN');
+    const response = await axios.get(url, {
+      headers: { Authorization: token },
+    });
+
+    // console.log(response);
+    return response.data;
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      alert(err.response?.data.message);
+    }
+    return false;
+  }
+};
+
+export { getRoomImageListFetcher, getUserListFetcher, inviteFriendFetcher, getImageData, getMarkerFetcher };
