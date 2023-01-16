2c36b23ec1dca90176563315f869b99cd8746c18
kanghunK <ltbehappy16@gmail.com>
2023-01-16 14:57:41 +0900

    Add: client.tsx에 axios baseURL 속성 추가

--- front/client.tsx
+++ front/client.tsx
@@ -7,16 +7,17 @@ import SWRDevtools from '@jjordy/swr-devtools';
 import App from '@layouts/App';
 
 axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3090';
 
 render(
-    <BrowserRouter>
-        {process.env.NODE_ENV === 'production' ? (
-            <App />
-        ) : (
-            <SWRDevtools>
-                <App />
-            </SWRDevtools>
-        )}
-    </BrowserRouter>,
-    document.querySelector('#app')
-)
+  <BrowserRouter>
+    {process.env.NODE_ENV === 'production' ? (
+      <App />
+    ) : (
+      <SWRDevtools>
+        <App />
+      </SWRDevtools>
+    )}
+  </BrowserRouter>,
+  document.querySelector('#app'),
+);
