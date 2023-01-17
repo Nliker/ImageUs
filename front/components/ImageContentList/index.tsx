cc4e6d25a7c82b821e9209c64d1df82cc6c48ac8
kanghunK <ltbehappy16@gmail.com>
2023-01-17 23:02:05 +0900

    Add: ImageData에 name 속성 추가

--- front/components/ImageContentList/index.tsx
+++ front/components/ImageContentList/index.tsx
@@ -4,19 +4,20 @@ import React, { memo } from 'react';
 interface ImageData {
   id: number;
   imageUrl: string;
-  create_date: string;
+  create_date: string | null;
+  name: string | null;
 }
 
 interface Props {
-  allImageData: Array<ImageData>;
-  observerRef: React.MutableRefObject<null>;
+  ImageData: Array<ImageData>;
+  observerRef?: React.MutableRefObject<null>;
 }
 
-const ImageContentList = ({ allImageData, observerRef }: Props) => {
+const ImageContentList = ({ ImageData, observerRef }: Props) => {
   const imageCard = (data: ImageData, index: number, thisArr: ImageData[]) => (
     <ImageContentBox key={data.id} data={data} index={index} thisArr={thisArr} observerRef={observerRef} />
   );
-  return <>{allImageData.map(imageCard)}</>;
+  return <>{ImageData.map(imageCard)}</>;
 };
 
 export default memo(ImageContentList);
