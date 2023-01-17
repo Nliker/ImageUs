c02157dee5cd8eaa464ff3c2fa749909342c4eb0
kanghunK <ltbehappy16@gmail.com>
2023-01-17 23:02:33 +0900

    Add: ImageData 인터페이스에 name 속성 추가

--- front/components/ContentSection/index.tsx
+++ front/components/ContentSection/index.tsx
@@ -1,8 +1,8 @@
 import React, { CSSProperties, forwardRef, memo, MutableRefObject, useCallback, useEffect, useState } from 'react';
 import { IImageData } from '@typing/db';
-import { getImageData, getMarkerFetcher, getRoomImageListFetcher } from '@utils/roomDataFetcher';
+import { getImageData, getMarkerFetcher, getRoomImageListFetcher, getUnreadImageList } from '@utils/roomDataFetcher';
 import { Link, useParams } from 'react-router-dom';
-import useSWR, { mutate } from 'swr';
+import useSWR, { useSWRConfig } from 'swr';
 import useSWRMutation from 'swr/mutation';
 import { ContentBox, ImageCard, ImageInfo, InfoItem, MainContainer, PostImage, Wrapper } from './styles';
 import { imageLoadNumber } from '@hooks/swrStore';
@@ -15,11 +15,18 @@ import { SyncLoader } from 'react-spinners';
 interface ImageData {
   id: number;
   imageUrl: string;
-  create_date: string;
+  create_date: string | null;
+  name: string | null;
 }
 
 interface Props {
   roomId?: string;
+}
+
+interface ObserverRefObj {
+  now?: React.MutableRefObject<null>;
+  today?: React.MutableRefObject<null>;
+  previous?: React.MutableRefObject<null>;
 }
 
 const spinnerCSS: CSSProperties = {
@@ -30,27 +37,64 @@ const spinnerCSS: CSSProperties = {
 
 const ContentSection = forwardRef<Scrollbars, Props>(({ roomId }, scrollRef) => {
   const [readStartNumber, setReadStartNumber] = useState(0);
-  const [allImageData, setAllImageData] = useState<Array<ImageData>>([]);
+
+  // 1시간 전에 업데이트 된 이미지는 방금 업데이트된 이미지에 들어간다.
+  const [nowImage, setNowImage] = useState<ImageData[]>([]);
+  const [todayImage, setTodayImage] = useState<ImageData[]>([]);
+  const [previousImage, setPreviousImage] = useState<ImageData[]>([]);
   const [scrollHeight, setScrollHeight] = useState();
 
+  const { mutate } = useSWRConfig();
   const {
     data: imageList,
     trigger: imageListTrigger,
     isMutating: imageListLoading,
   } = useSWRMutation(`/room/${roomId}/imagelist`, getRoomImageListFetcher);
+  const { data: realTimeImageList, isLoading: realTimeImgLoading } = useSWR(
+    `/room/${roomId}/unread-imagelist`,
+    getUnreadImageList,
+    {
+      revalidateOnFocus: false,
+      revalidateOnMount: false,
+      revalidateOnReconnect: false,
+      refreshInterval: 5000,
+    },
+  );
+
   const {
-    data: imageDataList,
-    trigger: imageDataTrigger,
-    isMutating: imageDataLoading,
-  } = useSWRMutation('/image-download', getImageData);
-  const { data: markerNumber, trigger: markerTrigger } = useSWRMutation(`/room/${roomId}/marker`, getMarkerFetcher);
+    data: realTimeImageData,
+    trigger: realTimeImageDataTrigger,
+    isMutating: realTimeImageLoading,
+  } = useSWRMutation('/image-download/real', getImageData);
+
+  const {
+    data: nowImageData,
+    trigger: nowImageDataTrigger,
+    isMutating: nowImageLoading,
+  } = useSWRMutation('/image-download/now', getImageData);
+  const {
+    data: todayImageData,
+    trigger: todayImageDataTrigger,
+    isMutating: todayImageLoading,
+  } = useSWRMutation('/image-download/today', getImageData);
+  const {
+    data: previousImageData,
+    trigger: previousImageDataTrigger,
+    isMutating: previousImageLoading,
+  } = useSWRMutation('/image-download/previous', getImageData);
+
+  const markerAPI = `/room/${roomId}/marker`;
+  const { data: markerNumber } = useSWR(markerAPI);
 
   const observerRef = useIntersect(
     async (entry, observer) => {
       observer.unobserve(entry.target);
+      /*
 
-      // 데이터 fetching 중이 아니고 다음 로드할 데이터가 남아있다면 데이터를 부른다.(imageList 요청)
-      if (!imageListLoading && !imageDataLoading && imageList?.imagelist.length === 12) {
+      데이터 fetching 중이 아니고 다음 로드할 데이터가 남아있다면 데이터를 부른다.(imageList 요청)
+
+      */
+      if (!imageListLoading && !todayImageLoading && !previousImageLoading && imageList?.loadDataLength === 12) {
         console.log('인터섹션 데이터 패칭 요청');
         imageListTrigger({ roomId, start: readStartNumber });
       }
@@ -59,120 +103,115 @@ const ContentSection = forwardRef<Scrollbars, Props>(({ roomId }, scrollRef) => 
       threshold: 0.5,
     },
   );
+  const [observerRefObj, setObserverRefObj] = useState<ObserverRefObj>({});
 
-  // 처음 마운트 됐을 때
+  /*
+
+   처음 마운트 됐을 때
+
+  */
   useEffect(() => {
     console.log('컨텐트 섹션 처음 마운트 룸아이디', roomId);
     imageListTrigger({ roomId, start: readStartNumber });
-    markerTrigger();
   }, [roomId]);
 
-  // 스크롤이 일어날 때 imageList 요청을 보내서 다음 사진을 받고
-  // 이미지 로드 시작번호를 업데이트 한다.
+  /*
+
+    이미지 리스트 요청을 보내고 실제 이미지 데이터를 
+    분할 요청해서 화면에 보여준다.
+
+  */
   useEffect(() => {
     if (!imageList) return;
+
+    if (readStartNumber === 0) {
+      mutate(markerAPI, getMarkerFetcher(markerAPI));
+    }
     setReadStartNumber((prev) => prev + 12);
-    imageDataTrigger(imageList);
+
+    nowImageDataTrigger({ imagelist: imageList.nowImageList });
+    todayImageDataTrigger({ imagelist: imageList.todayImageList });
+    previousImageDataTrigger({ imagelist: imageList.previousImageList });
   }, [imageList]);
 
-  // 전달받은 이미지 정보를 allImageData에 업데이트
   useEffect(() => {
-    if (!imageDataList) return;
-    setAllImageData((prev) => [...prev, ...imageDataList.imgData]);
-  }, [imageDataList]);
+    if (nowImageLoading || todayImageLoading || previousImageLoading) return;
+    if (!nowImageData || !todayImageData || !previousImageData) return;
 
-  // useEffect(() => {
-  //   //  패칭 후에 스크롤을 현재 위치로 유지시킨다.
-  //   const current = (scrollRef as MutableRefObject<Scrollbars>)?.current;
-  //   console.log(current.getScrollHeight());
-  //   if (current) {
-  //     current.scrollTop(current.getScrollHeight());
-  //   }
-  // }, [allImageData]);
+    console.log('nowImageData:', nowImageData);
+    setNowImage((prev) => [...prev, ...nowImageData]);
+    setTodayImage((prev) => [...prev, ...todayImageData]);
+    setPreviousImage((prev) => [...prev, ...previousImageData]);
+  }, [nowImageData, todayImageData, previousImageData]);
 
-  // const handleIntersectTargetRef = useCallback(() => {
+  /*
 
-  // }, []);
+    실시간 이미지 업데이트
+
+  */
+
+  useEffect(() => {
+    if (realTimeImgLoading || !realTimeImageList) return;
+
+    realTimeImageDataTrigger({ imagelist: realTimeImageList });
+  }, [realTimeImageList]);
+
+  useEffect(() => {
+    if (!realTimeImageData || realTimeImageLoading) return;
+    console.log('realTImeImageData:', realTimeImageData);
+
+    setNowImage((prev) => [...realTimeImageData, ...prev]);
+  }, [realTimeImageData]);
+
+  /*
+
+    옵저버의 타킷을 변경해주는 Effect
+
+  */
+
+  useEffect(() => {
+    console.log('nowImage:', nowImage);
+    if (todayImage?.length === 0) {
+      setObserverRefObj((prev) => ({ ...prev, now: observerRef, today: undefined, previous: undefined }));
+    } else if (previousImage?.length === 0) {
+      setObserverRefObj((prev) => ({ ...prev, now: undefined, today: observerRef, previous: undefined }));
+    } else {
+      setObserverRefObj((prev) => ({ ...prev, now: undefined, today: undefined, previous: observerRef }));
+    }
+  }, [nowImage, todayImage, previousImage]);
+
   return (
     <Wrapper>
       <Scrollbars ref={scrollRef}>
         <MainContainer>
           <div>
-            {/* {imageDataList?.nextImgData.length !== 0 && (
-                <div>
-                  <div>
-                    <span>최신 업로드 이미지</span>
-                  </div>
-                  <div>
-                    {imageDataList?.nextImgData.map((data) => {
-                      return (
-                        <ContentBox key={data.id}>
-                          <div>
-                            <Link to="#">
-                              <ImageCard>
-                                <img src={data.imageUrl} />
-                              </ImageCard>
-                              <ImageInfo>
-                                <InfoItem>
-                                  <span>이미지 1</span>
-                                </InfoItem>
-                                <InfoItem>
-                                  <span>{data.create_date}</span>
-                                </InfoItem>
-                              </ImageInfo>
-                            </Link>
-                          </div>
-                        </ContentBox>
-                      );
-                    })}
-                  </div>
-                </div>
-              )} */}
             <div>
               <div>
-                <span>게시된 이미지</span>
+                <span>방금 업데이트된 이미지</span>
               </div>
               <PostImage>
-                <ImageContentList allImageData={allImageData} observerRef={observerRef} />
+                <ImageContentList ImageData={nowImage} observerRef={observerRefObj.now} />
               </PostImage>
             </div>
-            {(imageListLoading || imageDataLoading) && <SyncLoader color="cornflowerblue" cssOverride={spinnerCSS} />}
-            {/* allImageData.map((data, index, thisArr) => {
-                return (
-                  <ContentBox key={data.id} ref={thisArr.length - 1 === index ? observerRef : undefined}>
-                    <div>
-                      <Link to="#">
-                        <ImageCard>
-                          <img src={data.imageUrl} />
-                        </ImageCard>
-                        <ImageInfo>
-                          <InfoItem>
-                            <span>이미지 1</span>
-                          </InfoItem>
-                          <InfoItem>
-                            <span>{data.create_date}</span>
-                          </InfoItem>
-                        </ImageInfo>
-                      </Link>
-                    </div>
-                  </ContentBox>
-                );
-              }) */}
-            {/* <div>
-                <div>
-                  <span>삭제된 이미지 목록</span>
-                </div>
-                <div>
-                  {imageDataList?.deleteImgData.map((data, i) => {
-                    // console.log(data);
-                    return (
-                      <ContentBox key={data.id}>
-                        <div>{i}번째 이미지는 삭제되었습니다..</div>
-                      </ContentBox>
-                    );
-                  })}
-                </div>
-              </div> */}
+            <div>
+              <div>
+                <span>오늘 업데이트된 이미지</span>
+              </div>
+              <PostImage>
+                <ImageContentList ImageData={todayImage} observerRef={observerRefObj.today} />
+              </PostImage>
+            </div>
+            <div>
+              <div>
+                <span>이전에 업데이트된 이미지</span>
+              </div>
+              <PostImage>
+                <ImageContentList ImageData={previousImage} observerRef={observerRefObj.previous} />
+              </PostImage>
+            </div>
+            {(imageListLoading || todayImageLoading || previousImageLoading) && (
+              <SyncLoader color="cornflowerblue" cssOverride={spinnerCSS} />
+            )}
           </div>
         </MainContainer>
       </Scrollbars>
