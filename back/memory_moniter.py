7b764d53e4501d8eab234ffffef27cef97f167b6
Nliker <tjwjdgus83@naver.com>
2023-01-19 14:48:46 +0900

    plot 주기 변경

--- back/memory_moniter.py
+++ back/memory_moniter.py
@@ -2,12 +2,12 @@ import time
 import psutil
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-
+import numpy as np
+import os
+np_file_path=os.path.dirname(os.path.realpath(__file__))+"np_save.npy"
 # while True:
 #     display_usage(None,psutil.virtual_memory().percent,30)
 #     time.sleep(0.5)
-
-y=[]
 def animate(i):
     memory_use_percent=psutil.virtual_memory().percent
 
@@ -15,8 +15,17 @@ def animate(i):
 
     plt.cla()
     plt.plot(y,'r',label="real-time Memory usage")
-    
+            
     plt.tight_layout()
+    
+if os.path.isfile(np_file_path):
+    y=np.load(np_file_path)
 
-ani=FuncAnimation(plt.gcf(),animate,interval=1000)
-plt.show()
+    ani=FuncAnimation(plt.gcf(),animate,interval=100)
+    plt.show()
+else:
+    while True:
+        memory_use_percent=psutil.virtual_memory().percent
+        print(memory_use_percent)
+        
+        time.sleep(1)
