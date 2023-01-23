59cfbefea14f7b0dd08106094aa266f041fb539c
kanghunK <ltbehappy16@gmail.com>
2023-01-23 12:32:19 +0900

    Design: Toolbar css 수정

--- front/components/ToolBar/styles.tsx
+++ front/components/ToolBar/styles.tsx
@@ -26,6 +26,8 @@ export const Wrapper = styled.div`
 export const RightIcons = styled.div`
   position: absolute;
   right: 0;
+
+  gap: 1rem;
 `;
 
 export const LeftIcon = styled.div`
@@ -41,7 +43,8 @@ export const UserBox = styled.div`
   right: -23px;
   padding: 20px;
   border-radius: 12px;
-  box-shadow: 0 0.125rem 0.5rem rgb(0 0 0 / 30%), 0 0.0625rem 0.125rem rgb(0 0 0 / 20%);
+  box-shadow: 0 0.125rem 0.5rem rgb(0 0 0 / 30%),
+    0 0.0625rem 0.125rem rgb(0 0 0 / 20%);
 
   z-index: 1;
   background-color: white;
@@ -96,3 +99,42 @@ export const LogoutBtn = styled.div`
     cursor: pointer;
   }
 `;
+
+export const SettingBox = styled.div`
+  position: absolute;
+  top: 48px;
+  flex-direction: column;
+
+  height: auto;
+  right: 17px;
+  padding: 20px 0;
+  border-radius: 12px;
+  box-shadow: 0 0.125rem 0.5rem rgb(0 0 0 / 30%),
+    0 0.0625rem 0.125rem rgb(0 0 0 / 20%);
+
+  z-index: 1;
+  background-color: white;
+
+  &::before {
+    content: '';
+    position: absolute;
+
+    width: 0;
+    height: 0;
+    bottom: 100%;
+    right: 1.5em;
+    border: 0.75rem solid transparent;
+    border-top: none;
+
+    border-bottom-color: #fff;
+    filter: drop-shadow(0 -0.0625rem 0.0625rem rgba(0, 0, 0, 0.1));
+  }
+
+  .leave_room {
+    cursor: pointer;
+    &:hover {
+      background-color: red;
+    }
+  }
+`;
+export const LeaveRoom = styled.div``;
