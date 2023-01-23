b29b2dd241043af0e381595101f98c9f0a0be5d4
kanghunK <ltbehappy16@gmail.com>
2023-01-23 12:28:45 +0900

    <Refactor> 1. 방 전환하거나 이미지 패치시 겹치는 아이디 오류 수정, 2. 옵저버 위치 수정하는 함수 설정

--- front/components/ContentSection/index.tsx
+++ front/components/ContentSection/index.tsx
@@ -1,9 +1,30 @@
-import React, { CSSProperties, forwardRef, memo, MutableRefObject, useCallback, useEffect, useState } from 'react';
-import { getImageData, getMarkerFetcher, getRoomImageListFetcher, getUnreadImageList } from '@utils/roomDataFetcher';
+import React, {
+  CSSProperties,
+  forwardRef,
+  memo,
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import {
+  getImageData,
+  getMarkerFetcher,
+  getRoomImageListFetcher,
+  getUnreadImageList,
+} from '@utils/roomDataFetcher';
 import { Link, useParams } from 'react-router-dom';
 import useSWR, { useSWRConfig } from 'swr';
 import useSWRMutation from 'swr/mutation';
-import { ContentBox, ImageCard, ImageInfo, InfoItem, MainContainer, PostImage, Wrapper } from './styles';
+import {
+  ContentBox,
+  ImageCard,
+  ImageInfo,
+  InfoItem,
+  MainContainer,
+  PostImage,
+  Wrapper,
+} from './styles';
 import { imageLoadNumber } from '@hooks/swrStore';
 import { AxiosResponse } from 'axios';
 import useIntersect from '@hooks/useIntersect';
@@ -11,9 +32,17 @@ import Scrollbars from 'react-custom-scrollbars';
 import ImageContentList from '@components/ImageContentList';
 import { SyncLoader } from 'react-spinners';
 import { CImageData } from '@typing/client';
+import { DImageData } from '@typing/db';
 
 interface Props {
   roomId?: string;
+}
+
+interface ImageList {
+  nowImageList: DImageData[];
+  todayImageList: DImageData[];
+  previousImageList: DImageData[];
+  loadDataLength: any;
 }
 
 const spinnerCSS: CSSProperties = {
@@ -22,196 +51,219 @@ const spinnerCSS: CSSProperties = {
   textAlign: 'center',
 };
 
-const ContentSection = forwardRef<Scrollbars, Props>(({ roomId }, scrollRef) => {
-  const [readStartNumber, setReadStartNumber] = useState(0);
+const ContentSection = forwardRef<Scrollbars, Props>(
+  ({ roomId }, scrollRef) => {
+    const [readStartNumber, setReadStartNumber] = useState(0);
+    const [scrollHeight, setScrollHeight] = useState();
 
-  // 1시간 전에 업데이트 된 이미지는 방금 업데이트된 이미지에 들어간다.
-  const [nowImage, setNowImage] = useState<CImageData[]>([]);
-  const [todayImage, setTodayImage] = useState<CImageData[]>([]);
-  ``;
-  const [previousImage, setPreviousImage] = useState<CImageData[]>([]);
-  const [scrollHeight, setScrollHeight] = useState();
+    const { mutate } = useSWRConfig();
+    const {
+      data: imageList,
+      trigger: imageListTrigger,
+      isMutating: imageListLoading,
+    } = useSWRMutation(`/room/${roomId}/imagelist`, getRoomImageListFetcher);
 
-  const { mutate } = useSWRConfig();
-  const {
-    data: imageList,
-    trigger: imageListTrigger,
-    isMutating: imageListLoading,
-  } = useSWRMutation(`/room/${roomId}/imagelist`, getRoomImageListFetcher);
-  const { data: realTimeImageList, isLoading: realTimeImgLoading } = useSWR(
-    `/room/${roomId}/unread-imagelist`,
-    getUnreadImageList,
-    {
-      revalidateOnFocus: false,
-      revalidateOnMount: false,
-      revalidateOnReconnect: false,
-      refreshInterval: 5000,
-    },
-  );
+    const { data: realTimeImageList, isLoading: realTimeImgLoading } = useSWR(
+      `/room/${roomId}/unread-imagelist`,
+      getUnreadImageList,
+      {
+        revalidateOnFocus: false,
+        revalidateOnMount: false,
+        revalidateOnReconnect: false,
+        refreshInterval: 5000,
+      },
+    );
+    const {
+      data: getImage,
+      trigger: getImageTrigger,
+      isMutating: getImageLoading,
+    } = useSWRMutation('/image-download', getImageData);
 
-  const {
-    data: realTimeImageData,
-    trigger: realTimeImageDataTrigger,
-    isMutating: realTimeImageLoading,
-  } = useSWRMutation('/image-download/real', getImageData);
+    const { data: nowImage, mutate: mutateNowImage } = useSWR<CImageData[]>(
+      `/image/${roomId}/now`,
+      {
+        fallbackData: [],
+      },
+    );
+    const { data: todayImage, mutate: mutateTodayImage } = useSWR<CImageData[]>(
+      `/image/${roomId}/today`,
+      {
+        fallbackData: [],
+      },
+    );
+    const { data: previousImage, mutate: mutatePrevImage } = useSWR<
+      CImageData[]
+    >(`/image/${roomId}/previous`, {
+      fallbackData: [],
+    });
 
-  const {
-    data: nowImageData,
-    trigger: nowImageDataTrigger,
-    isMutating: nowImageLoading,
-  } = useSWRMutation('/image-download/now', getImageData);
-  const {
-    data: todayImageData,
-    trigger: todayImageDataTrigger,
-    isMutating: todayImageLoading,
-  } = useSWRMutation('/image-download/today', getImageData);
-  const {
-    data: previousImageData,
-    trigger: previousImageDataTrigger,
-    isMutating: previousImageLoading,
-  } = useSWRMutation('/image-download/previous', getImageData);
+    const markerAPI = `/room/${roomId}/marker`;
+    const { data: markerNumber } = useSWR(markerAPI);
 
-  const markerAPI = `/room/${roomId}/marker`;
-  const { data: markerNumber } = useSWR(markerAPI);
-
-  const observerRef = useIntersect(
-    async (entry, observer) => {
-      observer.unobserve(entry.target);
-      /*
+    const [observerRefPos, setObserverRefPos] = useState<number>();
+    const observerRef = useIntersect(
+      async (entry, observer) => {
+        observer.unobserve(entry.target);
+        /*
 
       데이터 fetching 중이 아니고 다음 로드할 데이터가 남아있다면 데이터를 부른다.(imageList 요청)
 
       */
-      if (
-        !imageListLoading &&
-        !nowImageLoading &&
-        !todayImageLoading &&
-        !previousImageLoading &&
-        imageList?.loadDataLength === 12
-      ) {
-        console.log('인터섹션 데이터 패칭 요청');
-        imageListTrigger({ roomId, start: readStartNumber });
-      }
-    },
-    {
-      threshold: 0.5,
-    },
-  );
-  const [observerRefPos, setObserverRefPos] = useState<number>();
+        if (
+          !imageListLoading &&
+          !getImageLoading &&
+          imageList?.loadDataLength === 12
+        ) {
+          console.log('인터섹션 데이터 패칭 요청, 옵저버:', observerRefPos);
+          imageListTrigger({ roomId, start: readStartNumber });
+        }
+      },
+      {
+        threshold: 0.5,
+      },
+    );
 
-  /*
+    if (!nowImage || !todayImage || !previousImage) {
+      return <div>로딩중...</div>;
+    }
 
-   처음 마운트 됐을 때
+    /*
+
+   방에 처음 들어가서 마운트 됐을 때
 
   */
 
-  useEffect(() => {
-    console.log('컨텐트 섹션 처음 마운트 룸아이디', roomId);
-    imageListTrigger({ roomId, start: readStartNumber });
-  }, [roomId]);
+    useEffect(() => {
+      console.log('컨텐트 섹션 처음 마운트 룸아이디', roomId);
+      imageListTrigger({ roomId, start: readStartNumber });
 
-  /*
+      return () => {
+        mutateNowImage([], false);
+        mutateTodayImage([], false);
+        mutatePrevImage([], false);
+      };
+    }, [roomId]);
+
+    /*
 
     이미지 리스트 요청을 보내고 실제 이미지 데이터를 
     분할 요청해서 화면에 보여준다.
 
   */
 
-  useEffect(() => {
-    if (!imageList) return;
+    useEffect(() => {
+      if (!imageList) return;
 
-    if (readStartNumber === 0) {
-      mutate(markerAPI, getMarkerFetcher(markerAPI));
-    }
-    setReadStartNumber((prev) => prev + 12);
+      if (readStartNumber === 0) {
+        mutate(markerAPI, getMarkerFetcher(markerAPI));
+      }
 
-    nowImageDataTrigger({ imagelist: imageList.nowImageList });
-    todayImageDataTrigger({ imagelist: imageList.todayImageList });
-    previousImageDataTrigger({ imagelist: imageList.previousImageList });
+      setReadStartNumber((prev) => prev + 12);
 
-    // 인터섹션 옵저버 위치 변경
-    if (imageList.previousImageList.length !== 0) {
-      setObserverRefPos(2);
-    } else if (imageList.todayImageList.length !== 0) {
-      setObserverRefPos(1);
-    } else {
-      setObserverRefPos(0);
-    }
-  }, [imageList]);
+      fetchImages(imageList);
 
-  useEffect(() => {
-    if (nowImageLoading || todayImageLoading || previousImageLoading) return;
-    if (!nowImageData || !todayImageData || !previousImageData) return;
+      // 인터섹션 옵저버 위치 변경
+      if (imageList.previousImageList.length !== 0) {
+        setObserverRefPos(2);
+      } else if (imageList.todayImageList.length !== 0) {
+        setObserverRefPos(1);
+      } else {
+        setObserverRefPos(0);
+      }
+    }, [imageList]);
 
-    console.log('nowImageData:', nowImageData);
-    setNowImage((prev) => [...prev, ...nowImageData]);
-    setTodayImage((prev) => [...prev, ...todayImageData]);
-    setPreviousImage((prev) => [...prev, ...previousImageData]);
-  }, [nowImageData, todayImageData, previousImageData]);
+    const fetchImages = async (imageList: ImageList) => {
+      if (imageList.nowImageList.length !== 0) {
+        getImageTrigger(imageList.nowImageList).then((newImage) => {
+          if (newImage) mutateNowImage([...nowImage, ...newImage]);
+        });
+      }
+      if (imageList.todayImageList.length !== 0) {
+        getImageTrigger(imageList.todayImageList).then((newImage) => {
+          if (newImage) mutateTodayImage([...todayImage, ...newImage]);
+        });
+      }
+      if (imageList.previousImageList.length !== 0) {
+        getImageTrigger(imageList.previousImageList).then((newImage) => {
+          if (newImage) mutatePrevImage([...previousImage, ...newImage]);
+        });
+      }
+    };
 
-  /*
+    /*
 
     실시간 이미지 업데이트
 
   */
 
-  useEffect(() => {
-    if (realTimeImgLoading || !realTimeImageList) return;
+    useEffect(() => {
+      if (realTimeImgLoading || !realTimeImageList) return;
 
-    realTimeImageDataTrigger({ imagelist: realTimeImageList });
-  }, [realTimeImageList]);
+      getImageTrigger(realTimeImageList).then((newImage) => {
+        if (newImage) mutateNowImage([...newImage, ...nowImage]);
+      });
+    }, [realTimeImageList]);
 
-  useEffect(() => {
-    if (!realTimeImageData || realTimeImageLoading) return;
-    console.log('realTImeImageData:', realTimeImageData);
+    const observerConverter = useCallback(
+      (sectionOrder: number) => {
+        const value = sectionOrder === observerRefPos ? observerRef : undefined;
+        return value;
+      },
+      [observerRefPos],
+    );
 
-    setNowImage((prev) => [...realTimeImageData, ...prev]);
-  }, [realTimeImageData]);
+    // console.log('이미지 리스트: ', imageList);
+    // console.log('컨텐트 섹션: ', previousImage);
 
-  const observerConverter = useCallback(
-    (sectionOrder: number) => {
-      const value = sectionOrder === observerRefPos ? observerRef : undefined;
-      return value;
-    },
-    [observerRefPos],
-  );
-
-  return (
-    <Wrapper>
-      <Scrollbars ref={scrollRef}>
-        <MainContainer>
-          <div>
+    return (
+      <Wrapper>
+        <Scrollbars ref={scrollRef}>
+          <MainContainer>
             <div>
               <div>
-                <span>방금 업데이트된 이미지</span>
+                <div>
+                  <span>방금 업데이트된 이미지</span>
+                </div>
+                <PostImage>
+                  <ImageContentList
+                    ImageData={nowImage}
+                    sectionName={'now'}
+                    observerRef={observerConverter(0)}
+                  />
+                </PostImage>
               </div>
-              <PostImage>
-                <ImageContentList ImageData={nowImage} observerRef={observerConverter(0)} />
-              </PostImage>
-            </div>
-            <div>
               <div>
-                <span>오늘 업데이트된 이미지</span>
+                <div>
+                  <span>오늘 업데이트된 이미지</span>
+                </div>
+                <PostImage>
+                  <ImageContentList
+                    ImageData={todayImage}
+                    sectionName={'today'}
+                    observerRef={observerConverter(1)}
+                  />
+                </PostImage>
               </div>
-              <PostImage>
-                <ImageContentList ImageData={todayImage} observerRef={observerConverter(1)} />
-              </PostImage>
-            </div>
-            <div>
               <div>
-                <span>이전에 업데이트된 이미지</span>
+                <div>
+                  <span>이전에 업데이트된 이미지</span>
+                </div>
+                <PostImage>
+                  <ImageContentList
+                    ImageData={previousImage}
+                    sectionName={'previous'}
+                    observerRef={observerConverter(2)}
+                  />
+                </PostImage>
               </div>
-              <PostImage>
-                <ImageContentList ImageData={previousImage} observerRef={observerConverter(2)} />
-              </PostImage>
+              {(imageListLoading || getImageLoading) && (
+                <SyncLoader color="cornflowerblue" cssOverride={spinnerCSS} />
+              )}
             </div>
-            {(imageListLoading || nowImageLoading || todayImageLoading || previousImageLoading) && (
-              <SyncLoader color="cornflowerblue" cssOverride={spinnerCSS} />
-            )}
-          </div>
-        </MainContainer>
-      </Scrollbars>
-    </Wrapper>
-  );
-});
+          </MainContainer>
+        </Scrollbars>
+      </Wrapper>
+    );
+  },
+);
 export default ContentSection;
