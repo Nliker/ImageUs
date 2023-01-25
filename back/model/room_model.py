b4aa1c832422d2f0ba313065c45d2099c8349fcf
Nliker <tjwjdgus83@naver.com>
2023-01-25 13:24:14 +0900

    execute.close 명시

--- back/model/room_model.py
+++ back/model/room_model.py
@@ -6,7 +6,7 @@ class RoomDao:
         self.db=database
         
     def insert_room(self,new_room):
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             insert into rooms(
                 title,
                 host_user_id
@@ -14,13 +14,15 @@ class RoomDao:
                 :title,
                 :user_id
             )
-            """),new_room).lastrowid
+            """),new_room)
+        row=result.lastrowid
+        result.close()
         new_room_id=row
 
         return new_room_id
     
     def get_room_info(self,room_id):
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             select
                 id,
                 title,
@@ -30,7 +32,10 @@ class RoomDao:
             and deleted=0
             """),{
                     'room_id':room_id
-                }).fetchone()
+                })
+        row=result.fetchone()
+        result.close()
+
         room_info={
             'id':row['id'],
             'title':row['title'],
@@ -39,7 +44,7 @@ class RoomDao:
         return room_info
         
     def get_user_roomlist(self,user_id):
-        rows=self.db.execute(text("""
+        result=self.db.execute(text("""
             select
                 r_u.room_id as id,
                 r.title,
@@ -52,7 +57,9 @@ class RoomDao:
             and r_u.deleted=0
             """),{
                     'user_id':user_id
-                }).fetchall()
+                })
+        rows=result.fetchall()
+        result.close()
         
         user_room_info_list=[{
             'id':user_room_info['id'],
@@ -63,7 +70,7 @@ class RoomDao:
         return user_room_info_list
         
     def get_room_userlist(self,room_id): 
-        rows=self.db.execute(text("""
+        result=self.db.execute(text("""
             select
                 r_u.user_id as id,
                 u.name,
@@ -77,7 +84,9 @@ class RoomDao:
             and r_u.deleted=0
             """),{
                 'room_id':room_id
-            }).fetchall()
+            })
+        rows=result.fetchall()
+        result.close()
         
         room_user_info_list=[{
             'id':room_user_info['id'],
@@ -90,7 +99,7 @@ class RoomDao:
 
     def insert_room_user(self,room_id,user_id):
         try:
-            row=self.db.execute(text("""
+            result=self.db.execute(text("""
                 insert into rooms_user_list(
                     room_id,
                     user_id
@@ -101,13 +110,15 @@ class RoomDao:
             """),{
                     'room_id':room_id,
                     'user_id':user_id
-                }).rowcount
+                })
+            row=result.rowcount
+            result.close()
             return 1
         except IntegrityError as e:
             return 0
 
     def delete_room_user(self,room_id,user_id):
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             update rooms_user_list
             set deleted=1
             where (room_id=:room_id
@@ -116,19 +127,23 @@ class RoomDao:
             """),{
                     'room_id':room_id,
                     'user_id':user_id
-                }).rowcount
+                })
+        row=result.rowcount
+        result.close()
 
         return row
     
     def get_room_user(self,room_id,user_id):
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             select
                 room_id,
                 user_id
             from rooms_user_list
             where room_id=:room_id
             and user_id=:user_id
-            """),{'room_id':room_id,'user_id':user_id}).fetchone()
+            """),{'room_id':room_id,'user_id':user_id})
+        row=result.fetchone()
+        result.close()
 
         room_user={
             'room_id':row['room_id'],
@@ -138,7 +153,7 @@ class RoomDao:
         return room_user
 
     def insert_room_user_history(self,room_id,user_id):
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             insert into rooms_user_history(
                 user_id,
                 room_id
@@ -146,16 +161,21 @@ class RoomDao:
                 :user_id,
                 :room_id
             )
-            """),{'user_id':user_id,'room_id':room_id}).rowcount
+            """),{'user_id':user_id,'room_id':room_id})
+        row=result.rowcount
+        result.close()
+        
         return row
     
     def get_room_user_history_info(self,room_id,user_id):
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             select *
             from rooms_user_history
             where room_id=:room_id
             and user_id=:user_id
-            """),{'room_id':room_id,'user_id':user_id}).fetchone()
+            """),{'room_id':room_id,'user_id':user_id})
+        row=result.fetchone()
+        result.close()
         
         user_read_history={
             'user_id':row['user_id'],
@@ -169,7 +189,7 @@ class RoomDao:
 
     def update_room_user_history_start(self,room_id,user_id,images_len):
 
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             update rooms_user_history
             set marker_row=:images_len-last_unread_row
             ,last_unread_row=:images_len
@@ -177,37 +197,45 @@ class RoomDao:
             where room_id=:room_id
             and user_id=:user_id
             and deleted=0            
-        """),{'room_id':room_id,'user_id':user_id,'images_len':images_len}).rowcount
+        """),{'room_id':room_id,'user_id':user_id,'images_len':images_len})
+        row=result.rowcount
+        result.close()
 
         return row
         
     def update_room_user_history_last_unread_row(self,room_id,user_id,last_unread_row):
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             update rooms_user_history
             set last_unread_row=:last_unread_row
             where room_id=:room_id
             and user_id=:user_id
             and deleted=0
-        """),{'room_id':room_id,'user_id':user_id,'last_unread_row':last_unread_row}).rowcount
+        """),{'room_id':room_id,'user_id':user_id,'last_unread_row':last_unread_row})
+        row=result.rowcount
+        result.close()
         
         return row
     
     def delete_user_rooms(self,user_id):
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             update rooms_user_list
             set deleted=1
             where user_id=:user_id
             and deleted=0
-        """),{'user_id':user_id}).rowcount
+        """),{'user_id':user_id})
+        row=result.rowcount
+        result.close()
 
         return row
 
     def delete_user_room_history(self,user_id):
-        row=self.db.execute(text("""
+        result=self.db.execute(text("""
             update rooms_user_history
             set deleted=1
             where user_id=:user_id
             and deleted=0
-        """),{'user_id':user_id}).rowcount
+        """),{'user_id':user_id})
+        row=result.rowcount
+        result.close()
 
         return row
