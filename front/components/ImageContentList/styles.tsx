29866da9100b2a8c8f0b1b3b2912447ff09df133
kanghunK <ltbehappy16@gmail.com>
2023-01-27 18:25:16 +0900

    Add: 미디어 쿼리가 적용된 ImageLayout Element 추가

---
+++ front/components/ImageContentList/styles.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const ImageLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  gap: 1rem;
+
+  @media screen and (min-width: 450px) {
+    grid-template-columns: repeat(3, auto);
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: repeat(4, auto);
+  }
+`;
