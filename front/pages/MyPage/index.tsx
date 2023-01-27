a4dc22e55c3506a8ab684bec6953e67ec7b37a65
kanghunK <ltbehappy16@gmail.com>
2023-01-27 18:31:06 +0900

    Add: 마이 페이지에서 자신이 가진 사진 볼 수 있도록 기능 추가

--- front/pages/MyPage/index.tsx
+++ front/pages/MyPage/index.tsx
@@ -7,40 +7,104 @@ import useSWRMutation from 'swr/mutation';
 
 import { NavLink } from 'react-router-dom';
 import { Routes, Route } from 'react-router';
-import { ContentBox, EachRoomPictureList, ProfileBox, ProfileImage, ProfileInfo, SubMenu, WrapperBox } from './styles';
-import { getImageData, getUserFriendList, getUserImageList, getUserRoomListFetcher } from '@utils/userDataFetcher';
+import {
+  ContentBox,
+  EachRoomPictureList,
+  ProfileBox,
+  ProfileImage,
+  ProfileInfo,
+  SubMenu,
+  WrapperBox,
+} from './styles';
+import {
+  getImageData,
+  getUserFriendList,
+  getUserImageList,
+  getUserRoomListFetcher,
+} from '@utils/userDataFetcher';
 import { DImageData } from '@typing/db';
 import { userImageLoadNumber } from '@hooks/swrStore';
+import { CImageData } from '@typing/client';
+import useIntersect from '@hooks/useIntersect';
 
 const MyPage = () => {
-  const { data: roomlist, mutate: mutateRoomList } = useSWR('roomlist', getUserRoomListFetcher, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
-  const { data: friendList, mutate: mutateFriendList } = useSWR('friendlist', getUserFriendList, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
+  const userId = sessionStorage.getItem('USER_ID');
 
-  const { start } = userImageLoadNumber();
-  const { data: imageList } = useSWR<Array<DImageData> | undefined>(['userImageList', start], getUserImageList, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
-  const { data: imageDataList, trigger } = useSWRMutation('/image-download', getImageData);
+  const { data: roomlist, mutate: mutateRoomList } = useSWR(
+    'roomlist',
+    getUserRoomListFetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  );
+  const { data: friendList, mutate: mutateFriendList } = useSWR(
+    'friendlist',
+    getUserFriendList,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  );
+  const { data: userImageList, mutate: mutateImageList } = useSWR<CImageData[]>(
+    '/user/imageDataList',
+    {
+      fallbackData: [],
+    },
+  );
 
-  // 스크롤이 일어날 때 imageList 요청을 보내서 다음 사진을 받고
-  // mutate(key, start + 개수 + 1, true)로 이미지 로드 시작번호를 업데이트 한다.
+  const {
+    data: imageListInfo,
+    trigger: requestImageListInfo,
+    isMutating: imageInfoLoading,
+  } = useSWRMutation(`/user/${userId}/imagelist`, getUserImageList);
+  const { trigger: imageDataTrigger, isMutating: imageDataLoading } =
+    useSWRMutation('/user/image-download', getImageData);
 
-  // console.log(imageList);
+  const [readStartNumber, setReadStartNumber] = useState(0);
+
+  const observerRef = useIntersect(
+    async (entry, observer) => {
+      observer.unobserve(entry.target);
+      /*
+
+    데이터 fetching 중이 아니고 다음 로드할 데이터가 남아있다면 데이터를 부른다.(imageList 요청)
+
+    */
+      if (
+        !imageInfoLoading &&
+        !imageDataLoading &&
+        imageListInfo?.loadDataLength === 12
+      ) {
+        console.log('인터섹션 데이터 패칭 요청');
+        requestImageListInfo(readStartNumber);
+      }
+    },
+    {
+      threshold: 0.5,
+    },
+  );
 
   useEffect(() => {
-    if (!imageList) return;
-    trigger(imageList);
-  }, [imageList]);
+    requestImageListInfo(readStartNumber);
+
+    return () => {
+      console.log('마이페이지 언마운트');
+      setReadStartNumber(0);
+      mutateImageList([], false);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!imageListInfo || !userImageList) return;
+
+    setReadStartNumber((prev) => prev + 12);
+    imageDataTrigger(imageListInfo.imagelist).then((newImage) => {
+      if (newImage) mutateImageList([...userImageList, ...newImage]);
+    });
+  }, [imageListInfo]);
 
   return (
     <AppLayout>
@@ -57,7 +121,7 @@ const MyPage = () => {
               <ul>
                 <li>
                   <div>
-                    게시물<span>{imageList?.length}</span>
+                    게시물<span>{imageListInfo?.loadDataLength}</span>
                   </div>
                 </li>
                 <li>
@@ -75,17 +139,36 @@ const MyPage = () => {
           </ProfileBox>
           <EachRoomPictureList>
             <SubMenu>
-              <NavLink to={`/my_page`} className={({ isActive }) => (isActive ? 'menu_active' : undefined)} end>
+              <NavLink
+                to={`/my_page`}
+                className={({ isActive }) =>
+                  isActive ? 'menu_active' : undefined
+                }
+                end
+              >
                 <div>사진첩</div>
               </NavLink>
-              <NavLink to={`/my_page/my_profile`} className={({ isActive }) => (isActive ? 'menu_active' : undefined)}>
+              <NavLink
+                to={`/my_page/my_profile`}
+                className={({ isActive }) =>
+                  isActive ? 'menu_active' : undefined
+                }
+              >
                 <div>프로필</div>
               </NavLink>
             </SubMenu>
             <div>
               {/* 여기를 Route를 활용해서 만든다. */}
               <Routes>
-                <Route path="/" element={<MyPictures imageList={imageDataList} />} />
+                <Route
+                  path="/"
+                  element={
+                    <MyPictures
+                      imageList={userImageList}
+                      observerRef={observerRef}
+                    />
+                  }
+                />
                 <Route path="my_profile" element={<MypageInfo />} />
               </Routes>
             </div>
