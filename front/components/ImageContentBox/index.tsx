dcbadb889672350b7995ce77fc4971bccd6f2483
kanghunK <ltbehappy16@gmail.com>
2023-01-27 18:23:52 +0900

    Feat: 이미지 호버 시에 삭제 버튼과 자세히 보는 버튼 추가

--- front/components/ImageContentBox/index.tsx
+++ front/components/ImageContentBox/index.tsx
@@ -2,9 +2,11 @@ import { CImageData } from '@typing/client';
 import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import {
+  AertContainer,
   ContentBox,
   EditBtn,
   EditImageMenu,
+  HoverBox,
   ImageCard,
   ImageInfo,
   InfoContainer,
@@ -13,13 +15,14 @@ import useSWR, { useSWRConfig } from 'swr';
 import useSWRMutation from 'swr/mutation';
 import { deleteImageFetcher } from '@utils/roomDataFetcher';
 import { useParams } from 'react-router';
+import { Button } from '@styles/Button';
 
 interface Props {
   data: CImageData;
   index: number;
   thisArr: CImageData[];
   observerRef?: React.MutableRefObject<null>;
-  sectionName: string;
+  sectionName?: string;
 }
 
 /*
@@ -35,83 +38,148 @@ const ImageContentBox = ({
   observerRef,
   sectionName,
 }: Props) => {
+  const { data: showModalState, mutate: mutateModalState } =
+    useSWR('showModalState');
+  // const { data: deleteImageTriggerVar } = useSWR('deleteImage');
+  const { mutate: mutateDeleteImageInfo } = useSWR('deleteImageInfo');
+  const { mutate: mutateDetailImageInfo } = useSWR('detailImageInfo');
   const { roomId } = useParams<{ roomId: string }>();
-  const { data: imageList, mutate: fetchData } = useSWR(
+  const { data: roomImageList, mutate: fetchRoomImage } = useSWR(
     `/image/${roomId}/${sectionName}`,
   );
-  const { trigger: deleteTrigger } = useSWRMutation(
-    `/room/${roomId}/image`,
-    deleteImageFetcher,
+  const { data: deleteRoomImageId, isLoading: roomImageDeleting } = useSWR(
+    'roomImageDelete',
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
   );
 
-  const [showEditBox, setShowEditBox] = useState(false);
+  const { data: userImageList, mutate: fetchUserImage } = useSWR(
+    '/user/imageDataList',
+  );
+  const { data: deleteUserImageId, isLoading: userImageDeleting } = useSWR(
+    'userImageDelete',
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  );
+
+  const [isHover, setIsHover] = useState(false);
   const imageDeleteEl = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    window.addEventListener('click', handleCloseEditBox);
-    return () => {
-      window.removeEventListener('click', handleCloseEditBox);
-    };
-  });
+    if (!roomImageList || roomImageDeleting || deleteRoomImageId !== data.id)
+      return;
+    console.log('이미지 아이디: ', deleteRoomImageId);
+    const filteredList = roomImageList.filter(
+      (image: CImageData) => image.id !== data.id,
+    );
+    console.log(filteredList);
+    fetchRoomImage([...filteredList], false);
+  }, [deleteRoomImageId]);
 
-  const handleCloseEditBox = useCallback(
-    (e: MouseEvent) => {
-      if (
-        e.target instanceof HTMLElement ||
-        (e.target instanceof SVGElement &&
-          showEditBox &&
-          !imageDeleteEl.current?.contains(e.target))
-      ) {
-        setShowEditBox(false);
-      }
-    },
-    [showEditBox],
-  );
+  useEffect(() => {
+    if (!userImageList || userImageDeleting || deleteUserImageId !== data.id)
+      return;
+    console.log('이미지 아이디: ', deleteUserImageId);
+    const filteredList = userImageList.filter(
+      (image: CImageData) => image.id !== data.id,
+    );
+    console.log(filteredList);
+    fetchUserImage([...filteredList], false);
+  }, [deleteUserImageId]);
 
-  const onClickEditBtn = useCallback(() => {
-    setShowEditBox(true);
-  }, [showEditBox]);
+  // useEffect(() => {
+  //   window.addEventListener('click', handleCloseEditBox);
+  //   return () => {
+  //     window.removeEventListener('click', handleCloseEditBox);
+  //   };
+  // });
 
-  const deleteImage = useCallback(async () => {
-    deleteTrigger(data.id).then(() => {
-      const filteredList = imageList.filter(
-        (image: CImageData) => image.id !== data.id,
-      );
-      fetchData([...filteredList], false);
-    });
-  }, [data]);
+  // const handleCloseEditBox = useCallback(
+  //   (e: MouseEvent) => {
+  //     if (
+  //       e.target instanceof HTMLElement ||
+  //       (e.target instanceof SVGElement &&
+  //         showEditBox &&
+  //         !imageDeleteEl.current?.contains(e.target))
+  //     ) {
+  //       setShowEditBox(false);
+  //     }
+  //   },
+  //   [showEditBox],
+  // );
+
+  // const onClickEditBtn = useCallback(() => {
+  //   setShowEditBox(true);
+  // }, [showEditBox]);
+
+  // const deleteImage = useCallback(async () => {
+  //   deleteTrigger(data.id).then(() => {
+  //     const filteredList = roomImageList.filter(
+  //       (image: CImageData) => image.id !== data.id,
+  //     );
+  //     fetchRoomImage([...filteredList], false);
+  //   });
+  // }, [data]);
+
+  const onClickShowAlertBox = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    mutateModalState({ ...showModalState, alert: true });
+    mutateDeleteImageInfo({ id: data.id });
+  };
+
+  const onClickPictureInfo = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    mutateModalState({ ...showModalState, detailPicture: true });
+    mutateDetailImageInfo({ index, data });
+  };
 
   return (
-    <ContentBox
-      key={data.id}
-      ref={thisArr.length - 1 === index ? observerRef : undefined}
-    >
-      <ImageCard>
-        <img src={data.link} />
-      </ImageCard>
-      <InfoContainer>
-        <ImageInfo>
-          <div>
-            <span>작성자: {data.user_name}</span>
-          </div>
-          <div>
-            <span>작성일: {data.created_at}</span>
-          </div>
-        </ImageInfo>
-        {data.user_id + '' === sessionStorage.getItem('USER_ID') && (
-          <EditBtn onClick={onClickEditBtn}>
-            <BsThreeDotsVertical />
-          </EditBtn>
+    <>
+      <ContentBox
+        key={data.id}
+        ref={thisArr.length - 1 === index ? observerRef : undefined}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <ImageCard>
+          <img src={data.link} />
+        </ImageCard>
+        <InfoContainer>
+          <ImageInfo>
+            <div>
+              <span>작성자: {data.user_name}</span>
+            </div>
+            <div>
+              <span>작성일: {data.created_at}</span>
+            </div>
+          </ImageInfo>
+        </InfoContainer>
+        {isHover && (
+          <HoverBox>
+            <div className="btn_group">
+              <Button type="button" onClick={onClickPictureInfo}>
+                자세히 보기
+              </Button>
+              {data.user_id + '' === sessionStorage.getItem('USER_ID') && (
+                <Button
+                  type="button"
+                  className="error"
+                  onClick={onClickShowAlertBox}
+                >
+                  게시물 삭제하기
+                </Button>
+              )}
+            </div>
+          </HoverBox>
         )}
-        {showEditBox && (
-          <EditImageMenu ref={imageDeleteEl}>
-            <p className="delete_img" onClick={deleteImage}>
-              이미지 삭제하기
-            </p>
-          </EditImageMenu>
-        )}
-      </InfoContainer>
-    </ContentBox>
+      </ContentBox>
+    </>
   );
 };
 
