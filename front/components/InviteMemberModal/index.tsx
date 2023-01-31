cd77c28774ba56cee1b97df09ff379903131c56f
kanghunK <ltbehappy16@gmail.com>
2023-01-31 10:03:42 +0900

    Modify: 선택한 친구들 목록이 화면에 보이고 한번에 요청 보내도록 수정

--- front/components/InviteMemberModal/index.tsx
+++ front/components/InviteMemberModal/index.tsx
@@ -1,66 +1,146 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import useSWR, { useSWRConfig } from 'swr';
-import { AiOutlineClose } from 'react-icons/ai';
-import { CloseBtn, Container, Content, ListBox, Title, Wrapper } from './styles';
+import {
+  AiFillCheckCircle,
+  AiOutlineCheckCircle,
+  AiOutlineClose,
+} from 'react-icons/ai';
+import {
+  CloseBtn,
+  Container,
+  Content,
+  ListBox,
+  Title,
+  Wrapper,
+} from './styles';
 import { getUserFriendList } from '@utils/userDataFetcher';
 import { DFriendData } from '@typing/db';
 import useSWRMutation from 'swr/mutation';
-import { inviteFriendFetcher } from '@utils/roomDataFetcher';
+import {
+  getUserListFetcher,
+  inviteFriendFetcher,
+} from '@utils/roomDataFetcher';
+import { useParams } from 'react-router';
+import { AxiosError } from 'axios';
+
+type AppendCheckFriendData = DFriendData & { check: boolean };
 
 const InviteMemberModal = () => {
-  const { data: roomId } = useSWR('roomId');
-  const { data: modalState, mutate: modalMutate } = useSWR('showModalState');
-  const { data: friendList } = useSWR('friendlist', getUserFriendList, {
+  const { roomId } = useParams<{ roomId: string }>();
+  const { data: showModalState, mutate: modalMutate } =
+    useSWR('showModalState');
+
+  const { data: userFriendList } = useSWR('friendlist', getUserFriendList, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   });
-  const { trigger, isMutating } = useSWRMutation('inviteFriend', inviteFriendFetcher);
+  const { data: roomFreindList, mutate: userlistMutate } = useSWR(
+    ['userlist', roomId],
+    getUserListFetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  );
+  const { trigger: inviteFriendsTrigger } = useSWRMutation(
+    `/room/${roomId}/user`,
+    inviteFriendFetcher,
+  );
   const { mutate } = useSWRConfig();
 
-  const onClickCloseBtn = useCallback(() => {
-    modalMutate(
-      {
-        ...modalState,
-        invite_member: false,
-      },
-      false,
-    );
-  }, []);
+  const [notInvitedFriends, setNotInvitedFriends] = useState<
+    AppendCheckFriendData[]
+  >([]);
 
-  const onClickInviteBtn = useCallback(
-    (friendId: string) => async () => {
-      await trigger({ invite_userlist: [friendId], roomId });
-      await mutate(['userlist', roomId]);
-    },
-    [],
-  );
+  useEffect(() => {
+    if (!userFriendList || !roomFreindList) return;
+
+    const newList = userFriendList.filter((userInfo: DFriendData) => {
+      for (const roomUserInfo of roomFreindList) {
+        if (roomUserInfo.id === userInfo.id) return false;
+      }
+      return true;
+    });
+
+    const appendCheckList = newList.map((data: DFriendData) => {
+      return { ...data, check: false };
+    });
+
+    setNotInvitedFriends([...appendCheckList]);
+  }, [userFriendList, roomFreindList]);
+
+  const getSelectFriends = useCallback(() => {
+    const selectMemberName = notInvitedFriends
+      .filter((data) => data.check)
+      .map((data) => data.name);
+    return selectMemberName.join(' ');
+  }, [notInvitedFriends]);
+
+  const onClickFriendList = (dataId: string) => () => {
+    const newData = notInvitedFriends.map((data) => {
+      if (data.id === dataId) {
+        return { ...data, check: !data.check };
+      }
+      return { ...data };
+    });
+    setNotInvitedFriends([...newData]);
+  };
+
+  const onClickInviteFriends = useCallback(() => {
+    const selectDataId = notInvitedFriends
+      .filter((data) => data.check)
+      .map((data) => data.id);
+
+    if (selectDataId.length === 0) {
+      alert('선택된 친구가 없습니다.');
+    } else {
+      inviteFriendsTrigger(selectDataId)
+        .then(() => {
+          modalMutate({ ...showModalState, invite_member: false });
+          mutate(['userlist', roomId]);
+        })
+        .catch((err) => {
+          if (err instanceof AxiosError) {
+            alert('오류가 발생했습니다..');
+          }
+        });
+    }
+  }, [notInvitedFriends]);
 
   return (
     <Wrapper>
       <Container>
         <Title>
-          <span>친구 초대하기</span>
-          <CloseBtn onClick={onClickCloseBtn}>
-            <AiOutlineClose />
-          </CloseBtn>
+          <h1>친구 초대하기</h1>
         </Title>
         <Content>
           <ListBox>
+            <h2>초대할 친구 목록</h2>
             <ul>
-              {friendList &&
-                friendList.map((data: DFriendData) => (
-                  <li key={data.id}>
+              {notInvitedFriends &&
+                notInvitedFriends.map((data: AppendCheckFriendData) => (
+                  <li key={data.id} onClick={onClickFriendList(data.id)}>
                     <div>
+                      {data.check ? (
+                        <AiFillCheckCircle />
+                      ) : (
+                        <AiOutlineCheckCircle />
+                      )}
                       <p>{data.name}</p>
                       <p>{data.email}</p>
-                      <button type="button" onClick={onClickInviteBtn(data.id)}>
-                        초대하기
-                      </button>
                     </div>
                   </li>
                 ))}
             </ul>
+            <article>
+              <h2>선택한 친구들</h2>
+              <p>{getSelectFriends()}</p>
+            </article>
+            <button type="button" onClick={onClickInviteFriends}>
+              초대하기
+            </button>
           </ListBox>
         </Content>
       </Container>
