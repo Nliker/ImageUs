ffd4711eeb32ae6f18cde90bd94b289b3b06bb51
kanghunK <ltbehappy16@gmail.com>
2023-02-01 11:38:48 +0900

    Modify: AppLayout에서 SideBar를 관리하도록 수정 및 styled component 네이밍 수정

--- front/layouts/AppLayout/index.tsx
+++ front/layouts/AppLayout/index.tsx
@@ -1,37 +1,55 @@
-import React, { useCallback, useState } from 'react';
-import ToolBar from '@components/ToolBar';
-import SideBar from '@components/SideBar';
-import { Container, ContentWrapper, ModalWrapper, Wrapper } from './styles';
+import React, { useCallback, useRef, useState } from 'react';
+import useSWR from 'swr';
+import { useMediaQuery } from 'react-responsive';
 import TopNavBar from '@components/TopNavBar';
 import BottomNavBar from '@components/BottomNavBar';
-import { useMediaQuery } from 'react-responsive';
 import UploadModal from '@components/UploadModal';
-import ContentImageModal from '@components/ContentImageModal';
-import useSWR from 'swr';
-import { Route, useLocation } from 'react-router';
 import CreateRoomModal from '@components/CreateRoomModal';
 import InviteMemberModal from '@components/InviteMemberModal';
 import AlertBox from '@components/AlertBox';
 import DetailPictureInfo from '@components/DetailPictureInfo';
 import ModalLayout from '@layouts/ModalLayout';
+import {
+  OuterContainer,
+  InnerContainer,
+  ModalWrapper,
+  Wrapper,
+} from './styles';
+import ToolBar from '@components/ToolBar';
+import SideBar from '@components/SideBar';
 
 interface AppLayoutProps {
   children?: React.ReactNode;
   roomId?: string;
+  isImageRoom?: boolean;
 }
 
-const AppLayout = ({ children }: AppLayoutProps) => {
+const AppLayout = ({ children, isImageRoom }: AppLayoutProps) => {
   const { data: showModalState } = useSWR('showModalState');
+
   const [showSideBar, setshowSideBar] = useState<boolean>(false);
   const isMobile = useMediaQuery({ maxWidth: 1023 });
 
+  const toggleSidebar = useCallback(() => {
+    setshowSideBar((prev) => !prev);
+  }, [showSideBar]);
+
+  const closeSidebar = useCallback(() => {
+    setshowSideBar(false);
+  }, [showSideBar]);
+
   return (
     <Wrapper>
-      <Container showModal={showModalState}>
-        <TopNavBar />
-        {isMobile && <BottomNavBar />}
-        <ContentWrapper show={showSideBar}>{children}</ContentWrapper>
-      </Container>
+      <OuterContainer showModal={showModalState}>
+        {isMobile ? <BottomNavBar /> : <TopNavBar />}
+        <InnerContainer>
+          {isMobile && (
+            <ToolBar handleSidebar={toggleSidebar} isImageRoom={isImageRoom} />
+          )}
+          {isImageRoom && <SideBar show={showSideBar} close={closeSidebar} />}
+          {children}
+        </InnerContainer>
+      </OuterContainer>
       {showModalState?.detailPicture && (
         <ModalLayout modalName={'detailPicture'}>
           <DetailPictureInfo />
