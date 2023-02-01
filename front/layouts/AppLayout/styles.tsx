32a5136400b2a2ebfb55e2766838e634e8207bc7
kanghunK <ltbehappy16@gmail.com>
2023-02-01 11:38:48 +0900

    Modify: AppLayout에서 SideBar를 관리하도록 수정 및 styled component 네이밍 수정

--- front/layouts/AppLayout/styles.tsx
+++ front/layouts/AppLayout/styles.tsx
@@ -2,12 +2,12 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
   height: 100%;
-  overflow-x: hidden;
-  overflow-y: hidden;
+
+  overflow: hidden;
   user-select: none;
 `;
 
-export const Container = styled.div<{
+export const OuterContainer = styled.div<{
   showModal:
     | undefined
     | {
@@ -21,6 +21,7 @@ export const Container = styled.div<{
 }>`
   height: 100%;
   width: 100%;
+
   ${({ showModal }) =>
     (showModal?.upload ||
       showModal?.image ||
@@ -33,20 +34,12 @@ export const Container = styled.div<{
   `}
 `;
 
-export const ContentWrapper = styled.div<{ show: boolean }>`
+export const InnerContainer = styled.div`
   width: 100%;
   height: calc(100vh - 55px);
-  background-color: rgb(237, 241, 245);
-  overflow-y: auto;
 
-  transition: transform 0.5s;
-  transform: translateX(0px);
-  ${({ show }) =>
-    show &&
-    `
-  transition: transform 0.5s;
-  transform: translate3d(300px, 0, 0);
-  `}
+  overflow-y: auto;
+  background-color: rgb(255, 255, 255);
 `;
 
 export const ModalWrapper = styled.div`
