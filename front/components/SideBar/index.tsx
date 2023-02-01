648d810572b5c81b16d833ed582621a0096d605f
kanghunK <ltbehappy16@gmail.com>
2023-02-01 11:36:05 +0900

    Add: 사이드바 박스 외부 클릭 시 닫히는 기능 추가

--- front/components/SideBar/index.tsx
+++ front/components/SideBar/index.tsx
@@ -1,21 +1,60 @@
 import ChannelList from '@components/ChannelList';
 import CreateRoomModal from '@components/CreateRoomModal';
 import MemberList from '@components/MemberList';
-import React, { memo, useCallback, useState } from 'react';
+import React, {
+  memo,
+  MutableRefObject,
+  RefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { useParams } from 'react-router';
 import { Route, Routes } from 'react-router';
 import useSWR, { useSWRConfig } from 'swr';
 import ActionButton from './ActionButton';
-import { ContentTabs, ContentWrapper, CreateBtn, CreateBtnBox, Tab, Wrapper } from './styles';
+import {
+  Background,
+  ContentTabs,
+  ContentWrapper,
+  CreateBtnBox,
+  Tab,
+  Wrapper,
+} from './styles';
 
 interface SidebarProps {
   show: boolean;
-  roomId?: string;
   close: () => void;
 }
 
-const SideBar = memo(({ show, roomId, close }: SidebarProps) => {
-  const { data: showModalState, mutate: showModalMutate } = useSWR('showModalState');
-  const { mutate } = useSWRConfig();
+const SideBar = memo(({ show, close }: SidebarProps) => {
+  const { data: showModalState, mutate: showModalMutate } =
+    useSWR('showModalState');
+  const { roomId } = useParams<{ roomId: string }>();
+  const sideBarEl = useRef<HTMLDivElement>(null);
+  const backgroundEl = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    console.log(backgroundEl);
+    backgroundEl.current?.addEventListener('click', handleCloseSidebar);
+    return () => {
+      backgroundEl.current?.removeEventListener('click', handleCloseSidebar);
+    };
+  }, [backgroundEl.current, show]);
+
+  const handleCloseSidebar = useCallback(
+    (e: MouseEvent) => {
+      console.log('클로즈', backgroundEl);
+      if (
+        e.target instanceof HTMLElement &&
+        !sideBarEl.current?.contains(e.target)
+      ) {
+        close();
+      }
+    },
+    [show],
+  );
 
   const onClickCreateRoomBtn = useCallback(() => {
     showModalMutate({
@@ -29,43 +68,53 @@ const SideBar = memo(({ show, roomId, close }: SidebarProps) => {
       ...showModalState,
       invite_member: true,
     });
-  }, []);
-
-  // console.log('사이드바 룸아이디:', roomId);
+  }, [showModalState]);
 
   return (
-    <Wrapper show={show}>
-      <ContentWrapper>
-        <ContentTabs>
-          {/* 라디오 그룹을 name 이름이 동일하게 그룹으로 묶고 
-          id로 구분해준다. 그러면 하나의 그룹 안에서 한가지만 선택할 수 있게 된다. */}
-          <Tab>
-            <input type="radio" id="tab-1" name="tab-group-1" defaultChecked />
-            <label htmlFor="tab-1">방 목록</label>
-            <div className="tab_content">
-              <div className="tab_content_box">
-                <CreateBtnBox>
-                  <ActionButton onClickBtn={onClickCreateRoomBtn} btnTitle={'생성하기'} />
-                </CreateBtnBox>
-                <ChannelList closeSidebar={close} />
+    <>
+      {show && <Background ref={backgroundEl} />}
+      <Wrapper show={show} ref={sideBarEl}>
+        <ContentWrapper>
+          <ContentTabs>
+            <Tab>
+              <input
+                type="radio"
+                id="tab-1"
+                name="tab-group-1"
+                defaultChecked
+              />
+              <label htmlFor="tab-1">방 목록</label>
+              <div className="tab_content">
+                <div className="tab_content_box">
+                  <CreateBtnBox>
+                    <ActionButton
+                      onClickBtn={onClickCreateRoomBtn}
+                      btnTitle={'생성하기'}
+                    />
+                  </CreateBtnBox>
+                  <ChannelList closeSidebar={close} />
+                </div>
               </div>
-            </div>
-          </Tab>
-          <Tab>
-            <input type="radio" id="tab-2" name="tab-group-1" />
-            <label htmlFor="tab-2">멤버목록</label>
-            <div className="tab_content">
-              <div className="tab_content_box">
-                <CreateBtnBox>
-                  <ActionButton onClickBtn={onClickInviteMember} btnTitle={'초대하기'} />
-                </CreateBtnBox>
-                <MemberList roomId={roomId} />
+            </Tab>
+            <Tab>
+              <input type="radio" id="tab-2" name="tab-group-1" />
+              <label htmlFor="tab-2">멤버목록</label>
+              <div className="tab_content">
+                <div className="tab_content_box">
+                  <CreateBtnBox>
+                    <ActionButton
+                      onClickBtn={onClickInviteMember}
+                      btnTitle={'초대하기'}
+                    />
+                  </CreateBtnBox>
+                  <MemberList roomId={roomId} />
+                </div>
               </div>
-            </div>
-          </Tab>
-        </ContentTabs>
-      </ContentWrapper>
-    </Wrapper>
+            </Tab>
+          </ContentTabs>
+        </ContentWrapper>
+      </Wrapper>
+    </>
   );
 });
 
