b7f99d9a3a9d4186a97b1ac747039ed093fd28bc
kanghunK <ltbehappy16@gmail.com>
2023-02-01 11:36:57 +0900

    Modify: 툴바 호버 시에 박스 띄우기 기능 추가 및 css 수정

--- front/components/ToolBar/index.tsx
+++ front/components/ToolBar/index.tsx
@@ -4,10 +4,11 @@ import {
   LeftIcon,
   LogoutBtn,
   RightIcons,
-  SettingBox,
+  LeaveBox,
   UserBox,
   UserInfo,
   Wrapper,
+  IconBox,
 } from './styles';
 import { MdOutlineSpaceDashboard } from 'react-icons/md';
 import { useMediaQuery } from 'react-responsive';
@@ -22,61 +23,69 @@ import { Node } from 'typescript';
 import { useParams } from 'react-router';
 import { leaveRoomFetcher } from '@utils/roomDataFetcher';
 import { AxiosError } from 'axios';
+import { Button } from '@styles/Button';
 
 interface Props {
   handleSidebar?: (e: any) => void;
+  isImageRoom?: boolean;
 }
 
-const ToolBar = ({ handleSidebar }: Props) => {
-  const { data: isLogIn } = useSWR('/user/my');
-  const { roomId } = useParams<{ roomId: string | undefined }>();
-  const isMobile = useMediaQuery({ maxWidth: 1023 });
-  const [showUserBox, setShowUserBox] = useState<boolean | null>(null);
-  const [showSettingBox, setShowSettingBox] = useState<boolean | null>(null);
-  const navigate = useNavigate();
+const ToolBar = ({ handleSidebar, isImageRoom }: Props) => {
+  const { data: logInInfo } = useSWR('/user/my');
+  const [clickUserIcon, setClickUserIcon] = useState<boolean>(false);
+  const [hoverUserIcon, setHoverUserIcon] = useState<boolean>(false);
+  const [clickDoorIcon, setClickDoorIcon] = useState<boolean>(false);
+  const [hoverDoorIcon, setHoverDoorIcon] = useState<boolean>(false);
   const userInfoEl = useRef<HTMLDivElement>(null);
   const roomSettingEl = useRef<HTMLDivElement>(null);
+  const { roomId } = useParams<{ roomId: string | undefined }>();
+  const navigate = useNavigate();
+  const isMobile = useMediaQuery({ maxWidth: 1023 });
 
   useEffect(() => {
     window.addEventListener('click', handleCloseBox);
     return () => {
       window.removeEventListener('click', handleCloseBox);
     };
-  }, [showUserBox, showSettingBox]);
+  }, [clickUserIcon, clickDoorIcon]);
 
   const handleCloseBox = useCallback(
     (e: MouseEvent) => {
       if (
-        (e.target instanceof HTMLElement || e.target instanceof SVGElement) &&
-        showUserBox &&
+        e.target instanceof HTMLElement &&
+        clickUserIcon &&
         !userInfoEl.current?.contains(e.target)
       ) {
-        setShowUserBox(false);
+        setClickUserIcon(false);
+        setHoverUserIcon(false);
       }
 
       if (
-        (e.target instanceof HTMLElement || e.target instanceof SVGElement) &&
-        showSettingBox &&
+        e.target instanceof HTMLElement &&
+        clickDoorIcon &&
         !roomSettingEl.current?.contains(e.target)
       ) {
-        setShowSettingBox(false);
+        setClickDoorIcon(false);
+        setHoverDoorIcon(false);
       }
     },
-    [showUserBox, showSettingBox],
+    [clickUserIcon, clickDoorIcon],
   );
 
-  const onClickSettingBtn = useCallback(
-    () => setShowSettingBox(true),
-    [showSettingBox],
+  const onClickLeaveIcon = useCallback(
+    () => setClickDoorIcon((prev) => !prev),
+    [clickDoorIcon],
   );
 
-  const onClickUserBtn = useCallback(() => setShowUserBox(true), [showUserBox]);
+  const onMouseEnterDoorIcon = useCallback(() => {
+    if (clickDoorIcon) return;
+    setHoverDoorIcon(true);
+  }, [hoverDoorIcon, clickDoorIcon]);
 
-  const onClickLogOut = useCallback(() => {
-    sessionStorage.clear();
-    mutate('/user/my', false);
-    navigate('/');
-  }, []);
+  const onMouseLeaveDoorIcon = useCallback(() => {
+    if (clickDoorIcon) return;
+    setHoverDoorIcon(false);
+  }, [hoverDoorIcon, clickDoorIcon]);
 
   const onClickLeaveBtn = useCallback(() => {
     if (!roomId) {
@@ -96,61 +105,96 @@ const ToolBar = ({ handleSidebar }: Props) => {
       });
   }, []);
 
+  const onClickUserIcon = useCallback(
+    () => setClickUserIcon((prev) => !prev),
+    [clickUserIcon],
+  );
+
+  const onMouseEnterUserIcon = useCallback(() => {
+    if (clickUserIcon) return;
+    setHoverUserIcon(true);
+  }, [hoverUserIcon, clickUserIcon]);
+
+  const onMouseLeaveUserIcon = useCallback(() => {
+    if (clickUserIcon) return;
+    setHoverUserIcon(false);
+  }, [hoverUserIcon, clickUserIcon]);
+
+  const onClickLogOut = useCallback(() => {
+    sessionStorage.clear();
+    mutate('/user/my');
+    navigate('/');
+  }, []);
+
   return (
-    <Wrapper>
-      {isLogIn ? (
+    <Wrapper isImageRoom={isImageRoom}>
+      {logInInfo.logInState ? (
         <>
+          {isImageRoom && (
+            <LeftIcon className="toolbar_icon">
+              <div>
+                <span>
+                  <MdOutlineSpaceDashboard onClick={handleSidebar} />
+                </span>
+              </div>
+              <div>
+                <span
+                  onClick={onClickLeaveIcon}
+                  onMouseEnter={onMouseEnterDoorIcon}
+                  onMouseLeave={onMouseLeaveDoorIcon}
+                >
+                  <TbDoorExit />
+                </span>
+              </div>
+              {(clickDoorIcon || hoverDoorIcon) && (
+                <LeaveBox ref={roomSettingEl}>
+                  <p className="leave_room" onClick={onClickLeaveBtn}>
+                    방에서 나가기
+                  </p>
+                </LeaveBox>
+              )}
+            </LeftIcon>
+          )}
           <RightIcons className="toolbar_icon">
             <div>
-              <span onClick={onClickSettingBtn}>
-                <TbDoorExit />
-              </span>
-            </div>
-            <div>
-              <span onClick={onClickUserBtn}>
+              <span
+                onClick={onClickUserIcon}
+                onMouseEnter={onMouseEnterUserIcon}
+                onMouseLeave={onMouseLeaveUserIcon}
+              >
                 <BiUserCircle />
               </span>
             </div>
+            {(clickUserIcon || hoverUserIcon) && (
+              <UserBox ref={userInfoEl}>
+                <UserInfo>
+                  <div className={'info_words'}>
+                    <p>
+                      <strong>{logInInfo.user_info.name}</strong> 님 어서오세요!
+                    </p>
+                    <p>
+                      <strong>email:</strong> {logInInfo.user_info.email}
+                    </p>
+                  </div>
+                </UserInfo>
+                <LogoutBtn>
+                  <Button
+                    className="error"
+                    type="button"
+                    onClick={onClickLogOut}
+                  >
+                    로그아웃
+                  </Button>
+                </LogoutBtn>
+              </UserBox>
+            )}
           </RightIcons>
-          {roomId && (
-            <LeftIcon className="toolbar_icon">
-              <span>
-                <MdOutlineSpaceDashboard onClick={handleSidebar} />
-              </span>
-            </LeftIcon>
-          )}
         </>
       ) : (
         <div className="action_box">
           <Link to={'/login'}>로그인</Link>
           <Link to={'/signup'}>회원가입</Link>
         </div>
-      )}
-
-      {/* 로그아웃 상자 토글 버튼 */}
-      {showUserBox && (
-        <UserBox ref={userInfoEl}>
-          <UserInfo>
-            {/* <img src="image_test.png" alt="test_img" /> */}
-            <div className={'info_words'}>
-              <p>
-                <span>이름</span>님 어서오세요!
-              </p>
-              <p>asdfaskla@naver.com</p>
-            </div>
-          </UserInfo>
-          <LogoutBtn>
-            <span onClick={onClickLogOut}>로그아웃</span>
-          </LogoutBtn>
-        </UserBox>
-      )}
-
-      {showSettingBox && (
-        <SettingBox ref={roomSettingEl}>
-          <div className="leave_room" onClick={onClickLeaveBtn}>
-            <span>방에서 나가기</span>
-          </div>
-        </SettingBox>
       )}
     </Wrapper>
   );
