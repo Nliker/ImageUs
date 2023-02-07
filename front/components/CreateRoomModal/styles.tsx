6d393f9aaf992bb3cd0825909000a8265fbc8cc5
kanghunK <ltbehappy16@gmail.com>
2023-02-07 15:05:29 +0900

    Modify: 채널생성 모달 리액트 스크롤 추가 및 css 변경

--- front/components/CreateRoomModal/styles.tsx
+++ front/components/CreateRoomModal/styles.tsx
@@ -1,14 +1,7 @@
 import styled from '@emotion/styled';
 
 export const ModalBoxContainer = styled.div`
-  // position: absolute;
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
-
-  // width: 100%;
-  // height: 100%;
-  // z-index: 1002;
+  height: 100%;
 
   border-radius: 6px;
 `;
@@ -17,15 +10,23 @@ export const ModalBox = styled.div`
   display: flex;
   flex-direction: column;
 
+  height: 100%;
+  padding: 20px 0;
   border-radius: 12px;
+  box-sizing: border-box;
 
-  background-color: cadetblue;
+  background-color: white;
 `;
 
 export const Title = styled.div`
+  flex: 1;
   text-align: center;
+
+  height: 30%;
+
   font-size: 18px;
-  padding: 5px 0;
+  margin: 5px 0;
+
   span {
     display: inline-block;
   }
@@ -47,39 +48,108 @@ export const CloseBtn = styled.div`
 `;
 
 export const ContentBox = styled.div`
-  width: 250px;
-  padding: 20px;
-`;
-
-export const Content = styled.div``;
-
-export const RoomName = styled.div`
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  flex: 7;
+
+  height: 70%;
+  width: 100%;
+
+  box-sizing: border-box;
+
+  .result_content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    height: calc(100% - 60px);
+  }
 `;
 
-export const MemeberList = styled.div`
+export const Content = styled.div`
   display: flex;
   flex-direction: column;
 
-  padding: 1.5rem 0;
-  overflow-y: auto;
+  height: calc(100% - 60px);
+
+  overflow: auto;
+`;
+
+export const RoomName = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+
+  max-width: 50%;
+  margin: auto;
+  text-align: center;
+
+  & > label {
+    margin-right: 20px;
+  }
+`;
+
+export const MemeberList = styled.div`
+  flex: 6;
+
+  .member_list {
+    display: flex;
+    flex-direction: column;
+
+    max-width: 50%;
+    margin: auto;
+    padding: 1.5rem 0;
+
+    overflow-y: auto;
+
+    & .friend_info_row {
+      display: flex;
+      align-items: center;
+
+      gap: 3rem;
+      cursor: pointer;
+
+      &:hover {
+        background-color: whitesmoke;
+      }
+    }
+  }
 `;
 
 export const ActionBtn = styled.div`
-  display: inline-block;
-  float: right;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+
+  height: 60px;
+  padding: 0 20px;
 `;
 
 export const ResultRoomName = styled.div`
-  flex: 1;
+  flex: 2;
+  display: flex;
+  align-items: center;
+
+  max-width: 80%;
+  gap: 3rem;
 `;
 
 export const ResultMembers = styled.div`
-  flex: 5;
+  flex: 6;
+  max-width: 80%;
+
+  ul li {
+    list-style: circle;
+  }
 `;
 
 export const ResultActionBtn = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 60px;
+  padding: 0 20px;
+  box-sizing: border-box;
 `;
