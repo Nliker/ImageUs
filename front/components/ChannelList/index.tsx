0a92c8f26b1598b2d6d8ec1014c94b197e2ace4e
kanghunK <ltbehappy16@gmail.com>
2023-02-07 15:03:41 +0900

    Modify:  CollapseListBox 컴포넌트 추가 및 채널 생성 버튼 추가

--- front/components/ChannelList/index.tsx
+++ front/components/ChannelList/index.tsx
@@ -1,23 +1,59 @@
-import EachChannel from '@components/EachChannel';
 import React, { memo, useCallback, useState } from 'react';
 import { IoMdArrowDropright } from 'react-icons/io';
 import useSWR from 'swr';
-import { Collapse, Container, Subtitle } from './styles';
+import { Collapse, Container, CreateBtnBox, Subtitle } from './styles';
 import { getUserRoomListFetcher } from '@utils/userDataFetcher';
 import { DRoomData } from '@typing/db';
+import { useNavigate } from 'react-router';
+import CollapseListBox from '@components/CollapseListBox';
+import { useParams } from 'react-router';
+import ActionButton from '@styles/ActiveButton';
 
 interface Props {
   closeSidebar: () => void;
 }
 
 const ChannelList = memo(({ closeSidebar }: Props) => {
-  const { data: roomlist, mutate: mutateRoomList } = useSWR('roomlist', getUserRoomListFetcher, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
-  const [channelCollapse, setChannelCollapse] = useState<boolean>(false);
-  const toggleChannelCollapse = useCallback(() => setChannelCollapse((prev) => !prev), []);
+  const { roomId } = useParams<{ roomId: string }>();
+  const { data: roomlist, mutate: mutateRoomList } = useSWR(
+    'roomlist',
+    getUserRoomListFetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  );
+  const [channelCollapse, setChannelCollapse] = useState<boolean>(true);
+  const navigate = useNavigate();
+  const { data: showModalState, mutate: showModalMutate } =
+    useSWR('showModalState');
+
+  const onClickRoom = useCallback((roomId: number) => {
+    // closeSidebar();
+    navigate(`/booth/${roomId}`);
+  }, []);
+
+  const toggleChannelCollapse = useCallback(
+    () => setChannelCollapse((prev) => !prev),
+    [],
+  );
+
+  const collapseListBoxData = useCallback(() => {
+    if (!roomlist) return [];
+    const data = roomlist.map((room: DRoomData) => ({
+      id: room.id,
+      data: room.title,
+    }));
+    return [...data];
+  }, [roomlist]);
+
+  const onClickCreateRoomBtn = useCallback(() => {
+    showModalMutate({
+      ...showModalState,
+      create_room: true,
+    });
+  }, [showModalState]);
 
   return (
     <Container>
@@ -27,12 +63,19 @@ const ChannelList = memo(({ closeSidebar }: Props) => {
         </Collapse>
         <span>Channels</span>
       </Subtitle>
-      <div>
-        {channelCollapse &&
-          roomlist?.map((room: DRoomData) => {
-            return <EachChannel key={room.id} room={room} closeSidebar={closeSidebar} />;
-          })}
-      </div>
+      {channelCollapse && (
+        <>
+          <CollapseListBox
+            data={collapseListBoxData()}
+            dataClickCallBack={onClickRoom}
+            currentDataId={roomId}
+            nameKey={'channel'}
+          />
+          <CreateBtnBox>
+            <ActionButton onClickBtn={onClickCreateRoomBtn} btnTitle={'+'} />
+          </CreateBtnBox>
+        </>
+      )}
     </Container>
   );
 });
