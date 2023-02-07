e5018030fbcc3b38ee4079fe23f1d3d0b0ccee4d
kanghunK <ltbehappy16@gmail.com>
2023-02-07 15:04:13 +0900

    Feat: CollapseListBox 컴포넌트 추가

---
+++ front/components/CollapseListBox/index.tsx
@@ -0,0 +1,60 @@
+import autosize from 'autosize';
+import React, { memo, useCallback, useEffect, useRef } from 'react';
+import { Container, DataCheckInput, DataLabel } from './styles';
+
+interface Props {
+  data: { id: number; data: string }[];
+  currentDataId?: number | string;
+  nameKey: string;
+  readOnly?: boolean;
+  dataClickCallBack?: (roomId: number) => void;
+}
+
+const CollapseListBox = ({
+  data,
+  nameKey,
+  currentDataId,
+  readOnly,
+  dataClickCallBack,
+}: Props) => {
+  const preventClickCSS = {
+    pointerEvents: readOnly
+      ? ('none' as React.CSSProperties['pointerEvents'])
+      : ('auto' as React.CSSProperties['pointerEvents']),
+  };
+
+  const onClickDataLabel = useCallback(
+    (id: number) => () => {
+      if (dataClickCallBack) dataClickCallBack(id);
+    },
+    [data],
+  );
+
+  return (
+    <Container style={preventClickCSS}>
+      {data.map((item) => {
+        console.log(currentDataId, item.id);
+        return (
+          <div key={item.id}>
+            <div className="check_label_box">
+              <DataCheckInput
+                type="radio"
+                id={`${item.id}`}
+                name={`radio-group-${nameKey}`}
+                defaultChecked={'' + currentDataId === '' + item.id}
+              />
+              <DataLabel
+                htmlFor={`${item.id}`}
+                onClick={onClickDataLabel(item.id)}
+              >
+                {item.data}
+              </DataLabel>
+            </div>
+          </div>
+        );
+      })}
+    </Container>
+  );
+};
+
+export default memo(CollapseListBox);
