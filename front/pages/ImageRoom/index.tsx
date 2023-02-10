a3ad23322ff5af97682a08884facd3d2a5f31a14
kanghunK <ltbehappy16@gmail.com>
2023-02-10 10:10:48 +0900

    Add: ContentSectionWrapper css 컴포넌트 추가

--- front/pages/ImageRoom/index.tsx
+++ front/pages/ImageRoom/index.tsx
@@ -3,6 +3,7 @@ import AppLayout from '@layouts/AppLayout';
 import React, { useRef } from 'react';
 import Scrollbars from 'react-custom-scrollbars';
 import { useParams } from 'react-router';
+import { ContentSectionWrapper } from './styles';
 
 const ImageRoom = () => {
   const { roomId } = useParams<{ roomId: string }>();
@@ -13,7 +14,9 @@ const ImageRoom = () => {
   return (
     <AppLayout isImageRoom>
       {/* 키를 줌으로서 룸아이디에 따라서 컴포넌트를 unmount 시키고 remount 시킬 수 있다. */}
-      <ContentSection key={roomId} roomId={roomId} ref={scrollbarRef} />
+      <ContentSectionWrapper>
+        <ContentSection key={roomId} roomId={roomId} ref={scrollbarRef} />
+      </ContentSectionWrapper>
     </AppLayout>
   );
 };
