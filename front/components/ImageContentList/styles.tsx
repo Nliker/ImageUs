77e570718749332f2a639122cb6001bae8b03118
kanghunK <ltbehappy16@gmail.com>
2023-02-13 11:32:10 +0900

    Modify: ImageLayout의 gap 수정

--- front/components/ImageContentList/styles.tsx
+++ front/components/ImageContentList/styles.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const ImageLayout = styled.div`
   display: grid;
   grid-template-columns: repeat(2, auto);
-  gap: 1rem;
+  gap: 2rem;
 
   @media screen and (min-width: 450px) {
     grid-template-columns: repeat(3, auto);
