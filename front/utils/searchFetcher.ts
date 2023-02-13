7afb0e3e3e2e7fb95705d9997f6d1a84085e5fe5
kanghunK <ltbehappy16@gmail.com>
2023-02-13 11:42:38 +0900

    Modify: 검색 데이터 5개만 보여주도록 수정

--- front/utils/searchFetcher.ts
+++ front/utils/searchFetcher.ts
@@ -1,13 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
-const searchFetcher = async (arg: Array<string>) => {
-    const [url, email] = [arg[0], arg[1]];
-    return axios.get(`${url}?email=${email}`).then((res) => {
-        const { result } = res.data;
-        return result;
-    }).catch((err) => {
-        console.log(err);
+const searchFetcher = async (url: string) => {
+  // const [url, email] = [arg[0], arg[1]];
+  return axios
+    .get(url)
+    .then((res) => {
+      const { result } = res.data;
+
+      const searchData = result.slice(0, 5);
+      return [...searchData];
+    })
+    .catch((err) => {
+      console.log(err);
     });
-}
+};
 
 export default searchFetcher;
