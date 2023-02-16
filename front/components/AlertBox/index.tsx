50e74f1d6ce4c804c5bb509abf6ef55e90f2109b
kanghunK <ltbehappy16@gmail.com>
2023-02-16 16:04:26 +0900

    Modify: 이미지 삭제 alert 박스 css 수정

--- front/components/AlertBox/index.tsx
+++ front/components/AlertBox/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@styles/Button';
 import { CImageData } from '@typing/client';
 import { deleteImageFetcher } from '@utils/roomDataFetcher';
 import { deleteUserImage } from '@utils/userDataFetcher';
@@ -14,16 +15,6 @@ const AlertBox = () => {
   const { data: showModalState, mutate: changeAlertState } =
     useSWR('showModalState');
   const { data: imageDataInfo } = useSWR('deleteImageInfo');
-  //   const { mutate: requestDeleteImage } = useSWR(
-  //     [`/room/${roomId}/image`, imageDataInfo.id],
-  //     deleteImageFetcher,
-  //     {
-  //       revalidateIfStale: false,
-  //       revalidateOnFocus: false,
-  //       revalidateOnReconnect: false,
-  //       revalidateOnMount: false,
-  //     },
-  //   );
   const alertBoxEl = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -44,7 +35,6 @@ const AlertBox = () => {
   };
 
   const deleteImage = async () => {
-    // if (!roomId || !imageDataInfo.id) return;
     console.log('삭제 요청');
 
     if (currentPath === '/my_page') {
@@ -61,17 +51,19 @@ const AlertBox = () => {
   return (
     <Wrapper ref={alertBoxEl}>
       <p>정말 삭제하시겠습니까?</p>
-      <button type="button" onClick={deleteImage}>
-        확인
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          changeAlertState({ ...showModalState, alert: false });
-        }}
-      >
-        취소
-      </button>
+      <div className="btn_group">
+        <Button type="button" onClick={deleteImage}>
+          확인
+        </Button>
+        <Button
+          type="button"
+          onClick={() => {
+            changeAlertState({ ...showModalState, alert: false });
+          }}
+        >
+          취소
+        </Button>
+      </div>
     </Wrapper>
   );
 };
--- front/components/AlertBox/styles.tsx
+++ front/components/AlertBox/styles.tsx
@@ -1,10 +1,28 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
   width: 300px;
   height: 200px;
-
   border-radius: 12px;
+  padding: 40px 30px;
 
+  box-sizing: border-box;
   background-color: white;
+  box-shadow: 0 0 15px 1px rgb(0 0 0 / 40%);
+
+  & > p {
+    text-align: center;
+
+    margin: 0;
+    font-size: 18px;
+  }
+
+  .btn_group {
+    display: flex;
+    justify-content: space-around;
+  }
 `;
