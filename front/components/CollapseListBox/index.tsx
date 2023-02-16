095c1990b1b1792dc2a9868b0944be055df4a2f0
kanghunK <ltbehappy16@gmail.com>
2023-02-16 16:08:10 +0900

    Add: 방에 있는 멤버 강퇴 버튼 추가

--- front/components/CollapseListBox/index.tsx
+++ front/components/CollapseListBox/index.tsx
@@ -4,9 +4,9 @@ import React, { memo, useCallback, useEffect, useRef } from 'react';
 import { Container, DataCheckInput, DataLabel } from './styles';
 
 interface Props {
-  data?: { id: number; data: string }[];
-  currentDataId?: number | string;
-  nameKey: string;
+  data: { id: number; data: string }[];
+  currentLoginId?: number | string;
+  boxInfo: { boxName: string; hostId?: number };
   readOnly?: boolean;
   dataClickCallBack?: (roomId: number) => void;
   onClickDeleteMember?: (memberId: number) => () => void;
@@ -14,8 +14,8 @@ interface Props {
 
 const CollapseListBox = ({
   data,
-  nameKey,
-  currentDataId,
+  boxInfo,
+  currentLoginId,
   readOnly,
   dataClickCallBack,
   onClickDeleteMember,
@@ -33,35 +33,62 @@ const CollapseListBox = ({
     [data],
   );
 
+  const labelMarginCSS = {
+    marginLeft: '60px',
+  };
+
   return (
     <Container>
-      {data ? (
-        data.map((item) => {
-          return (
-            <div key={item.id}>
-              <div className="check_label_box" style={preventClickCSS}>
-                <DataCheckInput
-                  type="radio"
-                  id={`${item.id}`}
-                  name={`radio-group-${nameKey}`}
-                  defaultChecked={'' + currentDataId === '' + item.id}
-                />
-                <DataLabel
-                  htmlFor={`${item.id}`}
-                  onClick={onClickDataLabel(item.id)}
+      {data.map((item) => {
+        return (
+          <div key={item.id} className="check_box">
+            <div className="check_label_box" style={preventClickCSS}>
+              <DataCheckInput
+                type="radio"
+                id={`${item.id}`}
+                name={`radio-group-${boxInfo.boxName}`}
+                defaultChecked={'' + currentLoginId === '' + item.id}
+                boxName={boxInfo.boxName}
+              />
+
+              <DataLabel
+                htmlFor={`${item.id}`}
+                onClick={onClickDataLabel(item.id)}
+              >
+                <div className="mark_box">
+                  {boxInfo?.hostId === item.id && (
+                    <span className="room_manager_mark">방장</span>
+                  )}
+                  {currentLoginId === item.id && (
+                    <span className="onself_mark">나</span>
+                  )}
+                </div>
+                <span
+                  className="item_text"
+                  style={
+                    boxInfo?.hostId === item.id || currentLoginId === item.id
+                      ? labelMarginCSS
+                      : undefined
+                  }
                 >
                   {item.data}
-                </DataLabel>
-              </div>
-              {nameKey === 'member' && onClickDeleteMember && (
-                <Button onClick={onClickDeleteMember(item.id)}>X</Button>
-              )}
+                </span>
+              </DataLabel>
             </div>
-          );
-        })
-      ) : (
-        <div>로딩중...</div>
-      )}
+            {boxInfo.boxName === 'member' &&
+              boxInfo?.hostId !== item.id &&
+              currentLoginId !== item.id &&
+              onClickDeleteMember && (
+                <Button
+                  className="error"
+                  onClick={onClickDeleteMember(item.id)}
+                >
+                  강퇴
+                </Button>
+              )}
+          </div>
+        );
+      })}
     </Container>
   );
 };
