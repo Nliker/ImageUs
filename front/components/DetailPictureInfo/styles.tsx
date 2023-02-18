29a678d7e47a84e4e4631d1eb1e1133977246d30
kanghunK <ltbehappy16@gmail.com>
2023-02-18 15:54:40 +0900

    Modify: 이미지 상세보기 레이아웃 및 css 수정

--- front/components/DetailPictureInfo/styles.tsx
+++ front/components/DetailPictureInfo/styles.tsx
@@ -1,16 +1,56 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
+  position: relative;
   display: flex;
-  // width: calc(100% - 100px);
-  height: 500px;
+  flex-direction: column;
+  justify-content: center;
 
+  height: 100%;
   border-radius: 12px;
+  box-shadow: 0 0 15px 1px rgb(0 0 0 / 40%);
 
   background-color: white;
+
+  .head_info {
+    position: absolute;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    top: 20px;
+
+    padding: 0 30px;
+    width: 100%;
+
+    box-sizing: border-box;
+    text-align: center;
+
+    & > span {
+      color: black;
+    }
+
+    .writer {
+      font-size: 1.5rem;
+    }
+
+    .name_info {
+      display: flex;
+
+      gap: 10px;
+      height: 100%;
+    }
+
+    .date_info {
+      display: flex;
+      align-items: center;
+
+      height: 100%;
+    }
+  }
 `;
 
 export const ImageSection = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   flex-basis: 50%;
