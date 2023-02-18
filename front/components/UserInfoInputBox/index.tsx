4d28f9781280df6dc0595e86b2d25921075efdb0
kanghunK <ltbehappy16@gmail.com>
2023-02-18 15:55:08 +0900

    Add: 로그인 창에 소셜 로그인 버튼 추가

--- front/components/UserInfoInputBox/index.tsx
+++ front/components/UserInfoInputBox/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { IconContext } from 'react-icons/lib';
+import { RiKakaoTalkFill } from 'react-icons/ri';
+import { SiNaver } from 'react-icons/si';
+import { NavLink } from 'react-router-dom';
 import { InnerContainer, OuterContainer, Wrapper } from './styles';
 
 interface InputBoxProps {
@@ -7,30 +11,41 @@ interface InputBoxProps {
 }
 
 const UserInfoInputBox = ({ children, pageName }: InputBoxProps) => {
-  // const { currentPage } = useParams<{ currentPage: string }>();
-  //   const [checked, setChecked] = useState(false);
-
-  //   const checkHandler = useCallback(() => {
-  //     return setChecked((prev) => !prev);
-  //   }, []);
-  // console.log(currentPage);
   return (
     <Wrapper>
       <OuterContainer>
         <h1>{pageName}</h1>
-        <InnerContainer>
-          {children}
-          {/* {pageName === 'login' ? (
-            <MoveBox>
-              <NavLink to={'/signup'}>계정 만들기</NavLink>
-              <button type="submit">확인</button>
-            </MoveBox>
-          ) : (
-            <MoveBox>
-              <button type="submit">제출</button>
-            </MoveBox>
-          )} */}
-        </InnerContainer>
+        <InnerContainer>{children}</InnerContainer>
+        <div className="social_sign-in_box">
+          <hr className="social_sign-in_line" />
+          <span className="social_sign-in_title">간편 로그인</span>
+          <div className="social_icon_box">
+            <button type="button" className="social_button kakao">
+              <a href={`/oauth-login?coperation=kakao`}>
+                <IconContext.Provider
+                  value={{
+                    size: '80%',
+                    style: { display: 'inline-block' },
+                  }}
+                >
+                  <RiKakaoTalkFill />
+                </IconContext.Provider>
+              </a>
+            </button>
+            <button type="button" className="social_button naver">
+              <a href="/oauth-login?coperation=naver">
+                <IconContext.Provider
+                  value={{
+                    size: '80%',
+                    style: { display: 'inline-block' },
+                  }}
+                >
+                  <SiNaver />
+                </IconContext.Provider>
+              </a>
+            </button>
+          </div>
+        </div>
       </OuterContainer>
     </Wrapper>
   );
