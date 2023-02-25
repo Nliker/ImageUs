370b805d4930bc80d21c56d5c823caca5c447a82
kanghunK <ltbehappy16@gmail.com>
2023-02-25 11:23:13 +0900

    Add: onClickUploadModal 작성 및 css 추가

--- front/pages/ImageRoom/index.tsx
+++ front/pages/ImageRoom/index.tsx
@@ -2,18 +2,44 @@ import ContentSection from '@components/ContentSection';
 import AppLayout from '@layouts/AppLayout';
 import React, { useRef } from 'react';
 import Scrollbars from 'react-custom-scrollbars';
+import { IconContext } from 'react-icons/lib';
+import { SlCloudUpload } from 'react-icons/sl';
 import { useParams } from 'react-router';
+import useSWR from 'swr';
 import { ContentSectionWrapper } from './styles';
 
 const ImageRoom = () => {
   const { roomId } = useParams<{ roomId: string }>();
+  const { data, mutate: showModalMutate } = useSWR('showModalState');
+
   console.log('룸 룸아이디:', roomId);
+
+  const onClickUploadModal = () => {
+    showModalMutate(
+      {
+        ...data,
+        upload: true,
+      },
+      false,
+    );
+  };
 
   return (
     <AppLayout isImageRoom>
       {/* 키를 줌으로서 룸아이디에 따라서 컴포넌트를 unmount 시키고 remount 시킬 수 있다. */}
       <ContentSectionWrapper>
         <ContentSection key={roomId} roomId={roomId} />
+        <div onClick={onClickUploadModal} className="upload_icon">
+          <IconContext.Provider
+            value={{
+              size: '100%',
+              style: { display: 'inline-block' },
+            }}
+          >
+            <SlCloudUpload />
+          </IconContext.Provider>
+          <span>업로드</span>
+        </div>
       </ContentSectionWrapper>
     </AppLayout>
   );
--- front/pages/ImageRoom/styles.tsx
+++ front/pages/ImageRoom/styles.tsx
@@ -1,5 +1,22 @@
 import styled from '@emotion/styled';
 
 export const ContentSectionWrapper = styled.div`
-  height: calc(100% - 40px);
+  height: 100%;
+
+  .upload_icon {
+    position: fixed;
+    top: 75%;
+    right: 12px;
+    text-align: center;
+
+    width: 40px;
+    padding: 7px;
+    border-radius: 5px;
+    font-size: 0.8rem;
+
+    color: #6296de;
+    background-color: ghostwhite;
+    box-shadow: 0px 1px 1px 2px #e0e6e6b3;
+    cursor: pointer;
+  }
 `;
