d02a3569f912fecc4e2404d867c6eee063a657ef
kanghunK <ltbehappy16@gmail.com>
2023-02-25 11:11:25 +0900

    Modify: 화면 크기에 따라 DetailPictureInfo 모달 유동적으로 변하도록 수정

--- front/components/DetailPictureInfo/index.tsx
+++ front/components/DetailPictureInfo/index.tsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { ImageSection, InfoSection, Wrapper } from './styles';
 import useSWR from 'swr';
 import { IconContext } from 'react-icons/lib';
 import { RiMapPinUserLine } from 'react-icons/ri';
+import { BiUserPin } from 'react-icons/bi';
 
 const DetailPictureInfo = () => {
   const { data: imageInfo } = useSWR('detailImageInfo');
+
+  const getCurrentModalSize = useCallback(() => {
+    const modalHeight = (window.innerHeight * 0.7).toFixed(3);
+    const modalWidth = Number.parseFloat(modalHeight).toFixed(3);
+
+    return {
+      modalWidth,
+      modalHeight,
+    };
+  }, [window.innerHeight]);
+
+  const [modalSize, setModalSize] = useState(getCurrentModalSize());
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    const handleResizeModal = () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        const { modalWidth, modalHeight } = getCurrentModalSize();
+
+        setModalSize({
+          modalWidth,
+          modalHeight,
+        });
+      }, 300);
+    };
+
+    window.addEventListener('resize', handleResizeModal);
+
+    return () => {
+      window.removeEventListener('resize', handleResizeModal);
+    };
+  }, []);
+
+  const imageSizeCSS = useMemo(
+    () => (imageLink: string) => {
+      const imageObj = new Image();
+      imageObj.src = imageLink;
+
+      const boxAspect = window.innerWidth <= 600 ? 1.0714 : 1.4285;
+      const imageAspect = imageObj.width / imageObj.height;
+
+      if (boxAspect <= imageAspect) {
+        // const imageWidthActual =
+        //   window.innerWidth <= 600
+        //     ? 280 * imageAspect
+        //     : parseFloat(modalSize.modalHeight) * 0.7 * imageAspect;
+        // const boxWidth =
+        //   window.innerWidth <= 600 ? 300 : parseFloat(modalSize.modalWidth);
+        // const marginLeft = -Math.round((imageWidthActual - boxWidth) / 2);
+
+        return {
+          width: 'auto',
+          height: '100%',
+        };
+      } else {
+        return {
+          width: '100%',
+          height: 'auto',
+        };
+      }
+    },
+    [imageInfo],
+  );
 
   if (!imageInfo.data) {
     alert('잘못된 이미지 정보입니다.');
@@ -13,16 +78,16 @@ const DetailPictureInfo = () => {
   }
 
   return (
-    <Wrapper>
+    <Wrapper modalSize={modalSize}>
       <div className="head_info">
         <div className="name_info">
           <IconContext.Provider
             value={{
-              size: '10%',
+              size: '30px',
               style: { display: 'inline-block' },
             }}
           >
-            <RiMapPinUserLine />
+            <BiUserPin />
           </IconContext.Provider>
           <span className="writer">{imageInfo.data.user_name}</span>
         </div>
@@ -31,7 +96,11 @@ const DetailPictureInfo = () => {
         </div>
       </div>
       <ImageSection>
-        <img src={imageInfo.data.link} alt={`${imageInfo.index}번째 이미지`} />
+        <img
+          src={imageInfo.data.link}
+          style={imageSizeCSS(imageInfo.data.link)}
+          alt={`${imageInfo.index}번째 이미지`}
+        />
       </ImageSection>
     </Wrapper>
   );
