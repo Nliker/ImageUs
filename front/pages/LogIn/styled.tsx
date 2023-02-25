7c38c03789e4e24c40ccf2b78978999fe4a898a6
kanghunK <ltbehappy16@gmail.com>
2023-02-25 11:25:01 +0900

    Feat: 소셜 로그인 박스  추가

--- front/pages/LogIn/styled.tsx
+++ front/pages/LogIn/styled.tsx
@@ -48,12 +48,75 @@ export const CheckBox = styled.div`
 
 export const SubmitBox = styled.div`
   display: flex;
+  align-items: center;
   justify-content: space-between;
+
   margin-top: 40px;
+
+  .signup_link {
+    line-height: 1.38;
+    letter-spacing: -0.3px;
+    font-weight: 400;
+
+    color: #616568;
+    border-bottom: 1px solid #858a8d;
+    text-decoration: none;
+    cursor: pointer;
+  }
 `;
 
-// export const MoveBox = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   margin-top: 40px;
-// `;
+export const SocialLoginBox = styled.div`
+  margin-top: 13px;
+
+  .social_icon_box {
+    display: flex;
+    justify-content: center;
+
+    padding: 12px 0;
+    gap: 5rem;
+
+    .social_icon_a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      width: 40px;
+      height: 40px;
+      border-radius: 4px;
+    }
+
+    .kakao_icon {
+      color: black;
+      background-color: #fae500;
+    }
+
+    .naver_icon {
+      color: white;
+      background-color: #15c654;
+    }
+  }
+
+  .social_sign-in_title {
+    position: relative;
+
+    padding: 0 8px;
+    margin-bottom: 16px;
+    font-size: 14px;
+    line-height: 16px;
+    letter-spacing: -0.3px;
+    color: #b8b8b8;
+    z-index: 11;
+    background-color: #fff;
+  }
+
+  .social_sign-in_line {
+    position: relative;
+    bottom: -12px;
+    display: block;
+    margin: 0;
+    width: 100%;
+    height: 1px;
+    background-color: #b8b8b8;
+    border: none;
+  }
+`;
