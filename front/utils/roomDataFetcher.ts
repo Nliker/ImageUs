35f2c21ea8c3e31612d82189cfff3b179b439e77
kanghunK <ltbehappy16@gmail.com>
2023-03-12 11:54:52 +0900

    Delete: getImageData, getMarkerFetcher 패쳐 제거 및 오류 메시지 수정

--- front/utils/roomDataFetcher.ts
+++ front/utils/roomDataFetcher.ts
@@ -1,11 +1,6 @@
-import { CImageData } from '@typing/client';
 import { DImageData } from '@typing/db';
-import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import axios, { AxiosError } from 'axios';
 import { getToken } from './getToken';
-
-interface AxiosCustomRequestConfig extends AxiosRequestConfig {
-  retryCount: number;
-}
 
 const getDefaultImgFetcher = async (
   url: string,
@@ -18,7 +13,7 @@ const getDefaultImgFetcher = async (
   },
 ) => {
   try {
-    const token = await getToken();
+    const { token } = await getToken();
 
     if (!token) {
       throw new Error();
@@ -28,7 +23,7 @@ const getDefaultImgFetcher = async (
     const loadNumber = 12;
 
     const response = await axios.get(
-      `${url}?start=${start}&limit=${loadNumber}`,
+      '/backapi' + `${url}?start=${start}&limit=${loadNumber}`,
       {
         headers: {
           Authorization: token,
@@ -41,9 +36,7 @@ const getDefaultImgFetcher = async (
 
     return { imagelist: [...newDataList], loadDataLength: imagelist.length };
   } catch (err) {
-    if (err instanceof AxiosError) {
-      alert('오류가 발생했습니다..');
-    }
+    alert('이미지정보를 받아오지 못했습니다..');
     return {};
   }
 };
@@ -53,7 +46,7 @@ const getFilterImgFetcher = async (
   { arg }: { arg: { start: number; start_date?: string; end_date?: string } },
 ) => {
   try {
-    const token = await getToken();
+    const { token } = await getToken();
 
     if (!token) {
       throw new Error();
@@ -63,7 +56,8 @@ const getFilterImgFetcher = async (
     const limit = 12;
 
     const response = await axios.get(
-      `${url}?start=${start}&limit=${limit}&start_date=${start_date}&end_date=${end_date}`,
+      '/backapi' +
+        `${url}?start=${start}&limit=${limit}&start_date=${start_date}&end_date=${end_date}`,
       {
         headers: {
           Authorization: token,
@@ -76,99 +70,29 @@ const getFilterImgFetcher = async (
 
     return { imagelist: [...newDataList], loadDataLength: imagelist.length };
   } catch (err) {
-    if (err instanceof AxiosError) {
-      alert('오류가 발생했습니다..');
-    }
+    alert('이미지정보를 받아오지 못했습니다..');
     return {};
   }
 };
 
-const getImageData = async (
-  url: string,
-  { arg: imageList }: { arg: DImageData[] },
-) => {
-  try {
-    const token = await getToken();
-
-    if (!token) {
-      throw new Error();
-    }
-
-    if (imageList.length === 0) return [];
-    const imgDataList: CImageData[] = [];
-
-    const MAX_RETRY_COUNT = 2;
-    const axiosObj = axios.create();
-
-    axiosObj.interceptors.response.use(undefined, (error: AxiosError) => {
-      const config = error.config as AxiosCustomRequestConfig;
-      config.retryCount = config.retryCount ?? 0;
-
-      const shouldRetry = config.retryCount < MAX_RETRY_COUNT;
-      if (shouldRetry) {
-        config.retryCount += 1;
-        config.headers = { ...config!.headers };
-
-        return axiosObj.request(config);
-      }
-
-      return Promise.reject(error);
-    });
-
-    const imgDataStateList = await Promise.allSettled(
-      imageList.map(async (imageInfo) => {
-        const requestConfig: AxiosRequestConfig = {
-          url: `/image-download/${imageInfo.link}`,
-          method: 'GET',
-          headers: { Authorization: token },
-          responseType: 'blob',
-        };
-
-        const response = await axiosObj.request(requestConfig);
-
-        const created_at =
-          imageInfo.created_at !== null
-            ? imageInfo.created_at.split(' ')[0]
-            : null;
-        const url = window.URL.createObjectURL(
-          new Blob([response.data], { type: response.headers['content-type'] }),
-        );
-        return { ...imageInfo, link: url, created_at };
-      }),
-    );
-
-    imgDataStateList.forEach((data) => {
-      if (data.status === 'fulfilled') {
-        imgDataList.push(data.value);
-      }
-    });
-
-    return [...imgDataList];
-  } catch (err) {
-    if (err instanceof AxiosError) {
-      console.log('이미지 로딩에 실패했습니다.');
-    }
-    return [];
-  }
-};
-
 const getUserListFetcher = async (url: string) => {
-  const token = await getToken();
+  const { token } = await getToken();
 
   if (!token) {
     throw new Error();
   }
 
   try {
-    const response = await axios.get(url, {
+    const response = await axios.get('/backapi' + url, {
       headers: {
         Authorization: token,
       },
     });
     const userlist = response.data.userlist;
     return userlist;
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    alert('친구목록을 받아오지 못했습니다..');
+    return;
   }
 };
 
@@ -179,14 +103,14 @@ const inviteFriendFetcher = async (
   try {
     if (invite_userlist.length === 0) return;
 
-    const token = await getToken();
+    const { token } = await getToken();
 
     if (!token) {
       throw new Error();
     }
 
     await axios.post(
-      url,
+      '/backapi' + url,
       {
         invite_userlist,
       },
@@ -197,49 +121,25 @@ const inviteFriendFetcher = async (
       },
     );
   } catch (err) {
-    if (err instanceof AxiosError) {
-      alert('오류가 발생했습니다..');
-    }
-  }
-};
-
-const getMarkerFetcher = async (url: string) => {
-  try {
-    const token = await getToken();
-
-    if (!token) {
-      throw new Error();
-    }
-
-    const response = await axios.get(url, {
-      headers: { Authorization: token },
-    });
-
-    const { marker } = response.data;
-    return marker;
-  } catch (err) {
-    if (err instanceof AxiosError) {
-      alert(err.response?.data.message);
-    }
-    return false;
+    alert('친구를 초대하지 못했습니다..');
+    return;
   }
 };
 
 const getUnreadImageList = async (url: string) => {
   try {
-    const token = await getToken();
+    const { token } = await getToken();
 
     if (!token) {
       throw new Error();
     }
-    const response = await axios.get(url, {
+    const response = await axios.get('/backapi' + url, {
       headers: { Authorization: token },
     });
     return [...response.data.imagelist];
   } catch (err) {
-    if (err instanceof AxiosError) {
-      alert('오류가 발생했습니다..');
-    }
+    console.error(err);
+    return;
   }
 };
 
@@ -248,13 +148,13 @@ const deleteRoomImgFetcher = async (
   { arg: imageId }: { arg: number },
 ) => {
   try {
-    const token = await getToken();
+    const { token } = await getToken();
 
     if (!token) {
       throw new Error();
     }
 
-    await axios.delete(url, {
+    await axios.delete('/backapi' + url, {
       headers: { Authorization: token },
       data: {
         delete_room_image_id: imageId,
@@ -264,12 +164,13 @@ const deleteRoomImgFetcher = async (
     return imageId;
   } catch (err) {
     if (err instanceof AxiosError) {
-      if (err.response?.status === 403 || err.response?.status === 404) {
-        alert(err.response.data);
-      } else {
-        alert('오류가 발생했습니다..');
+      if (err.response?.status === 404) {
+        alert(err.response.data.message);
       }
+    } else {
+      alert('이미지를 삭제하지 못하였습니다..');
     }
+    return;
   }
 };
 
@@ -278,7 +179,7 @@ const createRoomFetcher = async (
   { arg }: { arg: { selectMemberIdList: number[]; roomName: string } },
 ) => {
   try {
-    const token = await getToken();
+    const { token } = await getToken();
 
     if (!token) {
       throw new Error();
@@ -286,7 +187,7 @@ const createRoomFetcher = async (
 
     const { selectMemberIdList, roomName } = arg;
     await axios.post(
-      url,
+      '/backapi' + url,
       {
         userlist: selectMemberIdList,
         title: roomName,
@@ -300,9 +201,8 @@ const createRoomFetcher = async (
     );
     alert('방을 생성하였습니다.');
   } catch (err) {
-    if (err instanceof AxiosError) {
-      alert('오류가 발생했습니다..');
-    }
+    alert('방을 생성하지 못했습니다..');
+    return;
   }
 };
 
@@ -311,13 +211,13 @@ const deleteMemberFetcher = async (
   { arg: memberId }: { arg: number },
 ) => {
   try {
-    const token = await getToken();
+    const { token } = await getToken();
 
     if (!token) {
       throw new Error();
     }
 
-    await axios.delete(url, {
+    await axios.delete('/backapi' + url, {
       headers: { Authorization: token },
       data: {
         delete_room_user_id: memberId,
@@ -331,9 +231,9 @@ const deleteMemberFetcher = async (
     if (err instanceof AxiosError) {
       if (err.response?.status === 403) {
         alert('방장이 아닙니다.');
-      } else {
-        alert('오류가 발생했습니다..');
       }
+    } else {
+      alert('요청을 실패하였습니다..');
     }
     return false;
   }
@@ -344,8 +244,6 @@ export {
   getFilterImgFetcher,
   getUserListFetcher,
   inviteFriendFetcher,
-  getImageData,
-  getMarkerFetcher,
   getUnreadImageList,
   deleteRoomImgFetcher,
   createRoomFetcher,
