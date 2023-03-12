5d5a4499817533b417deef2e3196012154241143
Nliker <tjwjdgus83@naver.com>
2023-03-12 18:30:04 +0900

    소셜로그인에 ES 기입 추가

--- back/app.py
+++ back/app.py
@@ -73,7 +73,7 @@ def create_app(test_config=None):
     services.image_service=ImageService(image_dao,app.config)
     services.room_service=RoomService(room_dao,app.config)
 
-    oauth_router(api,services,app.config)
+    oauth_router(api,services,app.config,es)
     user_router(api,services,app.config,es)
     room_router(api,services)
     image_router(api,services)
