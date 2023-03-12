efb978be8fdafd7c6cfec6165a107f62b6e19712
kanghunK <ltbehappy16@gmail.com>
2023-03-12 12:06:05 +0900

    Add: 등록되지 않은 방에 접근 방지코드 추가

--- front/pages/ImageRoom/index.tsx
+++ front/pages/ImageRoom/index.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
-import { mutate } from 'swr';
-import { useParams } from 'react-router';
+import useSWR, { mutate } from 'swr';
+import { Navigate, useParams } from 'react-router';
 
 import { IconContext } from 'react-icons/lib';
 import { SlCloudUpload } from 'react-icons/sl';
 
+import { DRoomData } from '@typing/db';
 import AppLayout from '@layouts/AppLayout';
 import ContentSection from './Components/ContentSection';
 import { ContentSectionWrapper } from './styles';
+import { getUserRoomListFetcher } from '@utils/userDataFetcher';
 
 const ImageRoom = () => {
   const { roomId } = useParams<{ roomId: string }>();
+  const userId = sessionStorage.getItem('user_id');
+  const { data: roomListInfo } = useSWR(
+    `/user/${userId}/roomlist`,
+    getUserRoomListFetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  );
+
+  const checkValideRoomId = () => {
+    if (!roomListInfo) return false;
+
+    const isValidRoomId = roomListInfo.some((roomInfo: DRoomData) => {
+      return '' + roomInfo.id === roomId;
+    });
+
+    return isValidRoomId;
+  };
 
   const onClickUploadModal = () => {
     mutate('modalState', { currentModalState: 'upload' });
   };
+
+  if (!checkValideRoomId()) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <AppLayout isImageRoom>
