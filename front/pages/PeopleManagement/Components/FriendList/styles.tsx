cafe8f60fc53b88fd5198c5b9f923e9de011b7a6
kanghunK <ltbehappy16@gmail.com>
2023-03-14 15:10:41 +0900

    Modify: 친구 목록 css 수정

--- front/pages/PeopleManagement/Components/FriendList/styles.tsx
+++ front/pages/PeopleManagement/Components/FriendList/styles.tsx
@@ -1,15 +1,18 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-  table tr th,
-  table tbody td {
-    text-align: center;
+  table {
+    white-space: nowrap;
   }
 
   table tbody tr {
     height: 100px;
   }
 
+  table tr th,
+  table tbody td {
+    text-align: center;
+  }
   @media screen and (max-width: 850px) {
     .delete_btn button {
       width: 53px;
