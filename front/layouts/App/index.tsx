0cf9c18bcbcf1145f41ac8a3014f3be277c25f12
kanghunK <ltbehappy16@gmail.com>
2023-03-27 01:26:48 +0900

    Add: 페이지 이동시 로딩창 보이도록 fallback 추가

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -5,6 +5,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { logInCheckFetcher } from '@utils/logInFetcher';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
+import Spinner from '@styles/Spinner';
 
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
@@ -24,16 +25,25 @@ const App = () => {
 
   return (
     <Routes>
-      <Route index element={<MainPage />} />
+      <Route index element={<MainPage fallback={<Spinner />} />} />
       <Route element={<PublicRoute />}>
-        <Route path="login" element={<LogIn />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="callback/oauth-login" element={<SocialLogInAuth />} />
+        <Route path="login" element={<LogIn fallback={<Spinner />} />} />
+        <Route path="signup" element={<SignUp fallback={<Spinner />} />} />
+        <Route
+          path="callback/oauth-login"
+          element={<SocialLogInAuth fallback={<Spinner />} />}
+        />
       </Route>
       <Route element={<PrivateRoute />}>
-        <Route path="my_page/*" element={<MyPage />} />
-        <Route path="room/:roomId" element={<ImageRoom />} />
-        <Route path="people_management/*" element={<PeopleManagement />} />
+        <Route path="my_page/*" element={<MyPage fallback={<Spinner />} />} />
+        <Route
+          path="room/:roomId"
+          element={<ImageRoom fallback={<Spinner />} />}
+        />
+        <Route
+          path="people_management/*"
+          element={<PeopleManagement fallback={<Spinner />} />}
+        />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
