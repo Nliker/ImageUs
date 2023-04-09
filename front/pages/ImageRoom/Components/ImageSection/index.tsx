ef2192fc7a3ea2fd341405419480eb2f5e3d9449
kanghunK <ltbehappy16@gmail.com>
2023-04-09 15:38:46 +0900

    Rename: ImageContentList -> ImageSection으로 폴더명 변경

--- front/pages/ImageRoom/Components/ImageSection/index.tsx
+++ front/pages/ImageRoom/Components/ImageSection/index.tsx
@@ -3,16 +3,25 @@ import { IconContext } from 'react-icons/lib';
 import { FcRemoveImage } from 'react-icons/fc';
 
 import { CImageData } from '@typing/client';
-import ImageContent from '../ImageContent';
+import ImageContent from './ImageContent';
 import { ImageLayout, NotImageData } from './styles';
+import Spinner from '@styles/Spinner';
 
 interface Props {
   ImageData: CImageData[];
   sectionName?: string;
+  isLoading: boolean;
   observerRef?: React.MutableRefObject<null>;
+  keyString?: string;
 }
 
-const ImageContentList = ({ ImageData, observerRef, sectionName }: Props) => {
+const ImageSection = ({
+  ImageData,
+  isLoading,
+  observerRef,
+  sectionName,
+  keyString,
+}: Props) => {
   const imageCard = (
     data: CImageData,
     index: number,
@@ -27,10 +36,19 @@ const ImageContentList = ({ ImageData, observerRef, sectionName }: Props) => {
       observerRef={observerRef}
     />
   );
+
+  // if (isLoading || !ImageData) {
+  //   console.log('로딩: ', isLoading);
+  //   console.log('데이터: ', ImageData);
+  //   return <Spinner />;
+  // }
+
+  // console.log('이미지 리스트: ', ImageData);
+
   return (
     <>
       {ImageData.length !== 0 ? (
-        <ImageLayout>{ImageData.map(imageCard)}</ImageLayout>
+        <ImageLayout key={keyString}>{ImageData.map(imageCard)}</ImageLayout>
       ) : (
         <NotImageData>
           <IconContext.Provider
@@ -48,4 +66,4 @@ const ImageContentList = ({ ImageData, observerRef, sectionName }: Props) => {
   );
 };
 
-export default memo(ImageContentList);
+export default memo(ImageSection);
