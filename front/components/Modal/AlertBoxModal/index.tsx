d782f5f9c39737a015a86ef3a67697c9dafea519
kanghunK <ltbehappy16@gmail.com>
2023-04-21 10:25:10 +0900

    Modify: custom 훅을 활용한 swr 관리로 수정

--- front/components/Modal/AlertBoxModal/index.tsx
+++ front/components/Modal/AlertBoxModal/index.tsx
@@ -7,82 +7,48 @@ import { leaveRoomFetcher } from '@utils/userDataFetcher';
 import { Button } from '@styles/Button';
 import { Wrapper } from './styles';
 import { deleteUserImage } from '@utils/imageFetcher';
+import useModal from '@hooks/useModal';
+import useUserData from '@hooks/useUserData';
+import useRoomData from '@hooks/useRoomData';
+import useRoomlist from '@hooks/useRoomlist';
 
 const AlertBox = () => {
   const userId = sessionStorage.getItem('user_id');
-  const { roomId } = useParams<{ roomId: string }>();
+  const alertBoxEl = useRef<HTMLDivElement>(null);
+
+  const { currentModal, alertData, clearModalCache } = useModal();
+  const { refresh: refreshRoomlist } = useRoomlist();
+  const { leaveRoom, deleteStoreImage } = useUserData();
+  const { deleteRoomImage } = useRoomData();
   const navigate = useNavigate();
 
-  const { data: modalStateData } = useSWR('modalState');
-  const alertBoxEl = useRef<HTMLDivElement>(null);
-  const currentModalState = modalStateData?.currentModalState;
-  const propsData = modalStateData?.data;
-
-  const { trigger: deleteRoomImgTrigger } = useSWRMutation(
-    `/room/${roomId}/image`,
-    deleteRoomImgFetcher,
-  );
-  const { trigger: deleteUserImgTrigger } = useSWRMutation(
-    '/image',
-    deleteUserImage,
-  );
-  const { trigger: leaveRoomTrigger } = useSWRMutation(
-    `/user/${userId}/room`,
-    leaveRoomFetcher,
-  );
-
-  useEffect(() => {
-    window.addEventListener('click', handleCloseAlert);
-    return () => {
-      window.removeEventListener('click', handleCloseAlert);
-    };
-  }, []);
-
-  const handleCloseAlert = (e: MouseEvent) => {
-    if (
-      e.target instanceof HTMLElement &&
-      currentModalState === 'alert' &&
-      !alertBoxEl.current?.contains(e.target)
-    ) {
-      mutate('modalState', { currentModalState: '' });
-    }
-  };
-
   const executeFetch = () => {
-    if (propsData.mutateKey === `/room/${roomId}/image`) {
-      deleteRoomImgTrigger(propsData.imageId).then((dataId) => {
-        mutate('roomImageDelete', dataId);
+    if (alertData.type === 'deleteRoomImage') {
+      deleteRoomImage();
+    } else if (alertData.type === 'leaveRoom') {
+      leaveRoom().then(() => {
+        refreshRoomlist();
+        navigate('/room-select');
       });
-    } else if (propsData.mutateKey === `/user/${userId}/room`) {
-      leaveRoomTrigger(roomId).then(() => {
-        navigate('/');
-      });
-    } else {
-      deleteUserImgTrigger(propsData.imageId).then((dataId) => {
-        mutate('userImageDelete', dataId);
-      });
+    } else if (alertData.type === 'deleteStoreImage') {
+      deleteStoreImage();
     }
   };
 
   return (
     <Wrapper ref={alertBoxEl}>
-      <p>{propsData?.content}</p>
+      <p>{alertData.text}</p>
       <div className="btn_group">
         <Button
           type="button"
           onClick={() => {
             executeFetch();
-            mutate('modalState', { currentModalState: '' });
+            clearModalCache();
           }}
         >
           확인
         </Button>
-        <Button
-          type="button"
-          onClick={() => {
-            mutate('modalState', { currentModalState: '' });
-          }}
-        >
+        <Button type="button" onClick={() => clearModalCache()}>
           취소
         </Button>
       </div>
