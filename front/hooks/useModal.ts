9aa090b6dc9e6b30bd1e8c0e9b44dc562bacf32b
kanghunK <ltbehappy16@gmail.com>
2023-04-21 10:19:44 +0900

    Feat: useModal 훅 작성

---
+++ front/hooks/useModal.ts
@@ -0,0 +1,30 @@
+import useSWR, { useSWRConfig } from 'swr';
+
+interface IModalData {
+  currentModal?: string | null;
+  alertData?: { type: string; text: string };
+}
+
+function useModal() {
+  const { data, error, mutate: modalMutate } = useSWR('modal');
+  const { mutate } = useSWRConfig();
+
+  const setModal = (newData: IModalData) => {
+    modalMutate({ ...data, ...newData });
+  };
+
+  const clearModalCache = () => {
+    mutate('modal', undefined, { revalidate: false });
+  };
+
+  return {
+    currentModal: data?.currentModal ?? '',
+    alertData: data?.alertData ?? null,
+    error,
+    loading: !data && !error,
+    setModal,
+    clearModalCache,
+  };
+}
+
+export default useModal;
