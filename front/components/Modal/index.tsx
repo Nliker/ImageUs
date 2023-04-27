2b7bddd284e000f2cd7a142de14b1988062eaf2f
kanghunK <ltbehappy16@gmail.com>
2023-04-27 11:32:44 +0900

    Refactor: Modal 컴포넌트 조건부 렌더링으로 리팩토링

--- front/components/Modal/index.tsx
+++ front/components/Modal/index.tsx
@@ -1,47 +1,35 @@
 import React from 'react';
-
 import AlertBox from './AlertBoxModal';
 import CreateRoomModal from './CreateRoomModal';
 import DetailPictureInfo from './DetailPictureModal';
 import InviteMemberModal from './InviteMemberModal';
 import UploadModal from './UploadModal';
 import ModalLayout from './ModalLayout';
-import useModal from '@hooks/useModal';
-import { IModalData } from '@typing/client';
+import { IAertData, IDetailPictureInfo, IModalData } from '@typing/client';
 
-const Modal = () => {
-  const { data } = useModal();
+const Modal = ({ modalData }: { modalData: IModalData }) => {
+  const { state } = modalData;
+  const currentModal = modalData?.currentModal;
 
-  const currentModalComponent = (data: IModalData) => {
-    const { currentModal, uploadImageLocate, alertData, detailPictureInfo } =
-      data;
-
-    switch (currentModal) {
-      case 'detailPicture':
-        return <DetailPictureInfo imageInfo={detailPictureInfo} />;
-
-      case 'alert':
-        return <AlertBox alertData={alertData} />;
-
-      case 'upload':
-        return <UploadModal uploadImageLocate={uploadImageLocate} />;
-
-      case 'createRoom':
-        return <CreateRoomModal />;
-
-      case 'inviteMember':
-        return <InviteMemberModal />;
-
-      default:
-        break;
-    }
-  };
-
-  if (!data) return null;
+  if (state === 'off' || !currentModal) return null;
 
   return (
-    <ModalLayout currentModal={data.currentModal}>
-      {currentModalComponent(data)}
+    <ModalLayout currentModal={currentModal}>
+      {currentModal === 'detailPicture' ? (
+        <DetailPictureInfo
+          imageInfo={modalData.detailPictureInfo as IDetailPictureInfo}
+        />
+      ) : currentModal === 'alert' ? (
+        <AlertBox alertData={modalData.alertData as IAertData} />
+      ) : currentModal === 'upload' ? (
+        <UploadModal
+          uploadImageLocate={modalData.uploadImageLocate as string}
+        />
+      ) : currentModal === 'createRoom' ? (
+        <CreateRoomModal />
+      ) : (
+        <InviteMemberModal />
+      )}
     </ModalLayout>
   );
 };
