d0bac44bb689795d2d9d08aef063db244e51ea93
kanghunK <ltbehappy16@gmail.com>
2023-05-06 19:03:14 +0900

    Modify: 멤버 초대 모달 레이아웃 및 css 수정

--- front/components/Modal/InviteMemberModal/index.tsx
+++ front/components/Modal/InviteMemberModal/index.tsx
@@ -4,14 +4,14 @@ import { useParams } from 'react-router';
 import { Scrollbars } from 'react-custom-scrollbars-2';
 import { AiFillCheckCircle, AiOutlineCheckCircle } from 'react-icons/ai';
 
-import { DFriendData, DRoomData } from '@typing/db';
+import { DFriendData } from '@typing/db';
 import { Button } from '@styles/Button';
-import { Container, Content, Title, Wrapper } from './styles';
-import useUserData from '@hooks/useUserData';
+import { ContentBox } from './styles';
 import useModal from '@hooks/useModal';
 import useRoomList from '@hooks/useRoomList';
 import useFriendList from '@hooks/useFriendList';
 import useUserListByRoom from '@hooks/useUserListByRoom';
+import ModalLayout from '../ModalLayout';
 
 type AppendCheckFriendData = DFriendData & { check: boolean };
 
@@ -25,6 +25,7 @@ const InviteMemberModal = () => {
   const { friendList } = useFriendList();
   const { userListByRoom, inviteMemberToRoom } = useUserListByRoom(roomId);
 
+  const size = { width: 412, height: 550 };
   const [canInviteFriends, setCanInviteFriends] = useState<
     AppendCheckFriendData[]
   >(
@@ -78,77 +79,59 @@ const InviteMemberModal = () => {
   };
 
   return (
-    <Wrapper>
-      <Title>
-        <h2>친구 초대하기</h2>
-      </Title>
-      <Container>
-        <Scrollbars>
-          <Content>
-            <div className="content_box">
-              <div className="content_list_box">
-                <div className="content_subname">
-                  <h3>초대할 친구 목록</h3>
-                </div>
-                <div className="content_list">
-                  <Scrollbars>
-                    <ul className="content_list_ul">
-                      {canInviteFriends.length !== 0 ? (
-                        canInviteFriends.map((data: AppendCheckFriendData) => (
-                          <li
-                            key={data.id}
-                            onClick={onClickFriendList(data.id)}
-                          >
-                            <div className="list_layout">
-                              <div className="list_check_icon">
-                                {data.check ? (
-                                  <AiFillCheckCircle />
-                                ) : (
-                                  <AiOutlineCheckCircle />
-                                )}
-                              </div>
-                              <div className="list_info">
-                                <p>이름: {data.name}</p>
-                                <p>이메일: {data.email}</p>
-                              </div>
-                            </div>
-                          </li>
-                        ))
-                      ) : (
-                        <p>등록할 친구가 없습니다.</p>
-                      )}
-                    </ul>
-                  </Scrollbars>
-                </div>
-              </div>
-              <div className="selected_list_box">
-                <div className="content_subname">
-                  <h3>선택한 친구들</h3>
-                </div>
-                <div className="selected_list">
-                  <Scrollbars>
-                    {checkFriends.length !== 0 ? (
-                      <ul className="selected_list_ul">
-                        {checkFriends.map((data) => (
-                          <li key={data.id}>{data.name}</li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <p>선택한 친구가 없습니다.</p>
-                    )}
-                  </Scrollbars>
-                </div>
-              </div>
-              <div className="content_btn">
-                <Button type="button" onClick={requestInviteMember}>
-                  초대하기
-                </Button>
-              </div>
-            </div>
-          </Content>
-        </Scrollbars>
-      </Container>
-    </Wrapper>
+    <ModalLayout currentModal="inviteMember" size={size}>
+      <ContentBox>
+        <div className="friend_list">
+          <h3>초대할 친구 목록</h3>
+          <div className="ul_container">
+            <Scrollbars>
+              <ul>
+                {canInviteFriends.length !== 0 ? (
+                  canInviteFriends.map((data: AppendCheckFriendData) => (
+                    <li key={data.id} onClick={onClickFriendList(data.id)}>
+                      <div className="list_layout">
+                        <div className="list_check_icon">
+                          {data.check ? (
+                            <AiFillCheckCircle />
+                          ) : (
+                            <AiOutlineCheckCircle />
+                          )}
+                        </div>
+                        <div className="list_info">
+                          <p>이름: {data.name}</p>
+                          <p>이메일: {data.email}</p>
+                        </div>
+                      </div>
+                    </li>
+                  ))
+                ) : (
+                  <p>등록할 친구가 없습니다.</p>
+                )}
+              </ul>
+            </Scrollbars>
+          </div>
+        </div>
+        <div className="selected_list">
+          <h3>선택한 친구들</h3>
+          <div className="ul_container">
+            <Scrollbars>
+              <ul>
+                {checkFriends.length !== 0 ? (
+                  checkFriends.map((data) => <li key={data.id}>{data.name}</li>)
+                ) : (
+                  <p>선택한 친구가 없습니다.</p>
+                )}
+              </ul>
+            </Scrollbars>
+          </div>
+        </div>
+        <div className="content_btn">
+          <Button type="button" onClick={requestInviteMember}>
+            초대하기
+          </Button>
+        </div>
+      </ContentBox>
+    </ModalLayout>
   );
 };
 
