faf941b1165979ac8fd023ef183cde22339522b0
kanghunK <ltbehappy16@gmail.com>
2023-05-07 16:43:23 +0900

    Modify: 방 생성 모달 레이아웃 및 css 수정, 불필요한 코드 제거

--- front/components/Modal/CreateRoomModal/index.tsx
+++ front/components/Modal/CreateRoomModal/index.tsx
@@ -1,36 +1,22 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import useSWR, { useSWRConfig } from 'swr';
-import useSWRMutation from 'swr/mutation';
-import { AxiosError } from 'axios';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import { AiFillCheckCircle, AiOutlineCheckCircle } from 'react-icons/ai';
 import { Scrollbars } from 'react-custom-scrollbars-2';
 
 import { DFriendData } from '@typing/db';
 import useInput from '@hooks/useInput';
-import {
-  getUserFdListFetcher,
-  getUserRoomListFetcher,
-} from '@utils/userDataFetcher';
-import { createRoomFetcher } from '@utils/roomDataFetcher';
 import { Button } from '@styles/Button';
 import {
   ActionBtn,
   Content,
-  ContentBox,
+  CreateRoomInfo,
   MemeberList,
-  ModalBox,
-  ModalBoxContainer,
-  ResultActionBtn,
-  ResultMembers,
-  ResultRoomName,
   RoomNameInput,
-  Title,
 } from './styles';
 import useModal from '@hooks/useModal';
-import useUserData from '@hooks/useUserData';
-import { useParams } from 'react-router';
 import useRoomList from '@hooks/useRoomList';
+import ModalLayout from '../ModalLayout';
+import useFriendList from '@hooks/useFriendList';
 
 type AddCheckFriendData = DFriendData & { check: boolean };
 
@@ -39,21 +25,19 @@ const CreateRoomModal = () => {
   if (!userId) return null;
 
   const { clearModalCache } = useModal();
-  const { createRoom, fetchRoomList } = useRoomList(userId);
+  const { createRoom } = useRoomList(userId);
+  const { friendList } = useFriendList();
 
-  const { data: friendList } = useSWR('friendlist', getUserFdListFetcher, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
+  const size = { width: 412, height: 550 };
 
   const [currentStage, setCurrentStage] = useState(0);
   const [friendCheckList, setFriendCheckList] = useState<AddCheckFriendData[]>(
     [],
   );
-  const [selectListState, setSelectListState] = useState<AddCheckFriendData[]>(
-    [],
-  );
+  const checkFriends = useMemo(() => {
+    const filterList = friendCheckList.filter((data) => data.check);
+    return [...filterList];
+  }, [friendCheckList]);
   const [roomName, setRoomName, handleRoomName] = useInput('');
 
   useEffect(() => {
@@ -68,34 +52,6 @@ const CreateRoomModal = () => {
     setFriendCheckList([...newList]);
   }, [friendList]);
 
-  useEffect(() => {
-    const selectMember = friendCheckList.filter(
-      (data: AddCheckFriendData) => data.check,
-    );
-    setSelectListState([...selectMember]);
-  }, [friendCheckList]);
-
-  const onClickPrevStage = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement>) => {
-      e.stopPropagation();
-      setCurrentStage(0);
-    },
-    [selectListState],
-  );
-
-  const onClickNext = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement>) => {
-      e.stopPropagation();
-      if (!roomName) {
-        alert('방이름이 입력되지 않았습니다.');
-        return;
-      } else {
-        setCurrentStage(1);
-      }
-    },
-    [selectListState, roomName],
-  );
-
   const onClickFriendList = (friendId: number) => () => {
     setFriendCheckList((prevData: AddCheckFriendData[]) => {
       const newData = prevData.map((data: AddCheckFriendData) => {
@@ -106,103 +62,112 @@ const CreateRoomModal = () => {
     });
   };
 
+  const onClickPrevStage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setCurrentStage(0);
+  };
+
+  const onClickNext = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    if (!roomName) {
+      alert('방이름이 입력되지 않았습니다.');
+      return;
+    } else {
+      setCurrentStage(1);
+    }
+  };
+
   const createRoomRequest = async () => {
-    const selectMemberIdList = selectListState.map(
-      (data: AddCheckFriendData) => data.id,
-    );
+    const selectMemberIdList = checkFriends.map((data) => data.id);
     await createRoom({ selectMemberIdList, roomName });
-    await fetchRoomList();
     clearModalCache();
   };
 
   return (
-    <ModalBoxContainer>
-      <ModalBox>
-        <Title>
-          <h2>방 생성하기</h2>
-        </Title>
-        {currentStage === 0 ? (
-          <ContentBox>
-            <Content>
-              <RoomNameInput>
-                <label htmlFor="room_name">방 이름</label>
-                <input
-                  type="text"
-                  id="room_name"
-                  onChange={handleRoomName}
-                  value={roomName}
-                />
-              </RoomNameInput>
+    <ModalLayout currentModal="createRoom" size={size}>
+      {currentStage === 0 ? (
+        <>
+          <Content>
+            <RoomNameInput>
+              <label htmlFor="room_name">방 이름</label>
+              <input
+                type="text"
+                id="room_name"
+                onChange={handleRoomName}
+                value={roomName}
+              />
+            </RoomNameInput>
+            <MemeberList>
               <Scrollbars>
-                <MemeberList>
-                  <div className="member_list">
-                    {friendCheckList.length !== 0 ? (
-                      friendCheckList.map((friendData: AddCheckFriendData) => (
-                        <div
-                          key={friendData.id}
-                          className="friend_info_row"
-                          onClick={onClickFriendList(friendData.id)}
-                        >
-                          {friendData.check ? (
-                            <AiFillCheckCircle />
-                          ) : (
-                            <AiOutlineCheckCircle />
-                          )}
-                          <div>
-                            <p>이름: {friendData.name}</p>
-                            <p>이메일: {friendData.email}</p>
-                          </div>
+                <div className="member_list">
+                  {friendCheckList.length !== 0 ? (
+                    friendCheckList.map((friendData: AddCheckFriendData) => (
+                      <div
+                        key={friendData.id}
+                        className="friend_info_row"
+                        onClick={onClickFriendList(friendData.id)}
+                      >
+                        {friendData.check ? (
+                          <AiFillCheckCircle />
+                        ) : (
+                          <AiOutlineCheckCircle />
+                        )}
+                        <div>
+                          <p>이름: {friendData.name}</p>
+                          <p>이메일: {friendData.email}</p>
                         </div>
-                      ))
-                    ) : (
-                      <p>등록된 친구가 없습니다.</p>
-                    )}
-                  </div>
-                </MemeberList>
+                      </div>
+                    ))
+                  ) : (
+                    <p>등록된 친구가 없습니다.</p>
+                  )}
+                </div>
               </Scrollbars>
-            </Content>
+            </MemeberList>
             <ActionBtn>
               <Button type="button" onClick={onClickNext}>
                 다음
               </Button>
             </ActionBtn>
-          </ContentBox>
-        ) : (
-          currentStage === 1 && (
-            <ContentBox>
-              <div className="result_content">
-                <ResultRoomName>
+          </Content>
+        </>
+      ) : (
+        currentStage === 1 && (
+          <>
+            <Content>
+              <CreateRoomInfo>
+                <div className="room_name">
                   <h3>방 이름</h3>
                   <p>{roomName}</p>
-                </ResultRoomName>
-                <ResultMembers>
+                </div>
+                <div className="room_members">
                   <h3>초대할 멤버</h3>
-                  {selectListState.length !== 0 ? (
-                    <ul>
-                      {selectListState
-                        .filter((data) => data.check)
-                        .map((friendData: AddCheckFriendData) => {
+                  <Scrollbars>
+                    {checkFriends.length !== 0 ? (
+                      <ul>
+                        {checkFriends.map((friendData: AddCheckFriendData) => {
                           return <li key={friendData.id}>{friendData.name}</li>;
                         })}
-                    </ul>
-                  ) : (
-                    <p>등록된 친구가 없습니다.</p>
-                  )}
-                </ResultMembers>
-              </div>
-              <ResultActionBtn>
+                      </ul>
+                    ) : (
+                      <p>등록된 친구가 없습니다.</p>
+                    )}
+                  </Scrollbars>
+                </div>
+              </CreateRoomInfo>
+              <ActionBtn style={{ justifyContent: 'space-between' }}>
                 <Button type="button" onClick={onClickPrevStage}>
                   이전
                 </Button>
                 <Button type="button" onClick={createRoomRequest}>
                   생성하기
                 </Button>
-              </ResultActionBtn>
-            </ContentBox>
-          )
-        )}
-      </ModalBox>
-    </ModalBoxContainer>
+              </ActionBtn>
+            </Content>
+          </>
+        )
+      )}
+    </ModalLayout>
   );
 };
 
--- front/components/Modal/CreateRoomModal/styles.tsx
+++ front/components/Modal/CreateRoomModal/styles.tsx
@@ -1,75 +1,11 @@
 import styled from '@emotion/styled';
 
-export const ModalBoxContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  border-radius: 6px;
-`;
-
-export const ModalBox = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  height: 100%;
-  border-radius: 12px;
-
-  box-sizing: border-box;
-  background-color: white;
-`;
-
-export const Title = styled.div`
-  flex: 1;
-
-  height: 30%;
-
-  box-shadow: 0 2px 4px hsla(0, 0%, 81.2%, 0.5);
-  text-align: center;
-
-  span {
-    display: inline-block;
-  }
-`;
-
-export const CloseBtn = styled.div`
-  float: right;
-  display: inline-block;
-  position: relative;
-  right: 5px;
-
-  cursor: pointer;
-
-  svg {
-    width: 22px;
-    height: 22px;
-    vertical-align: top;
-  }
-`;
-
-export const ContentBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 7;
-
-  height: 70%;
-  width: 100%;
-
-  box-sizing: border-box;
-
-  .result_content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    height: calc(100% - 60px);
-  }
-`;
-
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
 
-  height: calc(100% - 60px);
-  padding: 1rem 0;
+  height: 100%;
+  box-sizing: border-box;
 
   overflow: auto;
 `;
@@ -80,15 +16,16 @@ export const RoomNameInput = styled.div`
   align-items: center;
 
   width: 100%;
-  height: 40px;
+  height: 60px;
   gap: 1rem;
   margin: auto;
 
   text-align: center;
+  box-shadow: 0px 1px 2px -1px rgb(0 0 0 / 40%);
 `;
 
 export const MemeberList = styled.div`
-  flex: 6;
+  flex: 1 0 auto;
 
   .member_list {
     display: flex;
@@ -114,6 +51,38 @@ export const MemeberList = styled.div`
   }
 `;
 
+export const CreateRoomInfo = styled.div`
+  flex: 1 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  .room_name {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    gap: 3rem;
+  }
+
+  .room_members {
+    flex: 3;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    ul {
+      width: fit-content;
+      margin: auto;
+      padding: 0;
+
+      li {
+        list-style: circle;
+      }
+    }
+  }
+`;
+
 export const ActionBtn = styled.div`
   display: flex;
   justify-content: center;
@@ -123,34 +92,4 @@ export const ActionBtn = styled.div`
   padding: 0 20px;
 
   box-shadow: 0px -1px 2px -1px rgb(0 0 0 / 40%);
-`;
-
-export const ResultRoomName = styled.div`
-  flex: 2;
-  display: flex;
-  align-items: center;
-
-  max-width: 80%;
-  gap: 3rem;
-`;
-
-export const ResultMembers = styled.div`
-  flex: 6;
-  max-width: 80%;
-
-  ul li {
-    list-style: circle;
-  }
-`;
-
-export const ResultActionBtn = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  width: 100%;
-  height: 60px;
-  padding: 0 20px;
-
-  box-sizing: border-box;
 `;
