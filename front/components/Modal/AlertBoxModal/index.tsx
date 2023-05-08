94ce283b36f196fed3f0681fea10840a2e1a4398
kanghunK <ltbehappy16@gmail.com>
2023-05-08 22:47:19 +0900

    Modify: ModalLayout 컴포넌트 적용 및  css 수정

--- front/components/Modal/AlertBoxModal/index.tsx
+++ front/components/Modal/AlertBoxModal/index.tsx
@@ -3,33 +3,37 @@ import { Button } from '@styles/Button';
 import { Wrapper } from './styles';
 import useModal from '@hooks/useModal';
 import { IAertData } from '@typing/client';
+import ModalLayout from '../ModalLayout';
 
 const AlertBox = ({ alertData }: { alertData: IAertData }) => {
   const userId = sessionStorage.getItem('user_id');
   if (!userId) return null;
 
-  const alertBoxEl = useRef<HTMLDivElement>(null);
+  const size = { width: 300, height: 200 };
+  // const alertBoxEl = useRef<HTMLDivElement>(null);
   const { clearModalCache } = useModal();
   const { executeWork } = alertData;
 
   return (
-    <Wrapper ref={alertBoxEl}>
-      <p>{alertData?.text}</p>
-      <div className="btn_group">
-        <Button
-          type="button"
-          onClick={() => {
-            executeWork();
-            clearModalCache();
-          }}
-        >
-          확인
-        </Button>
-        <Button type="button" onClick={() => clearModalCache()}>
-          취소
-        </Button>
-      </div>
-    </Wrapper>
+    <ModalLayout currentModal="alert" size={size}>
+      <Wrapper>
+        <p>{alertData?.text}</p>
+        <div className="btn_group">
+          <Button
+            type="button"
+            onClick={() => {
+              executeWork();
+              clearModalCache();
+            }}
+          >
+            확인
+          </Button>
+          <Button type="button" onClick={() => clearModalCache()}>
+            취소
+          </Button>
+        </div>
+      </Wrapper>
+    </ModalLayout>
   );
 };
 
