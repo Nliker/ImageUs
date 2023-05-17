08666b6be0c634c07cf168c45acdbb370aa719ee
kanghunK <ltbehappy16@gmail.com>
2023-05-17 22:52:07 +0900

    Modify: room:roomId Route에 loader 추가

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useMemo } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import loadable from '@loadable/component';
 import useAuth from '@hooks/useAuth';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
+import useRoomList from '@hooks/useRoomList';
+import { DRoomData } from '@typing/db';
 
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
@@ -14,7 +21,7 @@ const PeopleManagement = loadable(() => import('@pages/PeopleManagement'));
 const ImageRoom = loadable(() => import('@pages/ImageRoom'));
 const SocialLogInAuth = loadable(() => import('@pages/SocialLogInAuth'));
 
-const App = () => {
+function App() {
   const { isAuthenticated, loading, userInfo, error, refreshAuthData } =
     useAuth();
 
@@ -27,23 +34,47 @@ const App = () => {
     refreshAuthData();
   }, []);
 
-  return (
-    <Routes>
-      <Route element={<PublicRoute authData={authData} />}>
-        <Route index element={<Intro />} />
-        <Route path="login" element={<LogIn />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="callback/oauth-login" element={<SocialLogInAuth />} />
-      </Route>
-      <Route element={<PrivateRoute authData={authData} />}>
-        <Route path="select-room" element={<SelectRoom />} />
-        <Route path="my_page/*" element={<MyPage userInfo={userInfo} />} />
-        <Route path="room/:roomId" element={<ImageRoom />} />
-        <Route path="people_management/*" element={<PeopleManagement />} />
-      </Route>
-      <Route path="*" element={<div>404 페이지</div>} />
-    </Routes>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route element={<PublicRoute authData={authData} />}>
+          <Route index element={<Intro />} />
+          <Route path="login" element={<LogIn />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="callback/oauth-login" element={<SocialLogInAuth />} />
+        </Route>
+        <Route element={<PrivateRoute authData={authData} />}>
+          <Route path="select-room" element={<SelectRoom />} />
+          <Route path="my_page/*" element={<MyPage userInfo={userInfo} />} />
+          <Route
+            path="room/:roomId"
+            loader={async ({ params }) => {
+              console.log('loader');
+              const userId = sessionStorage.getItem('user_id');
+              const { roomId } = params;
+              if (!userId)
+                throw new Error('유저의 정보가 없습니다.. 다시로그인하세요');
+
+              const { roomList } = useRoomList(userId);
+              const isValidRoomId = roomList?.some((roomInfo: DRoomData) => {
+                return '' + roomInfo.id === roomId;
+              });
+
+              console.log('방 존재', roomList, isValidRoomId);
+              if (!isValidRoomId)
+                throw new Error('존재하지 않는 방입니다..다른 방에 접속하세요');
+            }}
+            errorElement={<div>not found</div>}
+            element={<ImageRoom />}
+          />
+          <Route path="people_management/*" element={<PeopleManagement />} />
+        </Route>
+        <Route path="*" element={<div>404 페이지</div>} />
+      </Route>,
+    ),
   );
-};
+
+  return <RouterProvider router={router} />;
+}
 
 export default App;
