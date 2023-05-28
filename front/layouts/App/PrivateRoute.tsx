12d2a18215f67117ba039048cd1784ba97b6670b
kanghunK <ltbehappy16@gmail.com>
2023-05-28 16:16:08 +0900

    Modify: PrivateRoute의 Outlet에 AppLayout 제거 및 에러 발생 시 세션 스토리지 비우는 코드 추가

--- front/layouts/App/PrivateRoute.tsx
+++ front/layouts/App/PrivateRoute.tsx
@@ -1,39 +1,45 @@
 import React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router';
-import Spinner from '@styles/Spinner';
 import { CAuthData } from '@typing/client';
-import { getErrorMessage } from '@utils/getErrorMessage';
 import AppLayout from '@layouts/AppLayout';
+import { GlobalErrorBoundary } from '@components/ErrorBoundary';
+import { getErrorMessage } from '@utils/getErrorMessage';
+import Spinner from '@styles/Spinner';
+import withErrorBoundary from '@layouts/ErrorBoundary';
+import { DUserInfo } from '@typing/db';
 
 interface IProps {
   authData: CAuthData;
 }
 
+// interface IPrivatePage {}
+
 const PrivateRoute = ({ authData }: IProps) => {
-  const location = useLocation();
-  const currentPage = location.pathname.split('/')[1];
-  const { isAuthenticated, loading, error } = authData;
+  // const location = useLocation();
+  // const currentPage = location.pathname.split('/')[1];
+  const { isAuthenticated, loading, error, userInfo } = authData;
+
+  console.log('private', isAuthenticated, loading, userInfo);
 
   if (error) {
     const message = getErrorMessage(error);
     alert(message);
+    sessionStorage.clear();
     return <Navigate to="/login" />;
   }
-
-  console.log('private', isAuthenticated, loading);
 
   if (loading || isAuthenticated === 'init') return <Spinner />;
 
   if (isAuthenticated === 'unauthorized') {
-    alert('..로그인이 필요합니다.');
+    alert('..로그인이 필요합니다. 확인');
     return <Navigate to="/login" />;
   } else {
-    return (
-      <AppLayout isImageRoom={currentPage === 'room'}>
-        <Outlet />;
-      </AppLayout>
-    );
+    // const privateRouteOutlet = <Outlet context={{ userInfo }} />;
+    // return withErrorBoundary(privateRouteOutlet);
+
+    return <Outlet context={{ userInfo }} />;
   }
+  // return <Outlet context={{ userInfo }} />;
 };
 
 export default PrivateRoute;
