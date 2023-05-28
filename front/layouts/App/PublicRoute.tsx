9bdb305364accc18ae26c6b6e990a09d21de206b
kanghunK <ltbehappy16@gmail.com>
2023-05-28 16:16:39 +0900

    Modify: PublicRoute에 에러 발생 시 웹 스토리지 비우는 코드 추가

--- front/layouts/App/PublicRoute.tsx
+++ front/layouts/App/PublicRoute.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router';
+import { Navigate, Outlet, useLocation } from 'react-router';
 import Spinner from '@styles/Spinner';
 import { CAuthData } from '@typing/client';
 import { getErrorMessage } from '@utils/getErrorMessage';
+import { GlobalErrorBoundary } from '@components/ErrorBoundary';
+import withErrorBoundary from '@layouts/ErrorBoundary';
 
 interface IProps {
   authData: CAuthData;
@@ -14,8 +16,11 @@ const PublicRoute = ({ authData }: IProps) => {
   if (error) {
     const message = getErrorMessage(error);
     alert(message);
+    sessionStorage.clear();
     return <Navigate to="/login" />;
   }
+
+  console.log('public', isAuthenticated, loading);
 
   if (loading || isAuthenticated === 'init') return <Spinner />;
 
