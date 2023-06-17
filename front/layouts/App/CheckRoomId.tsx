eded9234b1b089622ec65082bbc95c1979168b92
kanghunK <ltbehappy16@gmail.com>
2023-06-17 12:49:37 +0900

    Modify: Outlet context 속성 제거

--- front/layouts/App/CheckRoomId.tsx
+++ front/layouts/App/CheckRoomId.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Outlet, useOutletContext, useParams } from 'react-router';
+import { Outlet, useParams } from 'react-router';
 import { DRoomData } from '@typing/db';
 import useRoomList from '@hooks/useRoomList';
-import { PrivateChildProps } from '@typing/client';
 import { PageLoading } from '@styles/Spinner';
+import { useUserInfo } from '@hooks/useUserInfo';
 
 const CheckRoomId = () => {
-  const { userInfo } = useOutletContext<PrivateChildProps>();
+  const { userInfo } = useUserInfo();
   const { roomId } = useParams<{ roomId?: string }>();
-  const { roomList, loading } = useRoomList(userInfo.id);
+  const { roomList } = useRoomList(userInfo.id);
 
   const isValidRoomId = roomList?.some((roomInfo: DRoomData) => {
     return '' + roomInfo.id === roomId;
   });
 
-  if (loading || !roomId) return <PageLoading />;
+  if (!roomId || !roomList) return <PageLoading />;
 
   if (!isValidRoomId) {
     const error = new Error('존재하지 않는 방입니다..다른 방에 접속하세요');
@@ -22,7 +22,7 @@ const CheckRoomId = () => {
     throw error;
   }
 
-  return <Outlet context={{ userInfo, roomId }} />;
+  return <Outlet />;
 };
 
 export default CheckRoomId;
